d351b5069c3e363ab7d1c782bb5bcd3e85a0ffa2
Iliyan Peychev <iliyan.peychev@liferay.com>
2014-11-26 17:07:49 +0100

    Add Function.prototype.bind

--- src/js/utils.js
+++ src/js/utils.js
@@ -66,6 +66,22 @@
         return -1;
     };
 
+    // Unceremoniously lifted from MDN
+    Function.prototype.bind = Function.prototype.bind || function(oThis) {
+        var aArgs = Array.prototype.slice.call(arguments, 1),
+            fToBind = this,
+            fNOP = function() {},
+            fBound = function() {
+                return fToBind.apply(this instanceof fNOP && oThis ? this : oThis,
+                    aArgs.concat(Array.prototype.slice.call(arguments)));
+            };
+
+        fNOP.prototype = this.prototype;
+        fBound.prototype = new fNOP();
+
+        return fBound;
+    };
+
     Object.prototype.forEach = Object.prototype.forEach || function(obj, callback, context) {
         for (var key in obj) {
             if (hasOwnProperty.call(obj, key)) {
