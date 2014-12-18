f50c772dde78aa324322ed4c9238f1ed0a65e998
Iliyan Peychev <iliyan.peychev@liferay.com>
2014-12-17 17:31:28 -0800

    Add test for www. in URLBuilder

--- src/js/url-builder.js
+++ src/js/url-builder.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// External protocols regex, supports:
+// "http", "https", "//" and "www."
+var REGEX_EXTERNAL_PROTOCOLS = /https?:\/\/|\/\/|www\./;
+
 /**
  * Creates an instance of URLBuilder class.
  *
@@ -10,7 +14,6 @@ function URLBuilder(configParser) {
     this._configParser = configParser;
 }
 
-var REGEX_EXTERNAL_PROTOCOLS = /https?:\/\/|\/\/|www\.|gopher:\/\//;
 
 URLBuilder.prototype = {
     constructor: URLBuilder,
