d882d384d8d83d2af322ce45f6e6e2a1b38b0860
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2014-12-19 08:32:51 -0800

    Adding gitignore, fixing so that everything is generated into a theme folder

--- packages/generator-liferay-theme/app/index.js
+++ packages/generator-liferay-theme/app/index.js
@@ -2,61 +2,87 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var path = require('path');
+var slug = require('slug');
+var _ = require('lodash');
 
 module.exports = yeoman.generators.Base.extend({
-  initializing: function () {
-    this.pkg = require('../package.json');
-  },
+	initializing: function () {
+		this.pkg = require('../package.json');
+	},
 
-  prompting: function () {
-    var done = this.async();
+	prompting: function () {
+		var done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the splendid' + chalk.red('LiferayTheme') + ' generator!'
-    ));
+		// Have Yeoman greet the user.
+		this.log(yosay(
+			'Welcome to the splendid ' + chalk.red('Liferay Theme') + ' generator!'
+		));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+		var prompts = [
+			{
+				type: 'input',
+				name: 'themeName',
+				message: 'What would you like to call your theme?',
+				default: 'My Liferay Theme'
+			},
+			{
+				type: 'input',
+				name: 'themeId',
+				message: 'Would you like to use this as the themeId?',
+				default: function(answers) {
+					return slug((answers.themeName || '').toLowerCase());
+				}
+			}
+		];
 
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+		this.prompt(prompts, function (props) {
+			this.themeName = props.themeName;
+			this.themeId = props.themeId;
 
-      done();
-    }.bind(this));
-  },
+			var themeDirName = this.themeId;
 
-  writing: {
-    app: function () {
-      this.fs.copy(
-        this.templatePath('_package.json'),
-        this.destinationPath('package.json')
-      );
-      this.fs.copy(
-        this.templatePath('_bower.json'),
-        this.destinationPath('bower.json')
-      );
-    },
+			if (!(/-theme$/.test(themeDirName))) {
+				themeDirName += '-theme';
+			}
 
-    projectfiles: function () {
-      this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
-      );
-    }
-  },
+			this.themeDirName = themeDirName;
 
-  install: function () {
-    this.installDependencies({
-      skipInstall: this.options['skip-install']
-    });
-  }
+			done();
+		}.bind(this));
+	},
+
+	configuring: {
+		enforceFolderName: function() {
+			if (this.themeDirName !== _.last(this.destinationRoot().split(path.sep))) {
+				this.destinationRoot(this.themeDirName);
+			}
+		},
+	},
+
+	writing: {
+		app: function () {
+			this.fs.copy(
+				this.templatePath('_package.json'),
+				this.destinationPath('package.json')
+			);
+			this.fs.copy(
+				this.templatePath('_bower.json'),
+				this.destinationPath('bower.json')
+			);
+		},
+
+		projectfiles: function () {
+			this.fs.copy(
+				this.templatePath('gitignore'),
+				this.destinationPath('.gitignore')
+			);
+		}
+	},
+
+	install: function () {
+		this.installDependencies({
+			skipInstall: this.options['skip-install']
+		});
+	}
 });
