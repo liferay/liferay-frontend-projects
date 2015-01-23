6c17c3659a667a186b75d2633b25e5a6ac222298
Iliyan Peychev <iliyan.peychev@liferay.com>
2015-01-23 08:48:26 +0100

    Add demo config.js

--- dist/demo/config/config.js
+++ dist/demo/config/config.js
@@ -1,5 +1,48 @@
 var __CONFIG__ = {
     "url": "http://localhost:3000/combo",
     "basePath": "/modules",
-    "combine": true
+    "combine": true,
+    "modules": {
+        "aui-base": {
+            "dependencies": [
+                "exports"
+            ],
+            "path": "aui-base.js"
+        },
+        "aui-core": {
+            "dependencies": [
+                "exports"
+            ],
+            "path": "aui-core.js"
+        },
+        "aui-dialog": {
+            "dependencies": [
+                "exports",
+                "aui-base",
+                "aui-core",
+                "aui-event"
+            ],
+            "path": "aui-dialog.js"
+        },
+        "aui-event": {
+            "dependencies": [
+                "exports"
+            ],
+            "path": "aui-event.js"
+        },
+        "chema/chemaps/aui-chemaps": {
+            "dependencies": [
+                "exports",
+                "aui-base"
+            ],
+            "path": "chema/chemaps/aui-chemaps.js"
+        },
+        "ambrin/aui-ambrin": {
+            "dependencies": [
+                "exports",
+                "aui-core"
+            ],
+            "path": "ambrin/aui-ambrin.js"
+        }
+    }
 };
