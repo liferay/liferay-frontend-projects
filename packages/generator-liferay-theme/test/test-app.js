8f052753759f8463ff2d190de87d5c9e697c9f40
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2015-02-11 16:05:34 -0800

    Check for correct file structure

--- packages/generator-liferay-theme/test/test-app.js
+++ packages/generator-liferay-theme/test/test-app.js
@@ -11,7 +11,9 @@ describe('liferay-theme:app', function () {
       .inDir(path.join(os.tmpdir(), './temp-test'))
       .withOptions({ 'skip-install': true })
       .withPrompt({
-        someOption: true
+        themeName: 'test-theme',
+        themeId: 'test-theme',
+        supportCompass: false
       })
       .on('end', done);
   });
@@ -19,9 +21,13 @@ describe('liferay-theme:app', function () {
   it('creates files', function () {
     assert.file([
       'bower.json',
+      'gulpfile.js',
       'package.json',
-      '.editorconfig',
-      '.jshintrc'
+      'src/css/custom.css',
+      'src/META-INF/context.xml',
+      'src/WEB-INF/liferay-plugin-package.properties',
+      'src/WEB-INF/src/resources-importer/readme.txt',
+      'src/WEB-INF/src/resources-importer/sitemap.json'
     ]);
   });
 });
