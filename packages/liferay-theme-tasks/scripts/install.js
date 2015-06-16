8fc14582127f0cd7515725b07ade314f51811fa1
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2015-06-16 12:25:45 -0700

    Dynamically install liferay theme dependencies based on parent theme's package.json properties

---
+++ packages/liferay-theme-tasks/scripts/install.js
@@ -0,0 +1,43 @@
+var npm = require('npm');
+var path = require('path');
+var fs = require('fs');
+
+npm.load({
+	loaded: false
+}, function(err) {
+	if (err) throw err;
+
+	var liferayVersion = getParentThemeLiferayVersion();
+
+	var themeDependencies = getThemeDependencies(liferayVersion);
+
+	npm.commands.install(themeDependencies, function(err, data) {
+		if (err) throw err;
+	});
+});
+
+function getParentThemeLiferayVersion() {
+	var cwd = process.cwd()
+
+	var parentThemePackageJSON = path.resolve(cwd, '../../', 'package.json');
+
+	var liferayVersion = '7.0';
+
+	if (fs.existsSync(parentThemePackageJSON)) {
+		var packageJSON = require(parentThemePackageJSON);
+
+		liferayVersion = (packageJSON.liferayTheme && packageJSON.liferayTheme.version) ? packageJSON.liferayTheme.version : liferayVersion;
+	}
+
+	return liferayVersion;
+}
+
+function getThemeDependencies(version) {
+	var versionString = '@^' + version + '.0';
+
+	var mixins = 'liferay-theme-mixins' + versionString;
+	var styled = 'liferay-theme-styled' + versionString;
+	var unstyled = 'liferay-theme-unstyled' + versionString;
+
+	return [mixins, styled, unstyled];
+}
