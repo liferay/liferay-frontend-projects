b7b8dccb322bf34477984d83c2e5615aaaf76f35
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2015-06-17 10:07:33 -0700

    Fix file creation test

--- packages/generator-liferay-theme/test/test-app.js
+++ packages/generator-liferay-theme/test/test-app.js
@@ -7,7 +7,7 @@ var os = require('os');
 
 describe('liferay-theme:app', function () {
   before(function (done) {
-    helpers.run(path.join(__dirname, '../app'))
+    helpers.run(path.join(__dirname, '../generators/app'))
       .inDir(path.join(os.tmpdir(), './temp-test'))
       .withOptions({ 'skip-install': true })
       .withPrompt({
