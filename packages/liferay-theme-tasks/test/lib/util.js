6f3941cae539024743d0e976b42f89d98c19ea01
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2015-07-02 10:26:14 -0700

    Add tests for lib/util.js

---
+++ packages/liferay-theme-tasks/test/lib/util.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var chai = require('chai');
+var fs = require('fs-extra');
+var os = require('os');
+var path = require('path');
+var util = require('../../lib/util.js');
+
+var assert = chai.assert;
+chai.use(require('chai-fs'));
+
+var tempPath = path.join(os.tmpdir(), 'liferay-theme-tasks', 'base-theme');
+
+describe('Util functions', function() {
+	it('should return correct src paths depending on deployed status/changed file', function(done) {
+		var originalSrcPath = 'src/css/**/*';
+
+		var srcPath = util.getSrcPath(originalSrcPath, {
+			deployed: true
+		});
+
+		assert.equal(srcPath, originalSrcPath);
+
+		var changedFile = {
+			type: 'changed',
+			path: path.join(tempPath, 'src/css/custom.css')
+		};
+
+		srcPath = util.getSrcPath(originalSrcPath, {
+			changedFile: changedFile,
+			deployed: true
+		});
+
+		assert.equal(srcPath, 'src/css/**/custom.css');
+
+		srcPath = util.getSrcPath(originalSrcPath, {
+			changedFile: changedFile,
+			deployed: true
+		}, function(changedFileName) {
+			assert.equal(changedFileName, 'custom.css');
+
+			return false;
+		});
+
+		assert.equal(srcPath, originalSrcPath);
+
+		done();
+	});
+
+	it('should only return true if css file', function(done) {
+		assert(util.isCssFile('custom.css'));
+		assert(!util.isCssFile('main.js'));
+
+		done();
+	});
+});
