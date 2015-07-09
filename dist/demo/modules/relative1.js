5309135fdfa90753e78e24409982231b36f223be
Iliyan Peychev <iliyan.peychev@liferay.com>
2015-07-09 08:32:27 +0200

    Add build files (auto-generated)
    Fixes #13

--- dist/demo/modules/relative1.js
+++ dist/demo/modules/relative1.js
@@ -1,10 +1,12 @@
 define("liferay@1.0.0/relative1", ["exports", "module", "./relative2"], function(exports, module, _relative2) {
     "use strict";
 
-    var _interopRequire = function(obj) {
-        return (obj && obj.__esModule ? obj["default"] : obj);
-    };
+    function _interopRequireDefault(obj) {
+        return (obj && obj.__esModule ? obj : {
+            "default": obj
+        });
+    }
 
-    var _relative22 = _interopRequire(_relative2);
-    module.exports = _relative22;
+    var _relative22 = _interopRequireDefault(_relative2);
+    module.exports = _relative22["default"];
 });
