0e7953272edbc9557efeb11fd8829adfb19d207d
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2015-09-18 16:35:12 -0700

    Include themelet js files in portal_normal on build

--- packages/liferay-theme-tasks/tasks/themelet.js
+++ packages/liferay-theme-tasks/tasks/themelet.js
@@ -2,6 +2,7 @@
 
 var _ = require('lodash');
 var async = require('async');
+var lfrThemeConfig = require('../lib/liferay_theme_config');
 var path = require('path');
 var plugins = require('gulp-load-plugins')();
 
@@ -17,7 +18,7 @@ module.exports = function(options) {
 	gulp.task('build:themelets', function(cb) {
 		runSequence(
 			['build:themelet-css', 'build:themelet-images', 'build:themelet-js', 'build:themelet-templates'],
-			'build:themelet-css-inject',
+			['build:themelet-css-inject', 'build:themelet-js-inject'],
 			cb
 		);
 	});
@@ -37,13 +38,35 @@ module.exports = function(options) {
 			.pipe(plugins.inject(sources, {
 				starttag: '/* inject:imports */',
 				endtag: '/* endinject */',
-				transform: function (filepath) {
-					filepath = filepath.replace(/(\/build\/css\/)(.*)/, '$2');
+				transform: function(filePath) {
+					filePath = filePath.replace(/(\/build\/css\/)(.*)/, '$2');
 
-					return '@import url(' + filepath + ');';
+					return '@import url(' + filePath + ');';
 				}
 			}))
 			.pipe(gulp.dest('build/css'));
+	});
+
+	gulp.task('build:themelet-js-inject', function(cb) {
+		var themeSrcPaths = path.join(pathBuild, 'js/themelets/**/*.js');
+
+		var sources = gulp.src(themeSrcPaths, {
+			read: false
+		});
+
+		return gulp.src(['build/templates/portal_normal.ftl', 'build/templates/portal_normal.vm'])
+			.pipe(plugins.inject(sources, {
+				starttag: '<!-- inject:js -->',
+				endtag: '<!-- endinject -->',
+				transform: function(filePath) {
+					var themeName = lfrThemeConfig.getConfig(true).name;
+
+					filePath = path.join(themeName, filePath.replace(/(\/build\/)(.*)/, '$2'));
+
+					return '<script src="' + filePath + '"></script>';
+				}
+			}))
+			.pipe(gulp.dest('build/templates'));
 	});
 
 	gulp.task('build:themelet-images', function(cb) {
