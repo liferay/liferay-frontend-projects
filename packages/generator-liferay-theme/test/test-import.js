b1e5237ee1df06a52b59e7f172ca65f1b05f44e7
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2015-10-27 11:48:34 -0700

    Add basic tests for import generator

---
+++ packages/generator-liferay-theme/test/test-import.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var _ = require('lodash');
+var assert = require('yeoman-generator').assert;
+var chai = require('chai');
+var fs = require('fs');
+var helpers = require('yeoman-generator').test;
+var os = require('os');
+var path = require('path');
+
+var chaiAssert = chai.assert;
+chai.use(require('chai-fs'));
+
+var tempDir = path.join(os.tmpdir(), 'temp-test');
+
+var tempThemeDir = path.join(tempDir, 'sdk-theme');
+
+var pathLiferayPluginPackageProperties = path.join(tempThemeDir, 'src/WEB-INF/liferay-plugin-package.properties');
+
+var pathLookAndFeel = path.join(tempThemeDir, 'src/WEB-INF/liferay-look-and-feel.xml');
+
+var pathSdkTheme = path.join(__dirname, 'assets/sdk-theme');
+
+describe('liferay-theme:app', function () {
+	afterEach(function(done) {
+		fs.rmdir(path.join(tempDir, 'sdk-theme'), function() {
+			done();
+		});
+	});
+
+	it('creates files', function(done) {
+		runGenerator(null, function() {
+			assert.file([
+				'gulpfile.js',
+				'package.json',
+				'src/css/custom.css',
+				'src/templates/portal_normal.vm',
+				'src/WEB-INF/liferay-look-and-feel.xml',
+				'src/WEB-INF/liferay-plugin-package.properties',
+				'src/WEB-INF/liferay-releng.properties'
+			]);
+
+			done();
+		});
+	});
+
+	it('populates 6.2 package.json correctly', function(done) {
+		runGenerator({
+			supportCompass: true
+		}, function() {
+			var pkg = getPackage();
+
+			assert.equal(pkg.liferayTheme.supportCompass, true);
+			assert.equal(pkg.liferayTheme.templateLanguage, 'vm');
+			assert.equal(pkg.liferayTheme.version, '6.2');
+			assert.equal(pkg.publishConfig.tag, '6_2_x');
+			assert.equal(pkg.version, '0.0.0');
+
+			done();
+		});
+	});
+});
+
+function getPackage(themeName) {
+	var fileContents = fs.readFileSync(path.join(tempThemeDir, 'package.json'));
+
+	return JSON.parse(fileContents);
+}
+
+function runGenerator(options, end) {
+	options = options || {};
+
+	options = _.defaults(options, {
+		importTheme: pathSdkTheme,
+		supportCompass: false
+	});
+
+	helpers.run(path.join(__dirname, '../generators/import'))
+		.inDir(tempDir)
+		.withOptions({
+			'skip-install': true
+		})
+		.withPrompt(options)
+		.on('end', end);
+}
