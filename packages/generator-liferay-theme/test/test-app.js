771f70bc271d062a7daf72e2b6e8dc0b0b86b685
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2015-10-27 09:59:03 -0700

    Fix tests. Test formatting

--- packages/generator-liferay-theme/test/test-app.js
+++ packages/generator-liferay-theme/test/test-app.js
@@ -1,32 +1,38 @@
 'use strict';
 
-var path = require('path');
 var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 var os = require('os');
+var path = require('path');
 
 describe('liferay-theme:app', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../generators/app'))
-      .inDir(path.join(os.tmpdir(), './temp-test'))
-      .withOptions({ 'skip-install': true })
-      .withPrompt({
-        themeName: 'test-theme',
-        themeId: 'test-theme',
-        supportCompass: false
-      })
-      .on('end', done);
-  });
+	before(function (done) {
+		this.timeout(10000);
 
-  it('creates files', function () {
-    assert.file([
-      'gulpfile.js',
-      'package.json',
-      'src/css/custom.css',
-      'src/META-INF/context.xml',
-      'src/WEB-INF/liferay-plugin-package.properties',
-      'src/WEB-INF/src/resources-importer/readme.txt',
-      'src/WEB-INF/src/resources-importer/sitemap.json'
-    ]);
-  });
+		helpers.run(path.join(__dirname, '../generators/app'))
+			.inDir(path.join(os.tmpdir(), './temp-test'))
+			.withOptions({
+				'skip-install': true
+			})
+			.withPrompt({
+				liferayVersion: '7.0',
+				supportCompass: false,
+				templateLanguage: 'vm',
+				themeId: 'test-theme',
+				themeName: 'test-theme'
+			})
+			.on('end', done);
+	});
+
+	it('creates files', function () {
+		assert.file([
+			'gulpfile.js',
+			'package.json',
+			'src/css/_custom.scss',
+			'src/META-INF/context.xml',
+			'src/WEB-INF/liferay-plugin-package.properties',
+			'src/WEB-INF/src/resources-importer/readme.txt',
+			'src/WEB-INF/src/resources-importer/sitemap.json'
+		]);
+	});
 });
