9184e166454e5fb78248be0add4fd5ad17198802
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2015-12-08 11:50:11 -0800

    Add unit tests for theme generator

---
+++ packages/generator-liferay-theme/test/unit/app.js
@@ -0,0 +1,99 @@
+'use strict';
+
+var chai = require('chai');
+var chaiAssert = chai.assert;
+
+var liferayThemeApp = require('../../generators/app/index');
+
+describe('liferay-theme:app unit tests', function() {
+	// _getArgs
+	it('creates new args object only once', function(done) {
+		var args = liferayThemeApp.prototype._getArgs();
+
+		chaiAssert.isObject(args);
+
+		args.test = 'test';
+
+		chaiAssert.deepEqual(args, liferayThemeApp.prototype._getArgs());
+
+		args.test2 = 'test';
+
+		chaiAssert.deepEqual(args, liferayThemeApp.prototype._getArgs());
+
+		done();
+	});
+
+	// _getWhenFn
+	it('returns false false when property has been set on argv and sets property on args object', function(done) {
+		liferayThemeApp.prototype.args = {};
+		liferayThemeApp.prototype.argv = {};
+
+		var flagName = 'compass';
+		var propertyName = 'supportCompass';
+
+		var whenFn = liferayThemeApp.prototype._getWhenFn(propertyName, flagName);
+
+		chaiAssert.isFunction(whenFn);
+		chaiAssert(whenFn());
+
+		liferayThemeApp.prototype.argv = {
+			compass: true
+		};
+
+		whenFn = liferayThemeApp.prototype._getWhenFn(propertyName, flagName);
+
+		chaiAssert.isFunction(whenFn);
+		chaiAssert(!whenFn());
+		chaiAssert.equal(liferayThemeApp.prototype.args[propertyName], true);
+
+		done();
+	});
+
+	// _mixArgs
+	it('mixes props and args', function(done) {
+		var props = liferayThemeApp.prototype._mixArgs({
+			supportCompass: true,
+			liferayVersion: '7.0'
+		}, {
+			supportCompass: false,
+			templateLanguage: 'vm',
+			themeId: 'id',
+			themeName: 'name'
+		});
+
+		chaiAssert.deepEqual(props, {
+			liferayVersion: '7.0',
+			supportCompass: false,
+			templateLanguage: 'vm',
+			themeId: 'id',
+			themeName: 'name'
+		});
+
+		done();
+	});
+
+	// _setArgv
+	it('should set correct argv properties based on shorthand values', function(done) {
+		var originalArgv = process.argv;
+
+		var mockArgv = ['node', 'yo', 'liferay-theme', '-n', 'My Liferay Theme', '-i', 'my-liferay-theme', '-l', '7.0'];
+
+		process.argv = mockArgv;
+
+		liferayThemeApp.prototype._setArgv();
+
+		chaiAssert.deepEqual(liferayThemeApp.prototype.argv, {
+			_: ['liferay-theme'],
+			i: 'my-liferay-theme',
+			id: 'my-liferay-theme',
+			l: '7.0',
+			liferayVersion: '7.0',
+			n: 'My Liferay Theme',
+			name: 'My Liferay Theme'
+		});
+
+		process.argv = originalArgv;
+
+		done();
+	});
+});
