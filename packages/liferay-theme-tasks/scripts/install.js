def781c7549f6984ede5dd4e2da4bca882363cf3
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2016-01-29 12:19:36 -0800

    Remove unused code

--- packages/liferay-theme-tasks/scripts/install.js
+++ packages/liferay-theme-tasks/scripts/install.js
@@ -1,9 +1,7 @@
-var _ = require('lodash');
 var events = require('events');
 var fs = require('fs');
 var lfrThemeConfig = require('../lib/liferay_theme_config');
 var path = require('path');
-var spawn = require('child_process').spawn;
 var themeUtil = require('../lib/util');
 var versionMap = require('../lib/version_map');
 
@@ -13,25 +11,7 @@ var DEFAULT_THEME_CONFIG = {
 	version: '7.0'
 };
 
-var SASS_DEPENDENCIES = {
-	libSass: {
-		'compass-mixins': '0.12.x',
-		'gulp-sass': '2.0.x'
-	},
-
-	rubySass: {
-		'gulp-ruby-sass': '2.0.x'
-	}
-};
-
-var STR_STYLED = 'styled';
-
 var STR_UNSTYLED = 'unstyled';
-
-var VERSION_MAP = {
-	'6.2': '6.2',
-	'7.0': '1.0'
-};
 
 var eventEmitter = new events.EventEmitter();
 
@@ -42,32 +22,6 @@ if (hasParentTheme()) {
 }
 
 var parentLiferayThemeVersion = themeConfig.version;
-
-function getSassDependencies(supportCompass) {
-	var dependencyMap = supportCompass ? SASS_DEPENDENCIES.rubySass : SASS_DEPENDENCIES.libSass;
-
-	return _.map(dependencyMap, function(item, index) {
-		return index + '@' + item;
-	});
-}
-
-function getThemeDependencies(version, supportCompass) {
-	var versionString = '@' + VERSION_MAP[version] + '.x';
-
-	var mixins = versionMap.getDependencyName('mixins', version) + versionString;
-	var styled = versionMap.getDependencyName('styled', version) + versionString;
-	var unstyled = versionMap.getDependencyName('unstyled', version) + versionString;
-
-	var dependencies = getSassDependencies(supportCompass).concat([mixins, styled, unstyled]);
-
-	if (themeConfig.baseTheme == 'classic' && version != '6.2') {
-		var classic = versionMap.getDependencyName('classic', version) + versionString;
-
-		dependencies.push(classic);
-	}
-
-	return dependencies;
-}
 
 function hasParentTheme() {
 	return fs.existsSync(path.join(__dirname, '../../../package.json'));
