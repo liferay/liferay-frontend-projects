1ad96f3865b2a448759f304252c8905d0eeb2db1
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2016-03-11 12:45:30 -0800

    Print test coverage with mocha tests

---
+++ packages/liferay-theme-tasks/test/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var Gulp = require('gulp').Gulp;
+var istanbul = require('gulp-istanbul');
+var mocha = require('gulp-mocha');
+
+var gulp = new Gulp();
+
+var runSequence = require('run-sequence').use(gulp);
+
+gulp.task('pre-test', function () {
+	return gulp.src(['lib/**/*.js', 'tasks/**/*.js'])
+		.pipe(istanbul())
+		.pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['pre-test'], function () {
+	return gulp.src(['test/lib/*.js', 'test/tasks/*.js'])
+		.pipe(mocha({
+			timeout: 4000
+		}))
+		.pipe(istanbul.writeReports());
+});
+
+runSequence('test');
