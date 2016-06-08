98321afbb5af54016693ade0a2ef5b31c6f25705
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2016-06-08 09:43:01 -0700

    Add goto-shell-helper test for watch socket

--- packages/liferay-theme-tasks/test/tasks/watch.js
+++ packages/liferay-theme-tasks/test/tasks/watch.js
@@ -43,6 +43,10 @@ describe('Watch Task', function() {
 
 			registerTasks({
 				gulp: gulp,
+				gogoShellConfig: {
+					host: '0.0.0.0',
+					port: 1337
+				},
 				pathBuild: './custom_build_path',
 				pathSrc: './custom_src_path',
 				rubySass: false
@@ -164,6 +168,69 @@ describe('Watch Task', function() {
 			assert(/<script src="\${theme_display.getPathThemeRoot\(\)}\/themelets\/test-themelet\/js\/main\.js"><\/script>/.test(deployedFileContent), 'themelet js got injected');
 
 			done();
+		});
+	});
+
+	it('should start watch socket', function(done) {
+		this.timeout(6000);
+
+		var GogoShellHelper = require('gogo-shell-helper');
+
+		var helper = GogoShellHelper.start({
+			host: '0.0.0.0',
+			port: 1337,
+			commands: [
+				{
+					command: 'install webbundledir:',
+					response: 'Bundle ID: 321'
+				},
+				{
+					command: 'start'
+				},
+				{
+					command: 'stop'
+				},
+				{
+					command: 'uninstall'
+				},
+				{
+					command: 'lb -u | grep',
+					response: '123|Active|1|install webbundle:file///path/to/base-theme.war'
+				}
+			]
+		});
+
+		var watch = gulp.watch;
+
+		gulp.watch = function() {
+			assert.equal(arguments[0], 'custom_src_path/**/*');
+
+			var watchCallback = arguments[1];
+
+			var webBundleBuild = path.join(tempPath, '.web_bundle_build');
+
+			assert.notIsEmptyDirectory(webBundleBuild);
+			assert.notIsEmptyDirectory(webBundleBuild, 'css');
+			assert.notIsEmptyDirectory(webBundleBuild, 'images');
+			assert.notIsEmptyDirectory(webBundleBuild, 'js');
+			assert.notIsEmptyDirectory(webBundleBuild, 'templates');
+			assert.notIsEmptyDirectory(webBundleBuild, 'themelets');
+			assert.notIsEmptyDirectory(webBundleBuild, 'WEB-INF');
+
+			gulp.watch = watch;
+
+			try {
+				helper.close(function() {
+					done();
+				});
+			}
+			catch (e) {
+				done();
+			}
+		};
+
+		runSequence('watch', function() {
+			console.log('watching');
 		});
 	});
 });
