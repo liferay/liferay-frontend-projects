dd02dcd3e5061bc9a1c68c38dece7a1787e86dee
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2016-09-19 12:20:37 -0700

    Use similar formatting to liferay-theme-tasks

--- packages/liferay-theme-es2015-hook/index.js
+++ packages/liferay-theme-es2015-hook/index.js
@@ -11,54 +11,38 @@ module.exports = function(gulp, options) {
 	var pathBuild = options.pathBuild;
 	var pathSrc = options.pathSrc;
 
-	gulp.task(
-		'configModules',
-		function(done) {
-			var metaInfPath = path.join(pathBuild, 'META-INF');
+	gulp.task('configModules', function(done) {
+		var metaInfPath = path.join(pathBuild, 'META-INF');
 
-			fs.ensureDirSync(metaInfPath);
+		fs.ensureDirSync(metaInfPath);
 
-			var configGenerator = new ConfigGenerator(
-				{
-					args: [path.join(pathBuild, 'js')],
-					config: '',
-					extension: '',
-					filePattern: '**/*.es.js',
-					format: ['/-/g', '_'],
-					ignorePath: true,
-					moduleConfig: path.join(process.cwd(), 'package.json'),
-					moduleRoot: path.join(pathBuild),
-					output: path.join(metaInfPath, 'config.json')
-				}
-			);
+		var configGenerator = new ConfigGenerator({
+			args: [path.join(pathBuild, 'js')],
+			config: '',
+			extension: '',
+			filePattern: '**/*.es.js',
+			format: ['/-/g', '_'],
+			ignorePath: true,
+			moduleConfig: path.join(process.cwd(), 'package.json'),
+			moduleRoot: path.join(pathBuild),
+			output: path.join(metaInfPath, 'config.json')
+		});
 
-			configGenerator.process().then(
-				function() {
-					done();
-				}
-			);
-		}
-	);
+		configGenerator.process().then(function() {
+			done();
+		});
+	});
 
-	metal.registerTasks(
-		{
-			base: path.join(pathSrc, 'js'),
-			buildAmdDest: path.join(pathBuild),
-			buildSrc: path.join(pathSrc, 'js/**/*.es.js'),
-			gulp: gulp,
-			moduleName: 'js',
-			taskPrefix: 'metal:'
-		}
-	);
+	metal.registerTasks({
+		base: path.join(pathSrc, 'js'),
+		buildAmdDest: path.join(pathBuild),
+		buildSrc: path.join(pathSrc, 'js/**/*.es.js'),
+		gulp: gulp,
+		moduleName: 'js',
+		taskPrefix: 'metal:'
+	});
 
-	gulp.hook(
-		'after:build:src',
-		function(done) {
-			runSequence(
-				'metal:build:amd',
-				'configModules',
-				done
-			);
-		}
-	);
+	gulp.hook('after:build:src', function(done) {
+		runSequence('metal:build:amd', 'configModules', done);
+	});
 };
