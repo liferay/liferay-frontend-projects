1a70b7bd14a177415f66e984bd0bc435273a888d
Dustin Ryerson <dustin.ryerson@liferay.com>
2016-09-22 12:18:19 -0700

    Add Coveralls

---
+++ packages/liferay-theme-finder/gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var coveralls = require('gulp-coveralls');
+var gulp = require('gulp');
+
+gulp.task('coveralls', function() {
+	gulp.src('coverage/**/lcov.info')
+		.pipe(coveralls());
+});
