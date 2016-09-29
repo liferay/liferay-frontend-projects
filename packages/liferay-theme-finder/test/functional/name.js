27f94c5930d327c0ef3d4916162c8d43c3ae605b
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2016-09-29 14:14:37 -0700

    Restructure tests

--- packages/liferay-theme-finder/test/functional/name.js
+++ packages/liferay-theme-finder/test/functional/name.js
@@ -4,20 +4,7 @@ var _ = require('lodash');
 var path = require('path');
 var test = require('ava');
 
-var initCwd = process.cwd();
-var themeFinder;
-
-test.after(function() {
-	process.chdir(initCwd);
-});
-
-test.before(function() {
-	delete require.cache[path.join(__dirname, '../index.js')];
-
-	process.chdir(path.join(__dirname, 'fixtures/themes/7.0/base-theme'));
-
-	themeFinder = require('../index.js');
-});
+var themeFinder = require('../../index');
 
 test.cb('name should retrieve package.json file from npm', function(t) {
 	var pkgName = 'lfr-product-menu-animation-themelet';
@@ -45,31 +32,6 @@ test.cb('name should return error because module is not a liferay theme module',
 	themeFinder.name('generator-liferay-theme', function(err, pkg) {
 		t.true(_.isNull(pkg), 'pkg is null');
 		t.is(err.message, 'Package is not a Liferay theme or themelet module', 'it has appropriate error message');
-
-		t.end();
-	});
-});
-
-test.cb('find should return an object when searching for global modules', function(t) {
-	themeFinder.find(function(themeResults) {
-		t.true(_.isObject(themeResults));
-
-		t.end();
-	});
-});
-
-test.cb('find should return an object when searching for npm modules', function(t) {
-	themeFinder.find({
-		globalModules: false,
-		themelet: true
-	}, function(themeResults) {
-		_.forEach(themeResults, function(item, index) {
-			t.true(_.isObject(item));
-			t.true(_.isObject(item.liferayTheme), 'liferayTheme object is present');
-			t.true(item.keywords.indexOf('liferay-theme') > -1, 'package has liferay-theme keyword');
-		});
-
-		t.true(_.isObject(themeResults));
 
 		t.end();
 	});
