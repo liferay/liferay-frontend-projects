6f6930b1d30510b3f59807a52d8f540cc8d585ea
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2016-10-19 10:12:17 -0700

    Bump update-notifier

--- packages/liferay-theme-tasks/lib/version_control.js
+++ packages/liferay-theme-tasks/lib/version_control.js
@@ -1,28 +1,27 @@
 'use strict';
 
-var _ = require('lodash');
-var chalk = require('gulp-util').colors;
-var updateNotifier = require('update-notifier');
-
 module.exports = function() {
-	var pkg = _.omit(require('../package.json'), ['_shrinkwrap']);
-
 	try {
+		var chalk = require('gulp-util').colors;
+		var updateNotifier = require('update-notifier');
+
 		var notifier = updateNotifier({
-			pkg: pkg,
+			pkg: require('../package.json'),
 			updateCheckInterval: 1000
 		});
 
 		var update = notifier.update;
 
 		if (update) {
-			var seperator = chalk.yellow(new Array(65).join('-'));
+			var message = [
+				'You are running an old version of ' + chalk.cyan(update.name),
+				'Run ' + chalk.cyan('npm update', update.name) + ' to install newest version',
+				'Current version: ' + chalk.green(update.current) + ' Latest version: ' + chalk.green(update.latest)
+			].join('\n');
 
-			console.log(seperator);
-			console.log(' You are running an old version of %s', chalk.cyan(update.name));
-			console.log(' Run %s to install newest version', chalk.cyan('npm update', update.name));
-			console.log(' Current version: %s Latest version: %s', chalk.green(update.current), chalk.green(update.latest));
-			console.log(seperator);
+			notifier.notify({
+				message: message
+			});
 		}
 	}
 	catch (err) {
