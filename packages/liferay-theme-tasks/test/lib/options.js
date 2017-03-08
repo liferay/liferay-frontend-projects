3066cdedad6d84122d96f47fd627f6a6e2a18245
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-03-08 12:45:22 -0800

    Test that liferayTheme props are being added to options

--- packages/liferay-theme-tasks/test/lib/options.js
+++ packages/liferay-theme-tasks/test/lib/options.js
@@ -4,6 +4,17 @@ var argv = require('minimist')(process.argv.slice(2));
 var path = require('path');
 var test = require('ava');
 
+var initCwd = process.cwd();
+var themeFinder;
+
+test.after(function() {
+	process.chdir(initCwd);
+});
+
+test.before(function() {
+	process.chdir(path.join(__dirname, '../fixtures/themes/7.0/base-theme'));
+});
+
 test.before(function() {
 	clearCache();
 });
@@ -17,26 +28,80 @@ test('options should return default options with no config passed', function(t) 
 
 	t.deepEqual(options, {
 		argv: argv,
+		baseTheme: {
+			liferayTheme: {
+				baseTheme: 'styled',
+				rubySass: false,
+				screenshot: '',
+				templateLanguage: 'vm',
+				version: '7.0'
+			},
+			name: 'parent-theme',
+			publishConfig: {
+				tag: '7_0_x'
+			},
+			version: '1.0.0'
+		},
 		pathBuild: './build',
 		pathDist: './dist',
 		pathSrc: './src',
 		rubySass: false,
-		sassOptions: {}
+		sassOptions: {},
+		templateLanguage: 'ftl',
+		themeletDependencies: {
+			'test-themelet': {
+				liferayTheme: {
+					themelet: true,
+					version: '7.0'
+				},
+				name: 'test-themelet',
+				version: '0.0.0'
+			}
+		},
+		version: '7.0'
 	});
 });
 
 test('options should return previously set options if no config is passed', function(t) {
 	var options = require('../../lib/options')({
+		distName: 'dist-name',
 		pathBuild: './custom_build_path'
 	});
 
 	t.deepEqual(options, {
 		argv: argv,
+		baseTheme: {
+			liferayTheme: {
+				baseTheme: 'styled',
+				rubySass: false,
+				screenshot: '',
+				templateLanguage: 'vm',
+				version: '7.0'
+			},
+			name: 'parent-theme',
+			publishConfig: {
+				tag: '7_0_x'
+			},
+			version: '1.0.0'
+		},
+		distName: 'dist-name',
 		pathBuild: './custom_build_path',
 		pathDist: './dist',
 		pathSrc: './src',
 		rubySass: false,
-		sassOptions: {}
+		sassOptions: {},
+		templateLanguage: 'ftl',
+		themeletDependencies: {
+			'test-themelet': {
+				liferayTheme: {
+					themelet: true,
+					version: '7.0'
+				},
+				name: 'test-themelet',
+				version: '0.0.0'
+			}
+		},
+		version: '7.0'
 	});
 
 	var secondOptions = require('../../lib/options')();
