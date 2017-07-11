0616ccf3b269c54fa37a7cabcf34200f1e887cd6
Pier Paolo Ramon <ramonpierre@gmail.com>
2017-07-11 14:01:39 +0200

    Add failing tests on packages that end with '.js'

--- packages/babel-plugin-normalize-requires/src/__tests__/index.test.js
+++ packages/babel-plugin-normalize-requires/src/__tests__/index.test.js
@@ -2,6 +2,30 @@ import * as babel from 'babel-core';
 import plugin from '../index';
 
 describe('when requiring local modules', () => {
+	it('keeps trailing ".js" from package names', () => {
+		const source = `
+	    require('a-package.js')
+	    `;
+
+		const { code } = babel.transform(source, {
+			plugins: [plugin],
+		});
+
+		expect(code).toMatchSnapshot();
+	});
+
+	it('keeps trailing ".js" from package names', () => {
+		const source = `
+	    require('@some-scope/a-package.js')
+	    `;
+
+		const { code } = babel.transform(source, {
+			plugins: [plugin],
+		});
+
+		expect(code).toMatchSnapshot();
+	});
+
 	it('removes trailing ".js" from module names', () => {
 		const source = `
 	    require('./a-module.js')
