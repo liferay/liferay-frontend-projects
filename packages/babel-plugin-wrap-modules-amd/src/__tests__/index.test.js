ac16f300fdbcefe2180b3002ed12ec2863e25079
Iván Zaera Avellón <izaera@gmail.com>
2017-07-11 12:41:32 +0200

    Add babel-plugin-wrap-modules-amd

---
+++ packages/babel-plugin-wrap-modules-amd/src/__tests__/index.test.js
@@ -0,0 +1,18 @@
+import * as babel from 'babel-core';
+import plugin from '../index';
+
+it('correctly wraps modules', () => {
+	const source = `
+	console.log('Say something');
+	if (1 == 0) {
+		console.log('Something broke in the Matrix');
+	}
+	module.exports = 'All OK';
+	`;
+
+	const { code } = babel.transform(source, {
+		plugins: [plugin],
+	});
+
+	expect(code).toMatchSnapshot();
+});
---
+++ packages/babel-plugin-wrap-modules-amd/src/index.js
@@ -0,0 +1,71 @@
+import template from 'babel-template';
+
+const buildDefine = template(`
+     define(DEPS, function(module, exports, require) {
+ 	    SOURCE
+     })
+ `);
+
+/**
+  * @return {object} a babel visitor
+  */
+export default function({ types: t }) {
+	const wrapVisitor = {
+		Identifier(path, { dependencies }) {
+			const node = path.node;
+
+			if (node.name === 'require') {
+				const parent = path.parent;
+
+				if (
+					t.isCallExpression(parent) &&
+					parent.callee === node &&
+					parent.arguments.length == 1
+				) {
+					const argument0 = parent.arguments[0];
+
+					if (t.isLiteral(argument0)) {
+						const moduleName = argument0.value;
+
+						dependencies[moduleName] = moduleName;
+					}
+				}
+			}
+		},
+	};
+
+	return {
+		visitor: {
+			Program: {
+				enter(path, state) {
+					state.dependencies = {};
+				},
+				exit(path, { opts, dependencies }) {
+					// We must traverse the AST again because some plugins emit
+					// their require() calls after exiting Program node :-(
+					path.traverse(wrapVisitor, { opts, dependencies });
+
+					const node = path.node;
+					const body = node.body;
+
+					dependencies = Object.keys(dependencies).map(
+						dep => `'${dep}'`,
+					);
+
+					const buildDeps = template(`[
+                         'module', 'exports', 'require' 
+                         ${dependencies.length > 0 ? ',' : ''} 
+                         ${dependencies.join()}
+                     ]`);
+
+					node.body = [
+						buildDefine({
+							SOURCE: body,
+							DEPS: buildDeps(),
+						}),
+					];
+				},
+			},
+		},
+	};
+}
