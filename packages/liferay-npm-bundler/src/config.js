7878c796bdba09de649fbfec003557245cbe18b5
Iván Zaera Avellón <izaera@gmail.com>
2017-07-12 11:20:00 +0200

    Add liferay-npm-bundler project

---
+++ packages/liferay-npm-bundler/src/config.js
@@ -0,0 +1,143 @@
+import { getPackageDir } from 'liferay-npm-build-tools-common/lib/packages';
+import readJsonSync from 'read-json-sync';
+import resolveModule from 'resolve';
+
+let pluginsBaseDir = '.';
+let config = loadConfig();
+
+/**
+ * Load configuration at startup.
+ * @return {Object} the configuration object
+ */
+function loadConfig() {
+	// Load base configuration
+	let config = readJsonSync('.npmbundlerrc');
+
+	// Apply preset if necessary
+	if (config.preset) {
+		const presetFile = resolveModule.sync(config.preset, {
+			basedir: '.',
+		});
+
+		// Merge preset with base configuration
+		config = Object.assign(readJsonSync(presetFile), config);
+		pluginsBaseDir = getPackageDir(presetFile);
+	}
+
+	return config;
+}
+
+/**
+ * Require a module using the configured plugins directory.
+ * @param {String} module a module name
+ * @return {Object} the required module object
+ */
+function configRequire(module) {
+	const pluginFile = resolveModule.sync(module, {
+		basedir: pluginsBaseDir,
+	});
+
+	return require(pluginFile);
+}
+
+/**
+ * Get the configured output directory
+ * @return {String} the directory path 
+ */
+export function getOutputDir() {
+	return config['output'] || 'build/resources/main/META-INF/resources';
+}
+
+/**
+ * Get the configured file exclusions for a given package.
+ * @param {Object} pkg the package descriptor hash containing id, name, version
+ *        and dir fields
+ * @return {Array} an array of glob expressions
+ */
+export function getExclusions(pkg) {
+	let exclusions = config.exclude || {};
+
+	exclusions = exclusions[pkg.id] || exclusions[pkg.name] || [];
+
+	return exclusions;
+}
+
+/**
+ * Load Babel plugins from a given array of presets and plugins.
+ * @param {Array} presets an array of Babel preset names as defined by .babelrc
+ * @param {Array} plugins an array of Babel plugins names as defined by .babelrc
+ * @return {Array} the instantiated Babel plugins
+ */
+export function loadBabelPlugins(presets, plugins) {
+	// TODO: if plugins have config decide what to do with it
+	return []
+		.concat(
+			...presets.map(preset => {
+				let presetModule;
+
+				try {
+					presetModule = configRequire(preset);
+				} catch (err) {
+					presetModule = configRequire(`babel-preset-${preset}`);
+				}
+
+				return presetModule.default().plugins;
+			}),
+		)
+		.concat(...plugins);
+}
+
+/**
+ * Get the liferay-nmp-bundler plugins for a given package.
+ * @param {String} phase 'pre' or 'post'
+ * @param {Object} pkg the package descriptor hash containing id, name, version
+ *        and dir fields
+ * @return {Array} the instantiated Babel plugins
+ */
+export function getPlugins(phase, pkg) {
+	const pluginsKey = phase === 'pre' ? 'plugins' : 'post-plugins';
+
+	let plugins = [];
+
+	if (config[pkg.id] && config[pkg.id][pluginsKey]) {
+		plugins = config[pkg.id][pluginsKey];
+	} else if (config['*'] && config['*'][pluginsKey]) {
+		plugins = config['*'][pluginsKey];
+	}
+
+	return plugins.map(pluginName => {
+		let pluginConfig = {};
+
+		if (Array.isArray(pluginName)) {
+			pluginConfig = pluginName[1];
+			pluginName = pluginName[0];
+		}
+
+		const pluginModule = configRequire(
+			`liferay-npm-bundler-plugin-${pluginName}`,
+		);
+
+		return {
+			run: pluginModule.default,
+			config: pluginConfig,
+		};
+	});
+}
+
+/**
+ * Get Babel config for a given package
+ * @param {Object} pkg the package descriptor hash containing id, name, version
+ *        and dir fields
+ * @return {Object} a Babel configuration object as defined by its API
+ */
+export function getBabelConfig(pkg) {
+	let babelConfig = {};
+
+	if (config[pkg.id] && config[pkg.id]['.babelrc']) {
+		babelConfig = config[pkg.id]['.babelrc'];
+	} else if (config['*'] && config['*']['.babelrc']) {
+		babelConfig = config['*']['.babelrc'];
+	}
+
+	return babelConfig;
+}
---
+++ packages/liferay-npm-bundler/src/index.js
@@ -0,0 +1,200 @@
+import * as babel from 'babel-core';
+import cpFile from 'cp-file';
+import fs from 'fs';
+import globby from 'globby';
+import { mkdirp } from 'liferay-build-tools-util';
+import path from 'path';
+import readJsonSync from 'read-json-sync';
+
+import * as config from './config';
+import { getPackageDependencies } from './dependencies';
+
+/**
+ * Default entry point for the liferay-npm-bundler.
+ * @param {Array} args the CLI arguments
+ * @return {void}
+ */
+export default function(args) {
+	let promises = [];
+
+	const outputDir = config.getOutputDir();
+
+	// Create work directories
+	mkdirp(`${outputDir}/node_modules`);
+
+	// Copy project's package.json
+	promises.push(copyRootPackageJson(outputDir));
+
+	// Grab NPM dependencies
+	let pkgs = getPackageDependencies('.');
+	pkgs = Object.keys(pkgs).map(id => pkgs[id]);
+	pkgs = pkgs.filter(pkg => pkg.dir != '.');
+
+	// Process NPM dependencies
+	const start = new Date().getTime();
+
+	pkgs.forEach(pkg => {
+		console.log(`Bundling ${pkg.id}`);
+
+		const outPkgDir = `${outputDir}/node_modules/${pkg.id.replace(
+			'/',
+			'%2F',
+		)}`;
+
+		mkdirp(outPkgDir);
+
+		promises.push(
+			copyPackage(pkg, outPkgDir)
+				.then(() => (pkg.dir = outPkgDir))
+				.then(() => processPackage('pre', pkg))
+				.then(() => runBabel(pkg))
+				.then(() => processPackage('post', pkg))
+				.then(() => console.log(`Bundled ${pkg.id}`)),
+		);
+	});
+
+	Promise.all(promises)
+		.then(() =>
+			console.log(
+				`Bundled all dependencies in ${new Date().getTime() -
+					start} milliseconds`,
+			),
+		)
+		.catch(function(err) {
+			console.log(err);
+			process.exit(1);
+		});
+}
+
+/**
+ * Copy project root package.json file to output directory.
+ * @param {String} outputDir the output directory path
+ * @return {Promise} a Promise fulfilled when the copy has been finished
+ */
+function copyRootPackageJson(outputDir) {
+	return cpFile('package.json', `${outputDir}/package.json`);
+}
+
+/**
+ * Copy an NPM package to output directory.
+ * @param {Object} pkg the package descriptor hash containing id, name, version
+ *        and dir fields
+ * @param {String} dir the output directory path
+ * @return {Promise} a Promise fulfilled when the copy has been finished
+ */
+function copyPackage(pkg, dir) {
+	const exclusions = config.getExclusions(pkg);
+
+	const globs = [`${pkg.dir}/**/*`, `!${pkg.dir}/node_modules/**/*`].concat(
+		exclusions.map(exclusion => `!${pkg.dir}/${exclusion}`),
+	);
+
+	return globby(globs).then(paths => {
+		paths = paths.map(path => path.substring(pkg.dir.length + 1));
+
+		const promises = paths.map(path =>
+			cpFile(`${pkg.dir}/${path}`, `${dir}/${path}`).catch(err => {}),
+		);
+
+		return Promise.all(promises);
+	});
+}
+
+/**
+ * Process an NPM package with the configured liferay-nmp-bundler plugins. This
+ * function is called two times (known as phases) per package: one before Babel 
+ * runs and one after.
+ * @param {String} phase 'pre' or 'post' depending on what phase we are in
+ * @param {Object} pkg the package descriptor hash containing id, name, version
+ *        and dir fields
+ * @return {Promise} a Promise fulfilled when the process has been finished
+ */
+function processPackage(phase, pkg) {
+	return new Promise((resolve, reject) => {
+		const pkgJsonPath = `${pkg.dir}/package.json`;
+		const pkgJson = readJsonSync(pkgJsonPath);
+
+		let state = {
+			pkgJson: pkgJson,
+		};
+
+		try {
+			config.getPlugins(phase, pkg).forEach(plugin => {
+				plugin.run({ pkg, config: plugin.config }, state);
+			});
+		} catch (err) {
+			reject(err);
+		}
+
+		fs.writeFileSync(pkgJsonPath, JSON.stringify(state.pkgJson, '', 2));
+
+		resolve();
+	});
+}
+
+/**
+ * Run Babel on a package.
+ * @param {Object} pkg the package descriptor hash containing id, name, version
+ *        and dir fields
+ * @return {Promise} a Promise fulfilled when the process has been finished
+ */
+function runBabel(pkg) {
+	const babelConfig = config.getBabelConfig(pkg);
+
+	// Intercept presets and plugins to load them from here
+	babelConfig.plugins = config.loadBabelPlugins(
+		babelConfig.presets || [],
+		babelConfig.plugins || [],
+	);
+	babelConfig.presets = [];
+
+	// Tune babel config
+	babelConfig.babelrc = false;
+	babelConfig.only = '**/*';
+	if (babelConfig.sourceMaps === undefined) {
+		babelConfig.sourceMaps = true;
+	}
+
+	// Run babel through it
+	return globby([`${pkg.dir}/**/*.js`]).then(filePaths => {
+		const promises = filePaths.map(
+			filePath =>
+				new Promise((resolve, reject) => {
+					babel.transformFile(
+						filePath,
+						Object.assign(
+							{
+								filenameRelative: filePath,
+							},
+							babelConfig,
+						),
+						(err, result) => {
+							if (err) {
+								console.log(
+									`Error processing file: ${filePath}`,
+								);
+								reject(err);
+							} else {
+								const fileName = path.basename(filePath);
+
+								fs.writeFileSync(
+									filePath,
+									`${result.code}\n` +
+										`//# sourceMappingURL=${fileName}.map`,
+								);
+
+								fs.writeFileSync(
+									`${filePath}.map`,
+									JSON.stringify(result.map),
+								);
+
+								resolve();
+							}
+						},
+					);
+				}),
+		);
+
+		return Promise.all(promises);
+	});
+}
