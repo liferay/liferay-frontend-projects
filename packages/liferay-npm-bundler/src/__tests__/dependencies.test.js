c6411d6063d968b22c05e880927a732857d1493e
Iván Zaera Avellón <izaera@gmail.com>
2017-07-12 11:23:11 +0200

    Add tests for liferay-npm-bundler

---
+++ packages/liferay-npm-bundler/src/__tests__/dependencies.test.js
@@ -0,0 +1,100 @@
+import { getPackageDependencies } from '../dependencies';
+
+expect.extend({
+	toMatchDependencies(deps, ...pkgIds) {
+		let missingDeps = [];
+		let extraDeps = [];
+		let invalidDepFields = {};
+
+		pkgIds.forEach(pkgId => {
+			const dep = deps[pkgId];
+
+			if (!dep) {
+				missingDeps.push(pkgId);
+			} else {
+				const pkgIdParts = pkgId.split('@');
+				const pkgName = pkgIdParts[0];
+				const pkgVersion = pkgIdParts[1];
+				let invalidFields = invalidDepFields[pkgId] || {};
+
+				if (dep.id != pkgId) {
+					invalidFields.id = `${dep.id} (expected: ${pkgId})`;
+				}
+
+				if (dep.name != pkgName) {
+					invalidFields.name = `${dep.name} (expected: ${pkgName})`;
+				}
+
+				if (dep.version != pkgVersion) {
+					invalidFields.version =
+						`${dep.version} ` + `(expected: ${pkgVersion})`;
+				}
+
+				if (
+					dep.dir != '.' &&
+					!dep.dir.endsWith(`/node_modules/${pkgName}`)
+				) {
+					invalidFields.dir =
+						`${dep.dir} ` +
+						`(expected: .../node_modules/${pkgName})`;
+				}
+
+				if (Object.keys(invalidFields).length > 0) {
+					invalidDepFields[pkgId] = invalidFields;
+				}
+			}
+		});
+
+		Object.keys(deps).forEach(depId => {
+			if (pkgIds.indexOf(depId) == -1) {
+				extraDeps.push(depId);
+			}
+		});
+
+		let pass = true;
+		let message = '';
+
+		if (missingDeps.length > 0) {
+			message += `\nRequired dependencies missing: ${missingDeps}\n`;
+			pass = false;
+		}
+
+		if (extraDeps.length > 0) {
+			message += `\nNot required dependencies found: ${extraDeps}\n`;
+			pass = false;
+		}
+
+		if (Object.keys(invalidDepFields).length > 0) {
+			message += `\nInvalid dependency fields: ${JSON.stringify(
+				invalidDepFields,
+				'',
+				2,
+			)}\n`;
+			pass = false;
+		}
+
+		return {
+			message,
+			pass,
+		};
+	},
+});
+
+beforeAll(() => {
+	process.chdir('./packages/liferay-npm-bundler/src/__tests__');
+});
+
+afterAll(() => {
+	process.chdir('../../../..');
+});
+
+it.only('loads project dependencies correctly', () => {
+	const deps = getPackageDependencies('.');
+
+	expect(deps).toMatchDependencies(
+		'test-project@1.0.0',
+		'test-project-dep-0@1.0.0',
+		'test-project-dep-1@1.0.0',
+		'test-project-dep-0@0.1.0',
+	);
+});
