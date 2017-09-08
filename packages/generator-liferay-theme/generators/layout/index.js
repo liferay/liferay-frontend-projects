2c8ca77e7a60c161605cd30b76cc208983340281
Robert Frampton <robert.frampton@liferay.com>
2017-09-08 14:37:46 -0700

    Merge pull request #22 from alexandervalencia/18
    Issue #18 - Fixes layout template loading incorrectly in a theme

--- packages/generator-liferay-theme/generators/layout/index.js
+++ packages/generator-liferay-theme/generators/layout/index.js
@@ -29,6 +29,8 @@ var layoutGeneratorPrototype = _.merge(liferayThemeGeneratorPrototype, {
 
 			var done = this.async();
 
+			instance.rootDir = instance.destinationRoot();
+
 			if (this.layoutDirName !== _.last(this.destinationRoot().split(path.sep))) {
 				var layoutDirName = this.layoutDirName;
 
@@ -64,16 +66,26 @@ var layoutGeneratorPrototype = _.merge(liferayThemeGeneratorPrototype, {
 		var thumbnailDestination = this.themeLayout ? this.thumbnailFilename : path.join('docroot', this.thumbnailFilename);
 		var templateDestination = this.themeLayout ? this.templateFilename : path.join('docroot', this.templateFilename);
 
+		var xmlDestination = 'docroot/WEB-INF/liferay-layout-templates.xml';
+
+		this.templateDestination = '';
+
 		this.fs.copy(this.templatePath('docroot/layout.png'), this.destinationPath(thumbnailDestination));
 		this.template('docroot/layout.tpl', templateDestination, this);
 
 		if (!this.themeLayout) {
 			this.fs.copy(this.templatePath('gitignore'), this.destinationPath('.gitignore'));
 			this.template('_package.json', 'package.json', this);
-			this.template('docroot/WEB-INF/liferay-layout-templates.xml', 'docroot/WEB-INF/liferay-layout-templates.xml', this);
 			this.template('docroot/WEB-INF/liferay-plugin-package.properties', 'docroot/WEB-INF/liferay-plugin-package.properties', this);
 			this.template('gulpfile.js', 'gulpfile.js', this);
 		}
+		else {
+			this.templateDestination = path.join('/layouttpl/custom/', this.layoutDirName);
+
+			xmlDestination = path.join(instance.rootDir, 'src/WEB-INF/liferay-layout-templates.xml');
+		}
+
+		this.template('docroot/WEB-INF/liferay-layout-templates.xml', xmlDestination, this);
 
 		if (!this.options['skip-creation']) {
 			var done = this.async();
