fdc79d3d3662896e7a4bb8c5430836db50dbbc0b
Chema Balsas <jose.balsas@liferay.com>
2017-11-09 08:59:22 -0800

    Simplifies copy path

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,9 +1,6 @@
 const fs = require('fs');
 const { COPYFILE_EXCL } = fs.constants;
 
-const path = require('path');
-const cwd = path.resolve();
-
-fs.copyFile('.eslintrc', path.resolve(cwd, '.eslintrc'), COPYFILE_EXCL, (err) => {
+fs.copyFile('.eslintrc', '../../.eslintrc', COPYFILE_EXCL, (err) => {
     if (err) console.log('Preserving .eslintrc file found at ' + cwd);
 });
