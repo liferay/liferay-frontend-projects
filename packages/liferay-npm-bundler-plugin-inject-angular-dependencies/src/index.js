1d3eff2dd208b57e314bcc11c6cc3855b2880be0
Iván Zaera Avellón <izaera@gmail.com>
2017-12-18 12:59:20 +0100

    Fix @angular/animations handling in inject-angular-dependencies plugin and make room for extensibility through configuration.
    Fixes #66

--- packages/liferay-npm-bundler-plugin-inject-angular-dependencies/src/index.js
+++ packages/liferay-npm-bundler-plugin-inject-angular-dependencies/src/index.js
@@ -1,30 +1,52 @@
 import fs from 'fs';
+import {getPackageTargetDir} from 'liferay-npm-build-tools-common/lib/packages';
 import path from 'path';
 import readJsonSync from 'read-json-sync';
+
+const dependenciesMap = {
+	'@angular/forms': ['rxjs'],
+	'@angular/platform-browser': ['@angular/animations'],
+};
 
 /**
  * @return {void}
  */
-export default function({pkg}, {pkgJson}) {
-	if (pkg.name !== '@angular/forms') {
+export default function({pkg, config}, {pkgJson}) {
+	const deps = Object.assign(
+		{},
+		dependenciesMap,
+		config.dependenciesMap || {}
+	)[pkg.name];
+
+	if (!deps) {
 		return;
 	}
 
 	pkgJson.dependencies = pkgJson.dependencies || {};
 
-	if (!pkgJson.dependencies['rxjs']) {
-		const nodeModulesDir = path.resolve(path.join(pkg.dir, '..'));
+	deps.forEach(dep => {
+		let dirPrefix;
 
-		for (let dir of fs.readdirSync(nodeModulesDir)) {
-			if (dir.startsWith('rxjs@')) {
-				const rxjsPkgJson = readJsonSync(
-					path.join(nodeModulesDir, dir, 'package.json')
-				);
+		if (dep.indexOf('/') != -1) {
+			dirPrefix = `${getPackageTargetDir(dep)}@`;
+		} else {
+			dirPrefix = `${dep}@`;
+		}
 
-				pkgJson.dependencies['rxjs'] = rxjsPkgJson.version;
+		if (!pkgJson.dependencies[dep]) {
+			const nodeModulesDir = path.resolve(path.join(pkg.dir, '..'));
 
-				break;
+			for (let dir of fs.readdirSync(nodeModulesDir)) {
+				if (dir.startsWith(dirPrefix)) {
+					const depPkgJson = readJsonSync(
+						path.join(nodeModulesDir, dir, 'package.json')
+					);
+
+					pkgJson.dependencies[dep] = depPkgJson.version;
+
+					break;
+				}
 			}
 		}
-	}
+	});
 }
