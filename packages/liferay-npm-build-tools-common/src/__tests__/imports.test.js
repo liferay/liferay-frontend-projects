fa8ad6d6a57a0ee8d5c62db25db28ccbc6a18bcf
Iván Zaera Avellón <izaera@gmail.com>
2018-07-03 13:56:14 +0200

    Define a new normalization format for imports config
    Fixes #157

--- packages/liferay-npm-build-tools-common/src/__tests__/imports.test.js
+++ packages/liferay-npm-build-tools-common/src/__tests__/imports.test.js
@@ -1,24 +1,50 @@
 import {normalizeImportsConfig, unrollImportsConfig} from '../imports';
 
-it('normalizeImportsConfig works', () => {
-	const imports = normalizeImportsConfig({
-		'provider1': {
-			dep1: '^1.0.0',
-			dep2: '^2.0.0',
-		},
-		'provider2': {
-			dep9: '^9.0.0',
-			dep8: '^8.0.0',
-		},
-		'': {
-			depA: '3.0.0',
-		},
-		'depB': {
-			'/': '4.0.0',
-		},
+describe('normalizeImportsConfig()', () => {
+	it('works for empty string format normalization', () => {
+		const imports = normalizeImportsConfig({
+			'provider1': {
+				dep1: '^1.0.0',
+				dep2: '^2.0.0',
+			},
+			'provider2': {
+				dep9: '^9.0.0',
+				dep8: '^8.0.0',
+			},
+			'': {
+				depA: '3.0.0',
+			},
+			'depB': {
+				'/': '4.0.0',
+			},
+		});
+
+		expect(imports).toMatchSnapshot();
 	});
 
-	expect(imports).toMatchSnapshot();
+	it('works for slash format normalization', () => {
+		const imports = normalizeImportsConfig(
+			{
+				'provider1': {
+					dep1: '^1.0.0',
+					dep2: '^2.0.0',
+				},
+				'provider2': {
+					dep9: '^9.0.0',
+					dep8: '^8.0.0',
+				},
+				'': {
+					depA: '3.0.0',
+				},
+				'depB': {
+					'/': '4.0.0',
+				},
+			},
+			true
+		);
+
+		expect(imports).toMatchSnapshot();
+	});
 });
 
 it('unrollImportsConfig works', () => {
