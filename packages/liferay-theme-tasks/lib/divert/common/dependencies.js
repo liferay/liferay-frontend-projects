e2226a9b042e945522bb610c095f53eb202a3647
Chema Balsas <jose.balsas@liferay.com>
2018-07-03 02:32:33 +0200

    Adds the same special handling for admin as classic already has

--- packages/liferay-theme-tasks/lib/divert/common/dependencies.js
+++ packages/liferay-theme-tasks/lib/divert/common/dependencies.js
@@ -10,6 +10,7 @@ const {
 const themeUtil = require('../../util');
 
 const moduleNamesMap = {
+	admin: 'liferay-frontend-theme-admin-web',
 	classic: 'liferay-frontend-theme-classic-web',
 	mixins: 'liferay-frontend-common-css',
 	styled: 'liferay-frontend-theme-styled',
@@ -39,7 +40,7 @@ function getBaseThemeDependencies(baseThemePath) {
 		dependencies.push(path.resolve(baseThemePath, 'src/**/*'));
 
 		return getBaseThemeDependencies(baseThemePath, dependencies);
-	} else if (baseTheme === 'styled' || baseTheme === 'classic') {
+	} else if (baseTheme === 'styled' || baseTheme === 'classic' || baseTheme === 'admin') {
 		dependencies.splice(
 			1,
 			0,
@@ -55,6 +56,17 @@ function getBaseThemeDependencies(baseThemePath) {
 				0,
 				path.join(
 					themeUtil.resolveDependency(getDependencyName('classic')),
+					baseThemeGlob
+				)
+			);
+		}
+
+		if (baseTheme === 'admin') {
+			dependencies.splice(
+				2,
+				0,
+				path.join(
+					themeUtil.resolveDependency(getDependencyName('admin')),
 					baseThemeGlob
 				)
 			);
