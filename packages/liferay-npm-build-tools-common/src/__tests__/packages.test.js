b6ce2c388a75ce86923ef3be8f66034dbd89c60f
Iván Zaera Avellón <izaera@gmail.com>
2018-07-20 12:02:58 +0200

    Create a helper to resolve module names into file paths
    Fixes #169

---
+++ packages/liferay-npm-build-tools-common/src/__tests__/packages.test.js
@@ -0,0 +1,72 @@
+import path from 'path';
+
+import * as pkgs from '../packages';
+
+describe('resolveModuleFile', () => {
+	const pkgDir = path.join(
+		__dirname,
+		'__fixtures__',
+		'packages',
+		'a-package'
+	);
+
+	it('works for file names with extension', () => {
+		expect(pkgs.resolveModuleFile(pkgDir, 'no-package-json/index.js')).toBe(
+			'no-package-json/index.js'
+		);
+
+		expect(
+			pkgs.resolveModuleFile(pkgDir, './no-package-json/index.js')
+		).toBe('no-package-json/index.js');
+	});
+
+	it('works for file names without .js extension', () => {
+		expect(pkgs.resolveModuleFile(pkgDir, 'no-package-json/index')).toBe(
+			'no-package-json/index.js'
+		);
+
+		expect(pkgs.resolveModuleFile(pkgDir, './no-package-json/index')).toBe(
+			'no-package-json/index.js'
+		);
+	});
+
+	it('works for directories without package.json file', () => {
+		expect(pkgs.resolveModuleFile(pkgDir, 'no-package-json')).toBe(
+			'no-package-json/index.js'
+		);
+
+		expect(pkgs.resolveModuleFile(pkgDir, './no-package-json')).toBe(
+			'no-package-json/index.js'
+		);
+	});
+
+	it('works for directories with a package.json file', () => {
+		expect(
+			pkgs.resolveModuleFile(
+				pkgDir,
+				'with-package-json/no-dot/no-extension'
+			)
+		).toBe('with-package-json/no-dot/no-extension/file.js');
+
+		expect(
+			pkgs.resolveModuleFile(
+				pkgDir,
+				'with-package-json/no-dot/with-extension'
+			)
+		).toBe('with-package-json/no-dot/with-extension/file.js');
+
+		expect(
+			pkgs.resolveModuleFile(
+				pkgDir,
+				'with-package-json/with-dot/no-extension'
+			)
+		).toBe('with-package-json/with-dot/no-extension/file.js');
+
+		expect(
+			pkgs.resolveModuleFile(
+				pkgDir,
+				'with-package-json/with-dot/with-extension'
+			)
+		).toBe('with-package-json/with-dot/with-extension/file.js');
+	});
+});
