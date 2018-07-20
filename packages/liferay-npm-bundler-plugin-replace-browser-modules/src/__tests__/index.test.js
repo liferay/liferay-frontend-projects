238803d81a4f532d5812085e74fc7ac1b9ff1db8
Iván Zaera Avellón <izaera@gmail.com>
2018-07-20 12:03:29 +0200

    Use the resolveModuleFile helper to replace browser modules
    Fixes #169

--- packages/liferay-npm-bundler-plugin-replace-browser-modules/src/__tests__/index.test.js
+++ packages/liferay-npm-bundler-plugin-replace-browser-modules/src/__tests__/index.test.js
@@ -1,6 +1,6 @@
-import cpFile from 'cp-file';
-import * as fs from 'fs';
+import * as fs from 'fs-extra';
 import PluginLogger from 'liferay-npm-build-tools-common/lib/plugin-logger';
+import rcopy from 'recursive-copy';
 import plugin from '../index';
 
 // Package descriptor used in tests
@@ -8,45 +8,30 @@ const pkg = {
 	id: 'package@1.0.0',
 	name: 'package',
 	version: '1.0.0',
-	dir:
-		`${process.cwd()}/packages/` +
-		`liferay-npm-bundler-plugin-replace-browser-modules/src/__tests__`,
+	dir: `${__dirname}/pkg`,
 };
-
-// Package files used in tests
-const pkgFiles = [
-	'test-2.js.file',
-	'test-browser-2.js.file',
-	'test-browser.js.file',
-	'test-main.js.file',
-	'test.js.file',
-];
 
 let log;
 
 // Restore package status prior to running each test
-beforeEach(() => {
+beforeEach(done => {
 	log = new PluginLogger();
-	pkgFiles.forEach(file => {
-		cpFile.sync(`${pkg.dir}/sources/${file}`, `${pkg.dir}/${file}`);
-	});
+
+	rcopy(`${__dirname}/__fixtures__`, pkg.dir, {overwrite: true}).then(done);
 });
 
 // Delete result files after running each test
 afterEach(() => {
-	pkgFiles.forEach(file => {
-		try {
-			fs.unlinkSync(`${pkg.dir}/${file}`);
-		} catch (err) {}
-	});
+	fs.emptyDirSync(pkg.dir);
+	fs.rmdirSync(pkg.dir);
 });
 
 it('logs results correctly', () => {
 	const pkgJson = {
 		name: pkg.name,
 		version: pkg.version,
-		main: 'test-main.js.file',
-		browser: 'test-browser.js.file',
+		main: 'test-main.js',
+		browser: 'test-browser.js',
 	};
 
 	plugin({pkg, log}, {pkgJson});
@@ -58,14 +43,29 @@ it('replaces main file with browser file', () => {
 	const pkgJson = {
 		name: pkg.name,
 		version: pkg.version,
-		main: 'test-main.js.file',
-		browser: 'test-browser.js.file',
+		main: 'test-main.js',
+		browser: 'test-browser.js',
 	};
 
 	plugin({pkg, log}, {pkgJson});
 
 	expect(
-		fs.readFileSync(`${pkg.dir}/test-main.js.file`).toString()
+		fs.readFileSync(`${pkg.dir}/test-main.js`).toString()
+	).toMatchSnapshot();
+});
+
+it('replaces main directory with browser file', () => {
+	const pkgJson = {
+		name: pkg.name,
+		version: pkg.version,
+		main: './dir',
+		browser: 'test-browser.js',
+	};
+
+	plugin({pkg, log}, {pkgJson});
+
+	expect(
+		fs.readFileSync(`${pkg.dir}/dir/index.js`).toString()
 	).toMatchSnapshot();
 });
 
@@ -73,14 +73,14 @@ it('works with unpkg field too', () => {
 	const pkgJson = {
 		name: pkg.name,
 		version: pkg.version,
-		main: 'test-main.js.file',
-		unpkg: 'test-browser.js.file',
+		main: 'test-main.js',
+		unpkg: 'test-browser.js',
 	};
 
 	plugin({pkg, log}, {pkgJson});
 
 	expect(
-		fs.readFileSync(`${pkg.dir}/test-main.js.file`).toString()
+		fs.readFileSync(`${pkg.dir}/test-main.js`).toString()
 	).toMatchSnapshot();
 });
 
@@ -88,14 +88,14 @@ it('works with jsdelivr field too', () => {
 	const pkgJson = {
 		name: pkg.name,
 		version: pkg.version,
-		main: 'test-main.js.file',
-		jsdelivr: 'test-browser.js.file',
+		main: 'test-main.js',
+		jsdelivr: 'test-browser.js',
 	};
 
 	plugin({pkg, log}, {pkgJson});
 
 	expect(
-		fs.readFileSync(`${pkg.dir}/test-main.js.file`).toString()
+		fs.readFileSync(`${pkg.dir}/test-main.js`).toString()
 	).toMatchSnapshot();
 });
 
@@ -104,18 +104,22 @@ it('replaces server files with browser files', () => {
 		name: pkg.name,
 		version: pkg.version,
 		browser: {
-			'test-browser.js.file': 'test.js.file',
-			'test-browser-2.js.file': 'test-2.js.file',
+			'dir': 'test-main.js',
+			'test-browser.js': 'test.js',
+			'test-browser-2.js': 'test-2.js',
 		},
 	};
 
 	plugin({pkg, log}, {pkgJson});
 
 	expect(
-		fs.readFileSync(`${pkg.dir}/test-browser.js.file`).toString()
+		fs.readFileSync(`${pkg.dir}/dir/index.js`).toString()
 	).toMatchSnapshot();
 	expect(
-		fs.readFileSync(`${pkg.dir}/test-browser-2.js.file`).toString()
+		fs.readFileSync(`${pkg.dir}/test-browser.js`).toString()
+	).toMatchSnapshot();
+	expect(
+		fs.readFileSync(`${pkg.dir}/test-browser-2.js`).toString()
 	).toMatchSnapshot();
 });
 
@@ -125,14 +129,14 @@ it('does replace ignored modules with empty objects', () => {
 		version: pkg.version,
 		main: 'main.js',
 		browser: {
-			'test-browser.js.file': false,
+			'test-browser.js': false,
 		},
 	};
 
 	plugin({pkg, log}, {pkgJson});
 
 	expect(
-		fs.readFileSync(`${pkg.dir}/test-browser.js.file`).toString()
+		fs.readFileSync(`${pkg.dir}/test-browser.js`).toString()
 	).toMatchSnapshot();
 });
 
