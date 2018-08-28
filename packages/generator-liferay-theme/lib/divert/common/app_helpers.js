84f2a9bc398901f0ab23798721be2132800d886c
Chema Balsas <jose.balsas@liferay.com>
2018-08-28 15:45:21 +0200

    Prepares v8.0.0-beta.2

--- packages/generator-liferay-theme/lib/divert/common/app_helpers.js
+++ packages/generator-liferay-theme/lib/divert/common/app_helpers.js
@@ -2,8 +2,8 @@ var chalk = require('chalk');
 
 function _getDevDependencies() {
 	return `\t"gulp": "3.9.1",
-    \t"liferay-theme-tasks": "8.0.0-beta.1",
-    \t"liferay-theme-deps-7.0": "8.0.0-beta.1"`;
+    \t"liferay-theme-tasks": "8.0.0-beta.2",
+    \t"liferay-theme-deps-7.0": "8.0.0-beta.2"`;
 }
 
 function _getTemplateLanguageChoices(answers) {
