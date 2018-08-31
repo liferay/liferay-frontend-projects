4d9362f12e176fdcec5521dc23529eb0aff899da
Chema Balsas <jose.balsas@liferay.com>
2018-08-31 14:00:31 +0200

    v8.0.0-beta.5 Prepares templates

--- packages/generator-liferay-theme/lib/divert/7.0/app_helpers.js
+++ packages/generator-liferay-theme/lib/divert/7.0/app_helpers.js
@@ -1,7 +1,7 @@
 function _getDevDependencies() {
 	return `\t"gulp": "3.9.1",
-    \t"liferay-theme-tasks": "8.0.0-beta.4",
-    \t"liferay-theme-deps-7.0": "8.0.0-beta.4"`;
+    \t"liferay-theme-tasks": "8.0.0-beta.5",
+    \t"liferay-theme-deps-7.0": "8.0.0-beta.5"`;
 }
 
 module.exports = {
