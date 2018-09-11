61cee4e81f789998463ecac45c4c264cdf54d7db
Iván Zaera Avellón <izaera@gmail.com>
2018-09-11 12:54:33 +0200

    Fixes #174 - Don't ask for project name, infer it from folder

--- packages/generator-liferay-bundle/src/facet-project/index.js
+++ packages/generator-liferay-bundle/src/facet-project/index.js
@@ -21,14 +21,9 @@ export default class extends Generator {
 		this.answers = await this.prompt([
 			{
 				type: 'input',
-				name: 'name',
-				message: 'What is the technical name of your project?',
-				default: path.basename(process.cwd()),
-			},
-			{
-				type: 'input',
 				name: 'description',
-				message: 'And the human readable description?',
+				message:
+					'What is the human readable description of your project?',
 				default: path.basename(process.cwd()),
 			},
 		]);
@@ -40,9 +35,13 @@ export default class extends Generator {
 	writing() {
 		const cp = new Copier(this);
 
-		cp.copyFile('README.md');
+		const context = {
+			name: path.basename(process.cwd()),
+		};
+
+		cp.copyFile('README.md', {context});
+		cp.copyFile('package.json', {context});
 		cp.copyFile('.gitignore');
-		cp.copyFile('package.json');
 		cp.copyFile('.npmbundlerrc');
 	}
 }
