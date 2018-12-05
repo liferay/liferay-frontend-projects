57ce93f060baeaea3bba30e143ba15e67429c539
Iván Zaera Avellón <izaera@gmail.com>
2018-12-05 14:39:12 +0100

    Fixes #242 - Let SDK version be specified in the home config file while still using QA configs

--- packages/generator-liferay-bundle/src/facet-project/index.js
+++ packages/generator-liferay-bundle/src/facet-project/index.js
@@ -2,6 +2,7 @@ import path from 'path';
 import Generator from 'yeoman-generator';
 
 import pkgJson from '../../package.json';
+import * as cfg from '../config';
 import {promptWithConfig} from '../utils';
 import {Copier, toHumanReadable} from '../utils';
 
@@ -37,9 +38,15 @@ export default class extends Generator {
 	writing() {
 		const cp = new Copier(this);
 
+		const {
+			liferayNpmBundlerVersion,
+			liferayNpmBuildSupportVersion,
+		} = this._getVersions();
+
 		const context = {
 			name: path.basename(process.cwd()),
-			sdkVersion: `^${pkgJson.version}`,
+			liferayNpmBundlerVersion,
+			liferayNpmBuildSupportVersion,
 		};
 
 		cp.copyFile('README.md', {context});
@@ -48,5 +55,30 @@ export default class extends Generator {
 		cp.copyFile('.npmbuildrc');
 		cp.copyFile('.npmbundlerrc', {context});
 		cp.copyDir('assets');
+	}
+
+	/**
+	 * Get dev dependencies version numbers.
+	 * @return {object}
+	 */
+	_getVersions() {
+		let liferayNpmBundlerVersion = `^${pkgJson.version}`;
+		let liferayNpmBuildSupportVersion = `^${pkgJson.version}`;
+
+		const sdkVersion = cfg.getSDKVersion();
+
+		if (sdkVersion) {
+			if (Number.isNaN(parseInt(sdkVersion.charAt(0)))) {
+				liferayNpmBundlerVersion =
+					sdkVersion + '/packages/liferay-npm-bundler';
+				liferayNpmBuildSupportVersion =
+					sdkVersion + '/packages/liferay-npm-build-support';
+			} else {
+				liferayNpmBundlerVersion = sdkVersion;
+				liferayNpmBuildSupportVersion = sdkVersion;
+			}
+		}
+
+		return {liferayNpmBundlerVersion, liferayNpmBuildSupportVersion};
 	}
 }
