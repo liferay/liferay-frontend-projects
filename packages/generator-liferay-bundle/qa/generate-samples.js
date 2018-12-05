4c531370eb7b36419a5865e36248adb44ce21f79
Iván Zaera Avellón <izaera@gmail.com>
2018-12-05 14:35:55 +0100

    Fixes #242 - Substitute manually generated configs by automated ones

--- packages/generator-liferay-bundle/qa/generate-samples.js
+++ packages/generator-liferay-bundle/qa/generate-samples.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const childProcess = require('child_process');
 const fs = require('fs');
 const path = require('path');
@@ -6,10 +8,87 @@ const rimraf = require('rimraf');
 const outDir = path.resolve('samples');
 const pkgsDir = path.join(outDir, 'packages');
 
+/**
+ * @param {object} options
+ */
+function writeConfig(options) {
+	const name = options.folder;
+
+	console.log('Generating test config for', name);
+
+	fs.writeFileSync(
+		path.join('config', `${name}.json`),
+		JSON.stringify(
+			{
+				batchMode: true,
+				answers: {
+					'*': Object.assign(
+						{
+							description: options.folder,
+							category: 'category.sample',
+							liferayPresent: true,
+							liferayDir: '/Users/ivan/Liferay/CE/bundles',
+						},
+						options
+					),
+				},
+			},
+			null,
+			2
+		)
+	);
+}
+
 // Prepare a clean out directory
+console.log('Cleaning work directories');
 rimraf.sync(outDir);
 fs.mkdirSync(outDir);
 fs.mkdirSync(pkgsDir);
+
+const start = new Date();
+
+// Generate vanilla samples configuration
+[true, false].forEach(useBabel => {
+	[true, false].forEach(useLocalization => {
+		[true, false].forEach(useSettings => {
+			[true, false].forEach(sampleWanted => {
+				writeConfig({
+					target: `vanilla-portlet`,
+					folder:
+						`vanilla-portlet` +
+						`-${useBabel ? 'es6' : 'es5'}` +
+						`-${useLocalization ? 'l10n' : 'nol10n'}` +
+						`-${useSettings ? 'cfg' : 'nocfg'}` +
+						`-${sampleWanted ? 'sample' : 'nosample'}`,
+					useLocalization,
+					useSettings,
+					sampleWanted,
+				});
+			});
+		});
+	});
+});
+
+// Generate framework samples configuration
+['angular', 'metaljs', 'react', 'vuejs'].forEach(fw => {
+	[true, false].forEach(useLocalization => {
+		[true, false].forEach(useSettings => {
+			[true, false].forEach(sampleWanted => {
+				writeConfig({
+					target: `${fw}-portlet`,
+					folder:
+						`${fw}-portlet` +
+						`-${useLocalization ? 'l10n' : 'nol10n'}` +
+						`-${useSettings ? 'cfg' : 'nocfg'}` +
+						`-${sampleWanted ? 'sample' : 'nosample'}`,
+					useLocalization,
+					useSettings,
+					sampleWanted,
+				});
+			});
+		});
+	});
+});
 
 // Generate samples
 const configs = fs.readdirSync('config');
@@ -42,3 +121,9 @@ console.log(`
 fs.writeFileSync(path.join(outDir, 'lerna.json'), '{}');
 
 childProcess.spawnSync('lerna', ['init'], {stdio: 'inherit', cwd: outDir});
+
+console.log(
+	'Full generation of samples took',
+	((new Date().getTime() - start.getTime()) / 1000).toFixed(0),
+	'seconds'
+);
