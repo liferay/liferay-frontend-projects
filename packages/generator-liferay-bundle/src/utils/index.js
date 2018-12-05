b9314927be00d442dffe77ed762b1173bf4f68d3
Iván Zaera Avellón <izaera@gmail.com>
2018-12-05 14:46:23 +0100

    Fixes #232 - Move localization utils back to generic module

--- packages/generator-liferay-bundle/src/utils/index.js
+++ packages/generator-liferay-bundle/src/utils/index.js
@@ -109,6 +109,55 @@ export class JsonModifier {
 }
 
 /**
+ * Format label values according to different formats.
+ * @param {object} labels
+ * @return {object} returns an object with labels transformed according to
+ * 			different formats: 'raw', 'quoted', 'template', 'js'
+ */
+export function formatLabels(labels) {
+	return {
+		raw: labels,
+		quoted: Object.entries(labels).reduce((obj, [key, value]) => {
+			obj[key] = `'${value}'`;
+			return obj;
+		}, {}),
+		template: Object.keys(labels).reduce((obj, key) => {
+			obj[key] = `\${Liferay.Language.get('${hyphenate(key)}')}`;
+			return obj;
+		}, {}),
+		js: Object.keys(labels).reduce((obj, key) => {
+			obj[key] = `Liferay.Language.get('${hyphenate(key)}')`;
+			return obj;
+		}, {}),
+		jsx: Object.keys(labels).reduce((obj, key) => {
+			obj[key] = `{Liferay.Language.get('${hyphenate(key)}')}`;
+			return obj;
+		}, {}),
+	};
+}
+
+/**
+ * Convert key from camel case to hyphens.
+ * @param {string} key
+ * @return {string}
+ */
+export function hyphenate(key) {
+	let ret = '';
+
+	for (let i = 0; i < key.length; i++) {
+		let char = key.charAt(i);
+
+		if (char === char.toUpperCase()) {
+			char = `-${char.toLowerCase()}`;
+		}
+
+		ret += char;
+	}
+
+	return ret;
+}
+
+/**
  * A function to process prompts as specified in the configuration file.
  * @param  {Generator} generator a Yeoman generator
  * @param {string} namespace the generator namespace as defined in
