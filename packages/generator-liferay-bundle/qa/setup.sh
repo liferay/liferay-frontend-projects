c01336640ee090e175f1d0a40f775bbd6c09d1cf
Iván Zaera Avellón <izaera@gmail.com>
2019-01-18 14:08:58 +0100

    Make setup.sh work on MacOS and tidy up its output
    Fixes #258

--- packages/generator-liferay-bundle/qa/setup.sh
+++ packages/generator-liferay-bundle/qa/setup.sh
@@ -1,8 +1,49 @@
+#!/bin/sh
+
+# Globals
+QA_DIR=`pwd`
+LOG_FILE="$QA_DIR/logs/$1.log"
+POSHI_FILE="portal-7.1.x-20190110-e3c1b50.jar"
+
+# Check pre-requisites
+which 7z > /dev/null
+if [ $? != 0 ] ; then
+    echo "No 7z command found. Please install it as it is needed."
+    echo "Hint: install the 'p7zip' package."
+    exit 1
+fi
+
+which wget > /dev/null
+if [ $? != 0 ] ; then
+    echo "No wget command found. Please install it as it is needed."
+    echo "Hint: install the 'wget' package."
+    exit 1
+fi
+
+if [ "$FIREFOX" != "" ] ; then
+    FIREFOX=$FIREFOX
+elif [ -x /opt/firefox45/firefox ] ; then
+    FIREFOX='/opt/firefox45/firefox'
+elif [ -x '/Applications/Firefox.app/Contents/MacOS/firefox' ] ; then
+    FIREFOX='/Applications/Firefox.app/Contents/MacOS/firefox'
+else 
+    echo "No Firefox executable found. Please install it or point the FIREFOX env var to its location."
+    exit 1
+fi
+
+# Util methods
+task() {
+    echo "*** $1"
+    mkdir -p logs
+    cat /dev/null > $LOG_FILE
+}
+
+# Do things
 if [ "$1" = "test-all-samples" ] ; then
+    task test-all-samples
+
     sh ./setup.sh clean
     sh ./setup.sh unzip-portal-snapshot-bundle
-    sh ./setup.sh create-liferay-bundle-json
-    sh ./setup.sh config-portlets
     sh ./setup.sh generate-samples
     sh ./setup.sh deploy-portlets
     sh ./setup.sh sync-master-poshi-tests
@@ -10,121 +51,143 @@ if [ "$1" = "test-all-samples" ] ; then
     sh ./setup.sh start-and-run
 
 elif [ "$1" = "clean" ] ; then
-    rm -rf .gradle build liferay-portal-master null poshi/null poshi/standalone poshi/dependencies test-results
+    task clean
+
+    echo Cleaning miscellaneous work files
+    rm -rf .gradle build liferay-portal-master poshi/standalone poshi/dependencies test-results
+
+    echo Cleaning generated samples
+    git clean -dfx config >> $LOG_FILE 2>&1
+    rm -rf samples/* 
 
 elif [ "$1" = "unzip-portal-snapshot-bundle" ] ; then
-    mkdir temp
-    echo Downloading archive: temp/liferay-portal-tomcat-master.7z...
-    echo Please wait...
-    wget https://releases.liferay.com/portal/snapshot-master/latest/liferay-portal-tomcat-master.7z -P temp
-    echo Extracting archive...
-    7z x temp/liferay-portal-tomcat-master.7z > null
-    echo Extraction Complete
-    ls ./liferay-portal-master
+    task unzip-portal-snapshot-bundle
+
+    echo Downloading liferay-portal-tomcat-master.7z
+    rm -rf temp 
+    rm -rf liferay-portal-master
+    mkdir temp 
+    wget https://releases.liferay.com/portal/snapshot-master/latest/liferay-portal-tomcat-master.7z -P temp >> $LOG_FILE 2>&1
+
+    echo Extracting archive
+    7z x temp/liferay-portal-tomcat-master.7z >> $LOG_FILE
+    
+    echo Preparing Tomcat instance
     rm -rf temp
     cd liferay-portal-master
     rm -rf data logs work osgi/state tomcat-*/work
     cd ..
-    sh ./setup.sh prepare-portal-properties
 
-elif [ "$1" = "create-liferay-bundle-json" ] ; then
-    FILE="$HOME/.generator-liferay-bundle.json"
-
-    /bin/cat <<EOM >$FILE
-{
-    "sdkVersion": "../../../../../..",
-    "answers": {
-            "facet-deploy": {
-                    "liferayPresent": true,
-                    "liferayDir": "../../../liferay-portal-master"
-            }
-    }
-}
-EOM
-
-elif [ "$1" = "prepare-portal-properties" ] ; then
+    echo Preparing Liferay instance
     cp poshi/portal-ext.properties liferay-portal-master
 
-elif [ "$1" = "sample-list" ] ; then
-    rm -rfd poshi/dependencies
-    mkdir -p poshi/dependencies &&
-    cd samples/packages &&
-    ls >> ../../poshi/dependencies/temp_list.txt
-    cd ../../poshi/dependencies
-    # Export bundle sample projects are not addable widgets that can be tested
-    sed '/export-bundle/d' -i temp_list.txt
-    tr '\n' ',' < temp_list.txt > sample_list.txt
-    rm temp_list.txt
-    cd ../..
-
-elif [ "$1" = "config-portlets" ] ; then
-    cd config
-    # sed -i 's/\"liferayDir\": \"\/Users\/ivan\/Liferay\/CE\/bundles\"/\"liferayDir\": \"$GENERATOR_DIR\/qa\/liferay-portal-master\"/g' *.json
-    #sed -i 's,/Users/ivan/Liferay/CE/bundles,'"$GENERATOR_DIR"'/qa/liferay-portal-master,g' *.json
-    sed -i 's,/Users/ivan/Liferay/CE/bundles,../../../liferay-portal-master,g' *.json
-    cat export-bundle.json
-    cd ..
-
 elif [ "$1" = "generate-samples" ] ; then
-    node generate-samples.js &&
-    cd samples/packages
-    ls -l
-    cd ../..
+    task generate-samples
+
+    echo Generating sample projects
+    node generate-samples.js
 
 elif [ "$1" = "deploy-portlets" ] ; then
+    task deploy-portlets
+
+    echo Invoking deployment in generated samples
     cd samples
-    sed -i 's/\["packages.*"\]/\["packages\/'"$2"'*"\]/g' lerna.json
     lerna run deploy
     cd ..
 
 elif [ "$1" = "sync-master-poshi-tests" ] ; then
-    echo syncing master poshi tests
-    echo extracting poshi resource portal-7.1.x-20181128102607-5b0ef97.jar into poshi/standalone...
-    echo Please wait...
-    cd poshi
-    mkdir standalone
-    wget https://repository.liferay.com/nexus/content/repositories/liferay-public-releases/com/liferay/poshi/runner/resources/portal-7.1.x/20190110-e3c1b50/portal-7.1.x-20190110-e3c1b50.jar -P standalone
-    7z x standalone/portal-7.1.x-20190110-e3c1b50.jar -ostandalone > null
-    cd poshi resource jar extracted
-    cd master
-    echo updating poshi/standalone files with changes from poshi/master
-    rsync -a -v . ../standalone/testFunctional
+    task sync-master-poshi-tests
+
+    echo Downloading $POSHI_FILE
+    rm -rf poshi/standalone
+    mkdir -p poshi/standalone
+    cd poshi/standalone
+    wget https://repository.liferay.com/nexus/content/repositories/liferay-public-releases/com/liferay/poshi/runner/resources/portal-7.1.x/20190110-e3c1b50/$POSHI_FILE >> $LOG_FILE 2>&1
+
+    echo Extracting $POSHI_FILE
+    jar xvf $POSHI_FILE >> $LOG_FILE
+    rm $POSHI_FILE
+
+    echo Updating poshi/standalone files with changes from poshi/master
+    cd ../master
+    rsync -a -v . ../standalone/testFunctional >> $LOG_FILE
+    cd ../..
+
+    echo Updating Firefox path in poshi config files
+    git checkout poshi-runner.properties
+    SUBST=`echo $FIREFOX | sed 's/\//\\\\\//g'`
+    sed -i -e "s/\/opt\/firefox45\/firefox/$SUBST/g" poshi-runner.properties
+    rm poshi-runner.properties-e > /dev/null 2>&1
+
+elif [ "$1" = "sample-list" ] ; then
+    task sample-list
+
+    echo Filling the list of generated samples to test
+    rm -rfd poshi/dependencies
+    mkdir -p poshi/dependencies 
+    cd samples/packages 
+    ls >> ../../poshi/dependencies/temp_list.txt
+    cd ../../poshi/dependencies
+    # Export bundle sample projects are not addable widgets that can be tested
+    sed -i -e '/export-bundle/d' temp_list.txt
+    tr '\n' ',' < temp_list.txt > sample_list.txt
+    rm temp_list.txt
     cd ../..
 
 elif [ "$1" = "start-and-run" ] ; then
+    task start-and-run
+
     sh ./setup.sh start-bundle &
     sh ./setup.sh run-poshi-test
 
 elif [ "$1" = "start-bundle" ] ; then
-    echo start bundle &&
-    cd liferay-portal-master/tomcat-*/bin &&
-    sh ./catalina.sh run
+    task start-bundle 
+
+    echo Starting Tomcat
+    cd liferay-portal-master/tomcat-*/bin 
+    sh ./catalina.sh run >> $LOG_FILE
+
+elif [ "$1" = "stop-bundle" ] ; then
+    task stop-bundle 
+
+    echo Stopping Tomcat
+    cd liferay-portal-master/tomcat-*/bin 
+    sh ./catalina.sh stop >> $LOG_FILE 2>&1
 
 elif [ "$1" = "run-poshi-test" ] ; then
-    echo run-poshi-test
-    wait-on -t 600000 http://localhost:8080
+    task run-poshi-test
+
+    echo Waiting for portal to be available at http://localhost:8080 
+    rm index.html > /dev/null 2>&1
+    wget -t 6000 -w 10 --retry-connrefused http://localhost:8080 >> $LOG_FILE 2>&1
+    rm index.html > /dev/null 2>&1
+    
+    echo Launching poshi tests
     ./gradlew -b standalone-poshi.gradle -PposhiRunnerExtPropertyFileNames=poshi-runner.properties runPoshi
-    if [ $? -eq 1 ] ; then
-        cd liferay-portal-master/tomcat-*/bin
-        sh ./shutdown.sh
-        exit 1
-    else
-        cd liferay-portal-master/tomcat-*/bin
-        sh ./shutdown.sh
-        exit 1
-    fi
+    POSHI_RESULT=$?
+
+    echo Shutting down Tomcat
+    sh ./setup.sh stop-bundle
+    
+    exit $POSHI_RESULT
 
 else
+    echo ""
     echo "Usage: setup.sh ( commands ... )"
-    echo "commands:"
+    echo ""
+    echo "Commands:"
+    echo "  test-all-samples                  Run all tests"
+    echo ""
+    echo "  clean                             Clean all state and files to start from scratch"
     echo "  unzip-portal-snapshot-bundle      Download and unzip portal snapshot bundle"
-    echo "  prepare-portal-properties         Download and unzip portal snapshot bundle"
-    echo "  config-portlets                   Configure json files to snapshot bundle directory"
     echo "  generate-samples                  Generate sample portlets"
     echo "  deploy-portlets                   Deploys generated portlets to liferay bundle"
     echo "  sync-master-poshi-tests           Download poshi resource jar and update with master poshi test files"
-    echo "  start-and-run                     Run liferay bundle and run poshi test"
-    echo "  start-bundle                      Run liferay bundle"
+    echo "  sample-list                       Prepare the file with the lists of bundles to be tested by Poshi"
+    echo "  start-and-run                     Run Liferay bundle and run poshi test"
+    echo "  start-bundle                      Start Liferay"
+    echo "  stop-bundle                       Stop Liferay"
     echo "  run-poshi-test                    Run poshi test"
+    echo ""
     exit 1
 fi
