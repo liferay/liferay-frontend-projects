97b028909896e6ec4c3d1019a9b8d3b7da8f7561
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2019-01-22 13:33:31 -0800

    Add tests

---
+++ packages/liferay-jest-junit-reporter/__tests__/index.js
@@ -0,0 +1,65 @@
+jest.mock('fs');
+
+const reporter = require('../index');
+const fs = require('fs');
+
+const faildTestReport = {
+	numFailedTests: 1,
+	numTotalTests: 1,
+	startTime: 1000,
+	testResults: [
+		{
+			failureMessage: null,
+			testFilePath:
+				'/foo/bar/liferay-portal/modules/apps/test-module/bar/Baz.js',
+			testResults: [
+				{
+					duration: 46,
+					// copied from a failed jest test
+					failureMessages: [
+						'Error: \u001b[2mexpect(\u001b[22m\u001b[31mreceived\u001b[39m\u001b[2m).toBe(\u001b[22m\u001b[32mexpected\u001b[39m\u001b[2m) // Object.is equality\u001b[22m\n\nExpected: \u001b[32mfalse\u001b[39m\nReceived: \u001b[31mtrue\u001b[39m\n    at Object.<anonymous> (/Users/bryceosterhaus/liferay/repos/liferay-portal/modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/test/metal/js/KeyValue/KeyValue.es.js:70:18)\n    at Object.asyncFn (/Users/bryceosterhaus/liferay/repos/liferay-portal/modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/node_modules/jest-jasmine2/build/jasmine_async.js:108:37)\n    at resolve (/Users/bryceosterhaus/liferay/repos/liferay-portal/modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/node_modules/jest-jasmine2/build/queue_runner.js:56:12)\n    at new Promise (<anonymous>)\n    at mapper (/Users/bryceosterhaus/liferay/repos/liferay-portal/modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/node_modules/jest-jasmine2/build/queue_runner.js:43:19)\n    at promise.then (/Users/bryceosterhaus/liferay/repos/liferay-portal/modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/node_modules/jest-jasmine2/build/queue_runner.js:87:41)\n    at <anonymous>\n    at process._tickCallback (internal/process/next_tick.js:182:7)'
+					],
+					fullName: 'Field DocumentLibrary should not be readOnly'
+				}
+			]
+		}
+	]
+};
+
+const passedTestReport = {
+	numFailedTests: 0,
+	numTotalTests: 1,
+	startTime: 1000,
+	testResults: [
+		{
+			failureMessage: null,
+			testFilePath:
+				'/foo/bar/liferay-portal/modules/apps/test-module/bar/Baz.js',
+			testResults: [
+				{
+					duration: 46,
+					failureMessages: [],
+					fullName: 'Field DocumentLibrary should not be readOnly'
+				}
+			]
+		}
+	]
+};
+
+describe('liferay-jest-junit-reporter', () => {
+	it('should write a file for a passing test', () => {
+		reporter(passedTestReport);
+
+		const xmlWritten = fs.writeFileSync.mock.calls[0][1];
+
+		expect(xmlWritten).toMatchSnapshot();
+	});
+
+	it('should write a file for a failing test', () => {
+		reporter(faildTestReport);
+
+		const xmlWritten = fs.writeFileSync.mock.calls[1][1];
+
+		expect(xmlWritten).toMatchSnapshot();
+	});
+});
