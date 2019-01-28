608605fa88859cce3e5f41a27444baaa43db0d15
Chema Balsas <jose.balsas@liferay.com>
2019-01-28 14:03:09 +0100

    v8.0.0-rc.2 Prepares templates

--- packages/liferay-theme-tasks/lib/upgrade/6.2/upgrade.js
+++ packages/liferay-theme-tasks/lib/upgrade/6.2/upgrade.js
@@ -80,7 +80,7 @@ module.exports = function(options) {
 		lfrThemeConfig.removeDependencies(['liferay-theme-deps-6.2']);
 		lfrThemeConfig.setDependencies(
 			{
-				'liferay-theme-deps-7.0': '8.0.0-rc.1',
+				'liferay-theme-deps-7.0': '8.0.0-rc.2',
 			},
 			true
 		);
--- packages/liferay-theme-tasks/lib/upgrade/7.0/upgrade.js
+++ packages/liferay-theme-tasks/lib/upgrade/7.0/upgrade.js
@@ -43,8 +43,8 @@ module.exports = function(options) {
 		lfrThemeConfig.removeDependencies(['liferay-theme-deps-7.0']);
 		lfrThemeConfig.setDependencies(
 			{
-				'liferay-theme-deps-7.1': '8.0.0-rc.1',
-				'liferay-theme-tasks': '8.0.0-rc.1',
+				'liferay-theme-deps-7.1': '8.0.0-rc.2',
+				'liferay-theme-tasks': '8.0.0-rc.2',
 			},
 			true
 		);
