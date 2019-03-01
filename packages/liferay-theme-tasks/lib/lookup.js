2bf991fdd256654b2260762083b6982f8c21c285
Greg Hurrell <greg.hurrell@liferay.com>
2019-03-01 16:26:21 +0100

    Migrate "kickstart_prompt_helpers.js" to use `lookup()`
    This is my attempt at replacing the `divert()` pattern with something
    slightly less magical and more centralized. Instead of trying to require
    a file at runtime whose name we construct dynamically, give everything a
    name and look it up by name.
    
    More importantly though, from a maintainability perspective, this
    pattern enables us to put related functionality in the same file instead
    of spreading it across the filesystem, so it is easier to see the
    commonalities and differences between different versions.
    
    For example, in this commit, you can see at a glance that the 7.0
    version adds one more choice to the list of choices at the kickstart
    prompt, but other than that it is the same.
    
    I'll continue to move the other `divert()`-powered stuff in the
    following commits.
    
    Test plan: `yarn test`

---
+++ packages/liferay-theme-tasks/lib/lookup.js
@@ -0,0 +1,25 @@
+const lfrThemeConfig = require('./liferay_theme_config');
+
+const DEFAULT_VERSION = '7.1';
+
+/**
+ * Utility for retrieving version-specific functionality.
+ *
+ * @param {string} key The identifying name for the functionality.
+ * @param {string} [version] The Liferay version; will be inferred if omitted.
+ * @return {?}
+ */
+function lookup(key, version = null) {
+	if (!version) {
+		let config = lfrThemeConfig.getConfig();
+
+		version = config ? config.version : DEFAULT_VERSION;
+	}
+
+	return {
+		'kickstart:choices': require('./lookup/kickstart').choices,
+		'kickstart:afterPromptThemeSource': require('./lookup/kickstart').afterPromptThemeSource,
+	}[key](version);
+}
+
+module.exports = lookup;
