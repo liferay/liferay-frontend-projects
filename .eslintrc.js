618a1ff3a8e8f0d297135d2eb2eb636ac1a834ce
Greg Hurrell <greg@hurrell.net>
2019-03-03 11:57:46 +0100

    Remove top-level rules from .eslintrc.js
    Instead, we want to operate based on the rules that come from the preset
    (ie. in index.js).

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,25 +23,5 @@ module.exports = {
 		themeDisplay: true,
 		tinyMCE: true,
 	},
-	rules: {
-		'lines-around-comment': [
-			'error',
-			{
-				afterBlockComment: false,
-				afterLineComment: true,
-				beforeBlockComment: false,
-				beforeLineComment: false,
-			},
-		],
-		'padding-line-between-statements': [
-			'error',
-			{blankLine: 'always', prev: '*', next: 'return'},
-			{blankLine: 'always', prev: ['const', 'let', 'var'], next: '*'},
-			{
-				blankLine: 'any',
-				prev: ['const', 'let', 'var'],
-				next: ['const', 'let', 'var'],
-			},
-		],
-	},
+	rules: {},
 };
