542b733767f3f1fccf0eda4f28cd67edbd45cf74
Greg Hurrell <greg.hurrell@liferay.com>
2019-03-07 17:30:35 +0100

    Teach getPackageJSON() to use require()
    Rather than reading the file, passing an encoding to force
    `readFileSync` to return a string instead of a buffer, and then parsing
    the result as JSON, just `require` the file. The result should be
    exactly equivalent.
    
    Test plan: `yarn test`

--- packages/liferay-theme-tasks/lib/liferay_theme_config.js
+++ packages/liferay-theme-tasks/lib/liferay_theme_config.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
 const fs = require('fs-extra');
+const path = require('path');
 
 function getConfig(all) {
 	const packageJSON = getPackageJSON();
@@ -75,11 +76,7 @@ function deleteDependencies(sourceDependencies, deletedDependencies) {
 }
 
 function getPackageJSON() {
-	const packageJSONContent = fs.readFileSync('package.json', {
-		encoding: 'utf8',
-	});
-
-	return JSON.parse(packageJSONContent);
+	return require(path.resolve('package.json'));
 }
 
 function writePackageJSON(json) {
