ab10abf46d770c99caf33ac8b22c9496a828e30b
Iván Zaera Avellón <izaera@gmail.com>
2019-03-28 11:57:53 +0100

    Fixes #203 - Remove Promise polyfill (we'll rely on the user to provide it)

--- src/loader/bootstrap.js
+++ src/loader/bootstrap.js
@@ -5,11 +5,6 @@
  */
 
 import Loader from './loader.js';
-import Promise from 'es6-promise';
-
-if (typeof window.Promise === 'undefined') {
-	window.Promise = Promise;
-}
 
 const cfg = window.__CONFIG__ || {};
 const namespace = typeof cfg.namespace === 'string' ? cfg.namespace : undefined;
