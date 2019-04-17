liferay/liferay-frontend-projects

commit 54c57711d207a6a6612ac25adbbb3cead0e7a534

Fix some failing code
Fixes #31

packages/liferay-theme-tasks/lib/war_deployer.js
@@ -94,19 +94,17 @@ class WarDeployer extends EventEmitter {
 	}
 
 	_makeRequest() {
-		let instance = this;
-
 		let protocol = require(this.protocol);
 
 		let req = protocol.request(this._getPostOptions(), function(res) {
 			res.setEncoding('utf8');
 
 			res.on('data', function(chunk) {
-				instance._onResponseData(chunk);
+				this._onResponseData(chunk);
 			});
 
 			res.on('end', function() {
-				instance._onResponseEnd();
+				this._onResponseEnd();
 			});
 		});
 
@@ -148,8 +146,6 @@ class WarDeployer extends EventEmitter {
 	}
 
 	_promptCredentialsIfNeeded() {
-		let instance = this;
-
 		let questions = [];
 
 		if (!this.username) {
@@ -163,13 +159,13 @@ class WarDeployer extends EventEmitter {
 		if (questions.length) {
 			inquirer.prompt(questions, function(answers) {
 				Object.keys(answers).forEach(function(key) {
-					instance[key] = answers[key];
+					this[key] = answers[key];
 				});
 
-				instance._makeRequest();
+				this._makeRequest();
 			});
 		} else {
-			instance._makeRequest();
+			this._makeRequest();
 		}
 	}
 
@@ -194,8 +190,6 @@ class WarDeployer extends EventEmitter {
 	}
 
 	_writeWarFile(req) {
-		let instance = this;
-
 		let boundaryKey = this._getBoundaryKey();
 
 		req.write(this._getFileHeaders(this._fileName, boundaryKey));
@@ -205,7 +199,7 @@ class WarDeployer extends EventEmitter {
 			.on('end', function() {
 				req.end('\r\n--' + boundaryKey + '--');
 
-				instance.emit('end');
+				this.emit('end');
 			})
 			.pipe(req, {
 				end: false,

packages/liferay-theme-tasks/lib/__tests__/look_and_feel_util.test.js
@@ -101,7 +101,9 @@ it('mergeLookAndFeelJSON should merge look and feel json', done => {
 
 it('readLookAndFeelXML should return xml and access file only once', () => {
 	// erase cache for sinon spy
-	lookAndFeelUtil.xmlCache = {};
+	Object.keys(lookAndFeelUtil.xmlCache).forEach(
+		key => delete lookAndFeelUtil.xmlCache[key]
+	);
 
 	const spy = sinon.spy(fs, 'readFileSync');
 
@@ -110,7 +112,7 @@ it('readLookAndFeelXML should return xml and access file only once', () => {
 	xml = lookAndFeelUtil.readLookAndFeelXML(baseThemePath);
 
 	expect(/<look-and-feel>/.test(xml)).toBeTruthy();
-	expect(spy.calledOnce).toBe(true);
+	expect(spy.callCount).toBe(1);
 });
 
 it('extractThemeElement should extract elements based on tag name', done => {

packages/liferay-theme-tasks/lib/look_and_feel_util.js
@@ -85,7 +85,7 @@ function correctJSONIdentifiers(lookAndFeelJSON, name) {
 }
 
 function getLookAndFeelDoctype(themePath) {
-	let xmlString = this.readLookAndFeelXML(themePath);
+	let xmlString = readLookAndFeelXML(themePath);
 
 	let match;
 
@@ -107,7 +107,7 @@ function getLookAndFeelDoctypeByVersion(version) {
 }
 
 function getLookAndFeelJSON(themePath, cb) {
-	let xmlString = this.readLookAndFeelXML(themePath);
+	let xmlString = readLookAndFeelXML(themePath);
 
 	if (!xmlString) {
 		return cb();
@@ -141,13 +141,11 @@ function getNameFromPluginPackageProperties(themePath) {
 }
 
 function mergeLookAndFeelJSON(themePath, lookAndFeelJSON, cb) {
-	let instance = this;
-
-	instance.getLookAndFeelJSON(themePath, function(json) {
+	getLookAndFeelJSON(themePath, function(json) {
 		if (_.isEmpty(lookAndFeelJSON)) {
 			lookAndFeelJSON = json;
 		} else if (json) {
-			lookAndFeelJSON = instance.mergeJSON(lookAndFeelJSON, json);
+			lookAndFeelJSON = mergeJSON(lookAndFeelJSON, json);
 		}
 
 		let themeInfo = require(path.join(themePath, 'package.json'))
@@ -158,7 +156,7 @@ function mergeLookAndFeelJSON(themePath, lookAndFeelJSON, cb) {
 		if (_.isObject(baseTheme)) {
 			themePath = path.join(themePath, 'node_modules', baseTheme.name);
 
-			instance.mergeLookAndFeelJSON(themePath, lookAndFeelJSON, cb);
+			mergeLookAndFeelJSON(themePath, lookAndFeelJSON, cb);
 		} else {
 			cb(lookAndFeelJSON);
 		}
@@ -166,7 +164,7 @@ function mergeLookAndFeelJSON(themePath, lookAndFeelJSON, cb) {
 }
 
 function readLookAndFeelXML(themePath) {
-	let xmlString = this.xmlCache[themePath];
+	let xmlString = xmlCache[themePath];
 
 	if (xmlString) {
 		return xmlString;
@@ -191,7 +189,7 @@ function readLookAndFeelXML(themePath) {
 	try {
 		xmlString = fs.readFileSync(lookAndFeelPath, 'utf8');
 
-		this.xmlCache[themePath] = xmlString;
+		xmlCache[themePath] = xmlString;
 	} catch (err) {}
 
 	return xmlString;
@@ -215,8 +213,6 @@ function extractThemeElement(obj, key) {
 }
 
 function mergeJSON(themeObj, baseThemeObj) {
-	let instance = this;
-
 	_.forEach(QUERY_ELEMENTS, function(item, index) {
 		let mergedElement;
 		let queryString = 'look-and-feel.theme.0.' + index;
@@ -225,14 +221,14 @@ function mergeJSON(themeObj, baseThemeObj) {
 		let themeElement = _.get(themeObj, queryString);
 
 		if (item === 'value') {
-			mergedElement = instance.mergeThemeElementByValue(
+			mergedElement = mergeThemeElementByValue(
 				themeElement,
 				baseThemeElement
 			);
 		} else if (item === 'single') {
 			mergedElement = themeElement || baseThemeElement;
 		} else {
-			mergedElement = instance.mergeThemeElementById(
+			mergedElement = mergeThemeElementById(
 				themeElement,
 				baseThemeElement,
 				item
@@ -283,7 +279,7 @@ function mergeThemeElementByValue(themeElements, baseThemeElements) {
 }
 
 // Export private methods when in tests
-if (jest) {
+if (typeof jest !== 'undefined') {
 	Object.assign(module.exports, {
 		extractThemeElement,
 		mergeJSON,

packages/liferay-theme-tasks/lib/theme_finder.js
@@ -237,7 +237,7 @@ function searchNpm(config, cb) {
 }
 
 // Export private methods when in tests
-if (jest) {
+if (typeof jest !== 'undefined') {
 	Object.assign(module.exports, {
 		findThemeModulesIn,
 		getNpmPaths,

packages/liferay-theme-tasks/lib/util.js
@@ -155,7 +155,7 @@ function validateCustomDependencyPath(customPath) {
 }
 
 // Export private methods when in tests
-if (jest) {
+if (typeof jest !== 'undefined') {
 	Object.assign(module.exports, {
 		getCustomDependencyPath,
 		getDepsPath,

commit 6eb7e81c742759770c22f1e1f95a57d4b4828f9f

fix: invalid "this" in WarDeployer
Broken by: https://github.com/liferay/liferay-js-themes-toolkit/commit/54c57711d207a6a6612ac25adbbb3cead0e7a534#diff-b2e3a6736d0fb9f5dd8ce63552c3ea0f

packages/liferay-theme-tasks/lib/war_deployer.js
@@ -102,19 +102,19 @@ class WarDeployer extends EventEmitter {
 	_makeRequest() {
 		const protocol = require(this.protocol);
 
-		const req = protocol.request(this._getPostOptions(), function(res) {
+		const req = protocol.request(this._getPostOptions(), res => {
 			res.setEncoding('utf8');
 
-			res.on('data', function(chunk) {
+			res.on('data', chunk => {
 				this._onResponseData(chunk);
 			});
 
-			res.on('end', function() {
+			res.on('end', () => {
 				this._onResponseEnd();
 			});
 		});
 
-		req.on('error', function(err) {
+		req.on('error', err => {
 			if (err) {
 				throw err;
 			}
@@ -165,8 +165,8 @@ class WarDeployer extends EventEmitter {
 		}
 
 		if (questions.length) {
-			inquirer.prompt(questions, function(answers) {
-				Object.keys(answers).forEach(function(key) {
+			inquirer.prompt(questions, answers => {
+				Object.keys(answers).forEach(key => {
 					this[key] = answers[key];
 				});
 
@@ -203,7 +203,7 @@ class WarDeployer extends EventEmitter {
 		req.write(this._getFileHeaders(this._fileName, boundaryKey));
 
 		fs.createReadStream(path.join(CWD, 'dist', this.fileName + '.war'))
-			.on('end', function() {
+			.on('end', () => {
 				req.end('\r\n--' + boundaryKey + '--');
 
 				this.emit('end');