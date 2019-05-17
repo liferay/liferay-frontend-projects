ccc902bc4aff263d431e4529304db9698072465a
Greg Hurrell <greg.hurrell@liferay.com>
2019-05-17 11:17:12 +0200

    feat: add Babel merge strategy to get-merged-config.js
    As described in the related issue, our merge implementation is using the
    pre-2.0 version of the array merge algorithm in the "deepmerge" package.
    They removed it because it was a "complicated thing".
    
    In the case of merging Babel configs it verily breaks them, because you
    can specify plugins and presets as either `'name'` or `['name',
    options]`. Here's a reduced example of some surprising behavior:
    
    - merging ['a', 'b', ['c']]
    - into ['d', 'e']
    - yields: ['d', 'e', ['c'], 'b']
    
    ie. 'a' disappears into the void.
    
    See the test cases included in this commit for a real-life example taken
    from segments-web. The old algorithm would cause the
    `@babel/plugin-proposal-class-properties` plugin to vanish.
    
    The implementation here doesn't intend to handle all possible edge cases
    (for example, it doesn't try to match plugins that have been written in
    different forms such as "@babel/plugin-x" and "x"), but it covers the
    ones we are most likely to see in liferay-portal and it is well tested.
    
    I chose to write a narrow implementation like this rather than pull in a
    third-party dependency like "babel-merge", because I like to keep our
    dependency footprint small and we actually kind of want to discourage
    complicated Babel overrides in liferay-portal in any case, so it's in
    our interests to have something that isn't too flexible.
    
    Closes: https://github.com/liferay/liferay-npm-tools/issues/83

--- packages/liferay-npm-scripts/src/utils/deep-merge.js
+++ packages/liferay-npm-scripts/src/utils/deep-merge.js
@@ -35,14 +35,125 @@ function combineMerge(target, source, options) {
  */
 const overwriteMerge = (destinationArray, sourceArray) => sourceArray;
 
+function getItemDescription(item) {
+	try {
+		return JSON.stringify(item);
+	} catch (error) {
+		// Could be a circular reference, but we're unlikely to ever get here
+		// because the deepmerge package itself will die first.
+		return `[unstringifiable item: ${error}]`;
+	}
+}
+
+function getBabelName(item) {
+	if (typeof item === 'string') {
+		return item;
+	} else if (Array.isArray(item) && typeof item[0] === 'string') {
+		return item[0];
+	} else {
+		throw new Error(
+			`getBabelName(): malformed item ${getItemDescription(item)}`
+		);
+	}
+}
+
+function getBabelOptions(item) {
+	if (typeof item === 'string') {
+		return null;
+	} else if (Array.isArray(item)) {
+		const options = item[1];
+		return options &&
+			typeof options === 'object' &&
+			Object.keys(options).length
+			? options
+			: null;
+	} else {
+		// We never expect to get here, but just in case...
+		throw new Error('getBabelOptions(): incompatible item type');
+	}
+}
+
+/**
+ * Custom merge that knows how to merge "plugins" and "presets".
+ */
+function babelMerge(key) {
+	if (key === 'plugins' || key === 'presets') {
+		return function(target, source, options) {
+			// Create a mutable copy of `source`.
+			const pending = source.slice();
+
+			const result = target.map(targetItem => {
+				const targetName = getBabelName(targetItem);
+				const sourceIndex = pending.findIndex(sourceItem => {
+					const sourceName = getBabelName(sourceItem);
+					return sourceName === targetName;
+				});
+				if (sourceIndex !== -1) {
+					const [sourceItem] = pending.splice(sourceIndex, 1);
+					const mergedOptions = merge.all(
+						[
+							getBabelOptions(targetItem),
+							getBabelOptions(sourceItem)
+						].filter(Boolean),
+						options
+					);
+
+					return Object.keys(mergedOptions).length
+						? [targetName, mergedOptions]
+						: targetName;
+				} else {
+					const targetOptions = getBabelOptions(targetItem);
+					return targetOptions ? targetItem : targetName;
+				}
+			});
+
+			return result.concat(
+				pending.map(item => {
+					const itemName = getBabelName(item);
+					const itemOptions = getBabelOptions(item);
+					return itemOptions ? [itemName, itemOptions] : itemName;
+				})
+			);
+		};
+	} else {
+		return combineMerge;
+	}
+}
+
+const MODE = Object.freeze({
+	DEFAULT: 0,
+	OVERWRITE_ARRAYS: 1,
+	BABEL: 2
+});
+
 /**
  * Helper to get merge two json objects
- * @param {Object} defaultConfig Config file
- * @param {Object} customConfig Config file
+ * @param {Array} items An array of config objects
+ * @param {Number} mode Merge strategy for combining values.
  * @returns {Object}
  */
-module.exports = function(items, overwrite) {
-	return merge.all(items, {
-		arrayMerge: overwrite ? overwriteMerge : combineMerge
-	});
-};
+function deepMerge(items, mode = MODE.DEFAULT) {
+	switch (mode) {
+		case MODE.DEFAULT:
+			return merge.all(items, {
+				arrayMerge: combineMerge
+			});
+
+		case MODE.OVERWRITE_ARRAYS:
+			return merge.all(items, {
+				arrayMerge: overwriteMerge
+			});
+
+		case MODE.BABEL:
+			return merge.all(items, {
+				customMerge: babelMerge
+			});
+
+		default:
+			throw new Error(`deepMerge(): unsupported mode: ${mode}`);
+	}
+}
+
+deepMerge.MODE = MODE;
+
+module.exports = deepMerge;
