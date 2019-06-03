11dd1edf66465767104fb25188184ed715ba02e0
Greg Hurrell <greg.hurrell@liferay.com>
2019-06-03 11:15:08 +0200

    test: improve spawnSync tests
    In the commit where I added the `SpawnError` class I didn't touch the
    tests because I wanted it to be clear that this wasn't a breaking
    change.
    
    Looping back now to add some tests to make the nuances more explicit.

--- packages/liferay-npm-scripts/__tests__/utils/spawn-sync.js
+++ packages/liferay-npm-scripts/__tests__/utils/spawn-sync.js
@@ -6,16 +6,32 @@
 
 const spawnSync = require('../../src/utils/spawn-sync');
 
+const {SpawnError} = spawnSync;
+
 describe('spawnSync()', () => {
-	it('throws for non-existent commands', () => {
-		expect(() => spawnSync('non-existent-command')).toThrow(/ENOENT/);
-	});
-
-	it('throws for commands that return a non-zero exit code', () => {
-		expect(() => spawnSync('false')).toThrow(/code 1/);
-	});
-
 	it('succeeds for valid commands', () => {
 		expect(() => spawnSync('true')).not.toThrow();
+	});
+
+	describe('commands that return a non-zero exit code', () => {
+		it('throws an error containing the exit code', () => {
+			expect(() => spawnSync('false')).toThrow(/code 1/);
+		});
+
+		it('throws a SpawnError', () => {
+			expect(() => spawnSync('false')).toThrow(SpawnError);
+		});
+	});
+
+	describe('non-existent commands', () => {
+		it('throws an error that describes why the command did not run', () => {
+			expect(() => spawnSync('non-existent-command')).toThrow(/ENOENT/);
+		});
+
+		it('throws an Error, not a SpawnError', () => {
+			expect(() => spawnSync('non-existent-command')).not.toThrow(
+				SpawnError
+			);
+		});
 	});
 });
