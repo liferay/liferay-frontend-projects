e954c27d11b2aaa587b306cc5c64de9477513dca
Greg Hurrell <greg.hurrell@liferay.com>
2019-06-03 13:28:52 +0200

    fix: make lint/format behavior match documentation
    Our documentation said that you could provide "lint" and "format"
    configuration to control which paths get linted/formatted, but in
    practice we were always using the "lint" config for both.
    
    Obviously, it doesn't make a whole lot of sense to *check* formatting on
    one set of globs and *apply* formatting on a different set of globs,
    but in the future we're going to use ESLint in liferay-portal, at which
    point "linting" and "formatting" actually will come to mean different
    things. For example, we will want to "lint" only JavaScript (with
    ESLint), but we'll want to "format" JS, CSS etc (with Prettier).
    
    So, even though right now both "linting" and "formatting" are being
    handled by Prettier, this change prepares us for a future in which we'll
    actually have two separate tools in play.

--- packages/liferay-npm-scripts/src/scripts/lint.js
+++ packages/liferay-npm-scripts/src/scripts/lint.js
@@ -9,13 +9,15 @@ const path = require('path');
 const getMergedConfig = require('../utils/get-merged-config');
 const spawnSync = require('../utils/spawn-sync');
 
-const LINT_PATHS = getMergedConfig('npmscripts').lint;
+const CONFIG = getMergedConfig('npmscripts');
 
 /**
  * Main function for linting and formatting files
  * @param {boolean} fix Specify if the linter should auto-fix the files
  */
 module.exports = function(fix) {
+	const globs = fix ? CONFIG.format : CONFIG.lint;
+
 	const CONFIG_PATH = path.join(process.cwd(), 'TEMP-prettier-config.json');
 
 	fs.writeFileSync(CONFIG_PATH, JSON.stringify(getMergedConfig('prettier')));
@@ -25,7 +27,7 @@ module.exports = function(fix) {
 			'--config',
 			CONFIG_PATH,
 			fix ? '--write' : '--check',
-			...LINT_PATHS
+			...globs
 		];
 
 		spawnSync('prettier', args);
