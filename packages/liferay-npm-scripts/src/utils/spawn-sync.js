8749b8918daf08500d62baa752684e40607681a3
Greg Hurrell <greg.hurrell@liferay.com>
2019-06-03 10:15:19 +0200

    refactor!: back out "silent" option from spawnSync API
    Going to implement the suggestion from here:
    
    https://github.com/liferay/liferay-npm-tools/pull/96#discussion_r289726563
    
    instead.

--- packages/liferay-npm-scripts/src/utils/spawn-sync.js
+++ packages/liferay-npm-scripts/src/utils/spawn-sync.js
@@ -17,9 +17,8 @@ function getDescription(command, args) {
  * @param {string} command Path to bin file
  * @param {Array=} args=[] List of string arguments
  * @param {Object=} options={} Options to pass to spawn.sync
- * @param {boolean} silent=false Should swallow non-error exit codes.
  */
-module.exports = function(command, args = [], options = {}, silent = false) {
+function spawnSync(command, args = [], options = {}) {
 	const localCommand = path.join(
 		__dirname,
 		'../../node_modules/.bin',
@@ -32,7 +31,7 @@ module.exports = function(command, args = [], options = {}, silent = false) {
 		...options
 	});
 
-	if (status && !silent) {
+	if (status) {
 		throw new Error(
 			`Command ${getDescription(
 				executable,
@@ -47,4 +46,6 @@ module.exports = function(command, args = [], options = {}, silent = false) {
 			)} failed with error ${error}`
 		);
 	}
-};
+}
+
+module.exports = spawnSync;
