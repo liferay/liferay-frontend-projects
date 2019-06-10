f20c8975e4cc92cedbd978503229c98a5a88965e
Kevin Tan <kevin.tan@liferay.com>
2019-06-10 16:43:15 -0700

    Re-export storybook modules (#94)
    When writing stories for a portal module, they would be used like:
    
    ```
    import {
    	StorybookAddonA11y,
    	StorybookAddonActions,
    	StorybookAddonKnobs,
    	StorybookReact
    } from 'liferay-npm-scripts/src/storybook';
    
    const {addDecorator, storiesOf} = StorybookReact;
    
    const {withA11y} = StorybookAddonA11y;
    const {action} = StorybookAddonActions;
    const {array, boolean, select, text, withKnobs} = StorybookAddonKnobs;
    ```

---
+++ packages/liferay-npm-scripts/src/storybook/index.js
@@ -0,0 +1,17 @@
+/**
+ * © 2019 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import * as StorybookReact from '@storybook/react';
+import * as StorybookAddonKnobs from '@storybook/addon-knobs';
+import * as StorybookAddonActions from '@storybook/addon-actions';
+import * as StorybookAddonA11y from '@storybook/addon-a11y';
+
+export {
+	StorybookAddonA11y,
+	StorybookAddonActions,
+	StorybookAddonKnobs,
+	StorybookReact
+};
