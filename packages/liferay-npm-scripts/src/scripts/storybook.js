3c9b9c2aa3e64109a4c22bf79b5f99f804799777
Kevin Tan <kevin.tan@liferay.com>
2019-06-10 14:51:43 -0700

    Use configs inside liferay-npm-scripts instead of copying to temp (#94)

--- packages/liferay-npm-scripts/src/scripts/storybook.js
+++ packages/liferay-npm-scripts/src/scripts/storybook.js
@@ -4,43 +4,28 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-const fse = require('fs-extra');
 const path = require('path');
 const getMergedConfig = require('../utils/get-merged-config');
 const spawnSync = require('../utils/spawn-sync');
 
-const TEMP_DIR_NAME = 'TEMP-stories';
-
-const CONFIG_PATH = path.join(process.cwd(), TEMP_DIR_NAME);
 const STORYBOOK_CONFIG = getMergedConfig('npmscripts').storybook;
 
 /**
- * Remove the temporary directory created that stored the storybook configs
- * while storybook was running.
+ * Used for storybook/config.js to load stories from the current working
+ * directory.
  */
-function cleanup() {
-	fse.removeSync(CONFIG_PATH);
-}
+const STORYBOOK_ENV_CWD = `STORYBOOK_CWD=${process.cwd()}`;
 
 /**
  * Starts a storybook server for testing frontend components.
  */
 module.exports = function() {
-	process.stdin.resume();
-
-	fse.ensureDirSync(CONFIG_PATH);
-
-	fse.copySync(path.join(__dirname, '../storybook'), CONFIG_PATH);
-
 	const args = [
 		'--port',
 		STORYBOOK_CONFIG.port,
 		'--config-dir',
-		TEMP_DIR_NAME
+		path.join(__dirname, '../storybook')
 	];
 
-	// Run the cleanup function when storybook is exited with `ctr+c`.
-	process.on('SIGINT', cleanup);
-
-	spawnSync('start-storybook', args);
+	spawnSync(`${STORYBOOK_ENV_CWD} start-storybook`, args, {shell: true});
 };
--- packages/liferay-npm-scripts/src/storybook/addons.js
+++ packages/liferay-npm-scripts/src/storybook/addons.js
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
- /**
+/**
  * Order determines the order they will be displayed in the tabs.
  */
 import '@storybook/addon-knobs/register';
