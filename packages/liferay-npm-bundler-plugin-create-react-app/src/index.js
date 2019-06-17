a02458762b2d12854e917710d91f92e0acfe2e90
Iván Zaera Avellón <izaera@gmx.es>
2019-06-17 11:11:59 +0200

    Fixes #347: Inject create-react-app CSS files
    In addition to webpack's generated JS bundle, we need to add CSS bundles
    to the generated portlet so that the rendered HTML is correctly shown.

--- packages/liferay-npm-bundler-plugin-create-react-app/src/index.js
+++ packages/liferay-npm-bundler-plugin-create-react-app/src/index.js
@@ -4,6 +4,9 @@
  * SPDX-License-Identifier: LGPL-3.0-or-later
  */
 
+import fs from 'fs';
+import path from 'path';
+
 /**
  * @return {void}
  */
@@ -12,4 +15,18 @@ export default function({config, log, pkg, rootPkgJson, source}, {pkgJson}) {
 	// by the liferay-npm-build-support build script for create-react-app
 	// projects.
 	pkgJson.main = 'index.js';
+
+	// Create a portlet section if it doesn't exist
+	pkgJson.portlet = pkgJson.portlet || {};
+
+	// Add CSS files to portlet headers
+	const cssDir = path.join(pkg.dir, 'react-app', 'static', 'css');
+
+	const cssFiles = fs
+		.readdirSync(cssDir)
+		.filter(jsFile => jsFile.endsWith('.css'));
+
+	pkgJson.portlet['com.liferay.portlet.header-portlet-css'] = cssFiles.map(
+		cssFile => `react-app/static/css/${path.basename(cssFile)}`
+	);
 }
