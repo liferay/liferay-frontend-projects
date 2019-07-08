f0d88f210e3984c7ef4e82445e1068e1d8b0624e
Greg Hurrell <greg.hurrell@liferay.com>
2019-07-08 15:42:43 +0200

    test: add debugging commands
    The `ps -ef` is suggested here to see what might be hanging around:
    
    https://travis-ci.community/t/build-doesnt-finish-after-completing-tests/288/4
    
    Still doesn't explain why Jest keeps printing stuff to the console even
    after the script has supposedly exited though.

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -x
 
 if [ "$OS" = "Windows_NT" ]; then
   # On Windows, just the tests, not the format checks or lints, because
@@ -10,14 +11,16 @@ if [ "$OS" = "Windows_NT" ]; then
   # Note that we can't run `yarn test` directly because on Windows:
   #
   # - `yarn` is a .bat script.
-  # - `yarn test` invokes `yarn workspaces run test`.
+  # - `yarn test` invokes `yarn jest`.
   # - .bat scripts don't actually wait for child .bat scripts to return
   #   (https://github.com/npm/npm/issues/2938#issuecomment-11337463).
   # - This causes our script here to return early, and then the whole run times
   #   out after 10 minutes...
-  # - Even `yarn jest` doesn't work, because `jest` is a .bat script too.
   #
   node node_modules/jest-cli/bin/jest.js
 else
   yarn format:check && yarn lint && yarn test
 fi
+
+# Debugging.
+ps -ef
