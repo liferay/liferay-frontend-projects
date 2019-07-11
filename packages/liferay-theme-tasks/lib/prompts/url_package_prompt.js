eb279e183224df842044855f8cf42703f8df05de
Rodolfo Roza Miranda <rodolfo.roza@gs.liferay.com>
2019-07-11 13:32:38 -0300

    feat: display prompt to keep/remove extended themelets (#375)

--- packages/liferay-theme-tasks/lib/prompts/url_package_prompt.js
+++ packages/liferay-theme-tasks/lib/prompts/url_package_prompt.js
@@ -9,6 +9,9 @@ const {URL} = require('url');
 
 const themeFinder = require('../theme_finder');
 
+const _ = require('lodash');
+const promptUtil = require('./prompt_util');
+
 class URLPackagePrompt {
 	constructor(...args) {
 		this.init(...args);
@@ -17,18 +20,112 @@ class URLPackagePrompt {
 	init(config, cb) {
 		this.done = cb;
 		this.themelet = config.themelet;
+		this.config = config;
 
-		inquirer.prompt(
-			[
-				{
-					message: 'Enter the URL for the package:',
-					name: 'packageURL',
-					type: 'input',
-					validate: this._validatePackageURL,
-				},
-			],
-			this._afterPrompt.bind(this)
-		);
+		this._prompt();
+	}
+
+	_afterPrompt(answers) {
+		this._handleThemeletSelection(answers);
+
+		if (answers.packageURL) {
+			const error = this._validatePackageURL(answers.packageURL, answers);
+
+			if (error !== true) {
+				console.error(error);
+				this._prompt();
+				return;
+			}
+
+			const config = themeFinder.getLiferayThemeModuleFromURL(
+				answers.packageURL
+			);
+
+			answers.modules = {
+				[config.name]: Object.assign({}, config, {
+					__packageURL__: answers.packageURL,
+				}),
+			};
+
+			if (this.themelet) {
+				answers.module = {[config.name]: true};
+				answers.addedThemelets = [config.name];
+
+				if (answers.removedThemelets) {
+					const index = answers.removedThemelets.indexOf(config.name);
+
+					if (index > -1) {
+						answers.removedThemelets.splice(index, 1);
+					}
+				}
+			} else {
+				answers.module = config.name;
+			}
+		}
+
+		this.done(answers);
+	}
+
+	_filterModule(input) {
+		return _.mapValues(this.config.selectedModules, function(name) {
+			return input.indexOf(name) > -1;
+		});
+	}
+
+	_getModuleChoices() {
+		const choices = _.map(this.config.selectedModules, function(name) {
+			return {
+				checked: true,
+				name,
+			};
+		});
+
+		return choices;
+	}
+
+	_getModuleWhen() {
+		return !_.isEmpty(this.config.selectedModules);
+	}
+
+	_handleThemeletSelection(answers) {
+		if (this.themelet && answers.module) {
+			Object.keys(answers.module).forEach(k => {
+				const moduleName = this.config.selectedModules[k];
+				answers.module[moduleName] = answers.module[k];
+				delete answers.module[k];
+			});
+
+			_.assign(
+				answers,
+				promptUtil.formatThemeletSelection(
+					answers.module,
+					this.config.selectedModules
+				)
+			);
+		}
+	}
+
+	_prompt() {
+		const questions = [];
+
+		if (this.themelet) {
+			questions.push({
+				choices: _.bind(this._getModuleChoices, this),
+				filter: _.bind(this._filterModule, this),
+				message: 'Select the themelets you want to keep:',
+				name: 'module',
+				type: 'checkbox',
+				when: _.bind(this._getModuleWhen, this),
+			});
+		}
+
+		questions.push({
+			message: 'Enter the URL for the package: (Empty to skip)',
+			name: 'packageURL',
+			type: 'input',
+		});
+
+		inquirer.prompt(questions, this._afterPrompt.bind(this));
 	}
 
 	_validatePackageURL(packageURL, _answers) {
@@ -39,25 +136,6 @@ class URLPackagePrompt {
 		}
 
 		return true;
-	}
-
-	_afterPrompt(answers) {
-		const config = themeFinder.getLiferayThemeModuleFromURL(
-			answers.packageURL
-		);
-
-		answers.modules = {
-			[config.name]: {...config, __packageURL__: answers.packageURL},
-		};
-
-		if (this.themelet) {
-			answers.module = {[config.name]: true};
-			answers.addedThemelets = [config.name];
-		} else {
-			answers.module = config.name;
-		}
-
-		this.done(answers);
 	}
 }
 
