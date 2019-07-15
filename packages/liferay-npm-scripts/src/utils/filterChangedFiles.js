472d90ac4afae1de6b358218475b08344751fda1
Greg Hurrell <greg.hurrell@liferay.com>
2019-07-15 13:40:49 +0200

    refactor(scripts): compute changed file list internally
    Given what I discovered in the previous commit about limits on
    environment variable sizes on Windows, I think it's better to compute
    the changed file list internally.
    
    Deleted the now-irrelevant tests. Will loop back later with something
    automated once I've tested this in liferay-portal.

--- packages/liferay-npm-scripts/src/utils/filterChangedFiles.js
+++ packages/liferay-npm-scripts/src/utils/filterChangedFiles.js
@@ -6,24 +6,45 @@
 
 const path = require('path');
 
+const git = require('./git');
+
 /**
  * In the context of liferay-portal, we may want to run against a subset of
  * eligible files (eg. files changed on the current branch). To achieve this, we
- * check the LIFERAY_NPM_SCRIPTS_CHANGED_FILES environment variable for a list
- * of selections.
+ * check the LIFERAY_NPM_SCRIPTS_WORKING_BRANCH_NAME environment variable and if
+ * it is set, filter the `files` list to contain only files changed with respect
+ * to that branch (usually "master", but may also be "master-private".
  *
  * If the variable is not set, the `files` list is return unchanged.
  */
 function filterChangedFiles(files) {
-	const changedFiles = process.env.LIFERAY_NPM_SCRIPTS_CHANGED_FILES;
+	const upstream = process.env.LIFERAY_NPM_SCRIPTS_WORKING_BRANCH_NAME;
 
-	if (changedFiles === undefined) {
+	if (upstream === undefined) {
 		return files;
 	}
 
+	const topLevel = git('rev-parse', '--show-toplevel');
+
+	const mergeBase = git('merge-base', 'HEAD', upstream);
+
+	const changedFiles = git(
+		'diff',
+		'-z',
+		'--diff-filter=ACMR',
+		'--name-only',
+		mergeBase,
+		'HEAD'
+	)
+		.split(/\0/)
+		.map(file => {
+			return file ? path.join(topLevel, file) : file;
+		})
+		.filter(Boolean);
+
 	const set = new Set();
 
-	changedFiles.split(',').forEach(changedFile => {
+	changedFiles.forEach(changedFile => {
 		if (path.isAbsolute(changedFile)) {
 			set.add(changedFile);
 		} else {
