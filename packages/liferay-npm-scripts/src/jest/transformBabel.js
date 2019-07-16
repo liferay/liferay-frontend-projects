liferay/liferay-frontend-projects

commit 3c5a9c5c217e167394d1771136d8002702919bb4

feat(scripts): avoid problems caused by cross-project Soy dependencies
We saw this in "segments-web" when we moved `debounce()` into
"frontend-js-web": the `index.es.js` file from "frontend-js-web" ends up
importing `Dialog.soy`, and that blows up during test runs because:

- index.es.js is in the "src" directory.
- the compiled soy templates (eg. "Foo.soy.js", corresponding to
  "Foo.soy") are in the build directory and not the "src" directory.
- even though previous commits set up the mappings necessary for Jest to
  locate modules imported from other projects, they all point at "src"
  and not the build directory.
- we can't point at the build directory instead because that generally
  has a bunch of transforms applied that we don't want in the context of
  the test suite (eg. `Liferay.Loader.define` module wrappers and such),
  and we don't have any guarantee that we've run (nor would we want to
  run) builds of other projects in order to run tests for a different
  project.

So, we add a transform that makes these cross-project (often transitive)
imports of Soy templates harmless. As the comment in-line explains, in
projects that actually wish to exercise their own Soy templates, we
already have mappings configured; example here:

https://github.com/liferay/liferay-portal/blob/7b26f1191c7737228c9fd6e4b41a125d03efbaf1/modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/package.json#L41

packages/liferay-npm-scripts/src/jest/transformBabel.js
@@ -1,0 +1,9 @@
+/**
+ * © 2019 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+module.exports = require('babel-jest').createTransformer({
+	presets: ['@babel/preset-env']
+});

packages/liferay-npm-scripts/src/config/jest.config.js
@@ -1,0 +1,22 @@
+/**
+ * © 2019 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+const path = require('path');
+
+module.exports = {
+	globals: {
+		Liferay: {}
+	},
+	coverageDirectory: 'build/coverage',
+	modulePathIgnorePatterns: ['/__fixtures__/', '/build/', '/classes/'],
+	testMatch: ['**/test/**/*.js'],
+	testResultsProcessor: 'liferay-jest-junit-reporter',
+	testURL: 'http://localhost',
+	transform: {
+		'\\.soy$': path.join(__dirname, '..', 'jest', 'transformSoy.js'),
+		'.+': path.join(__dirname, '..', 'jest', 'transformBabel.js')
+	}
+};

packages/liferay-npm-scripts/src/jest/transformSoy.js
@@ -1,0 +1,32 @@
+/**
+ * © 2019 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/**
+ * This transform allows tests to require files of the form "Foo.soy"
+ * from other modules without blowing up, by substituting no-op
+ * templates that can be safely passed to `Soy.register()`.
+ *
+ * In tests that exercise their own Soy templates, we already build
+ * corresponding "Foo.soy.js" files and have mappings configured so that Jest
+ * can resolve them.
+ */
+module.exports = {
+	canInstrument: true,
+
+	getCacheKey(_src, file, _configString, _options) {
+		return file;
+	},
+
+	process(_src, _file) {
+		return `
+			const render = () => {};
+
+			const templates = {render};
+
+			module.exports = templates;
+		`;
+	}
+};

packages/liferay-npm-scripts/src/utils/getMergedConfig.js
@@ -81,7 +81,7 @@ function getMergedConfig(type, property) {
 
 		case 'jest':
 			mergedConfig = deepMerge([
-				require('../config/jest'),
+				require('../config/jest.config'),
 				require('../utils/getJestModuleNameMapper')(),
 				getUserConfig('jest')
 			]);

commit c9b65095f337a8386d3738e1c96c2198b40ca509

fix(scripts): make async work in tests again
Fixes the problem where any use of `async` in tests would lead to:

    ReferenceError: regeneratorRuntime is not defined

Thanks to `@babel/preset-env` and our use of a recent "targets"
configuration for test files, we should be able to use `async` without
any transforms (our version of Node, v10.15.1, supports `async`
directly), but I believe I broke that in 3c5a9c5c217e167394d1771136d8.

That commit had the effect of making partially (but not totally)
neutralizing our Babel config because our overrides (which explicitly
set the target to `"node": "10.15") were getting blowing away by the
transform which just uses vanilla `@babel/preset-env` without options.

This change here makes sure that we always pass the merged user config
directly to Jest's `createTransformer()` call. It's also possible that
we could get rid of this `transformBabel.js` transform entirely, but it
does provide us with a useful additional introspection point for
debugging purposes, so I suggest we keep it around, at least for now.

Test plan: apply this change in liferay-portal and see that `async` can
again be used in the tests.

packages/liferay-npm-scripts/src/jest/transformBabel.js
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-module.exports = require('babel-jest').createTransformer({
-	presets: ['@babel/preset-env']
-});
+const fs = require('fs');
+
+const config = JSON.parse(fs.readFileSync('.babelrc').toString());
+
+module.exports = require('babel-jest').createTransformer(config);