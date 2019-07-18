18f47b1b9c2e5e4a60698010754e125237efd838
Greg Hurrell <greg.hurrell@liferay.com>
2019-07-18 12:12:42 +0200

    refactor(scripts): extract run() from git()
    That way I can use it to run `yarn` (in the next commit).

--- packages/liferay-npm-scripts/src/utils/git.js
+++ packages/liferay-npm-scripts/src/utils/git.js
@@ -4,33 +4,13 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-const {spawn} = require('cross-spawn');
+const run = require('./run');
 
 /**
  * Convenience helper for running Git commands.
  */
 function git(...args) {
-	const command = `git ${args.join(' ')}`;
-
-	const {error, signal, status, stdout} = spawn.sync('git', args);
-
-	if (error) {
-		throw error;
-	}
-
-	if (signal) {
-		throw new Error(
-			`git(): command \`${command}\` exited due to signal ${signal}`
-		);
-	}
-
-	if (status) {
-		throw new Error(
-			`git(): command \`${command}\` exited with status ${status}`
-		);
-	}
-
-	return stdout.toString().trim();
+	return run('git', ...args);
 }
 
 module.exports = git;
