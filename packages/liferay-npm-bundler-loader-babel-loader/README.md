85d47ed69ee67e15d953f9699cc72de8abde3bdd
Iván Zaera Avellón <izaera@gmx.es>
2019-08-12 11:19:21 +0200

    Fixes #303: Add a Babel loader
    This loader runs Babel on JS files so that the project doesn't need any
    extra build step invoking Babel CLI.
    
    For now, this loader is a bit experimental in the sense that it is just
    build sugar, but in the future we expect possible build optimizations
    given that the bundler will be in control of the whole build process.

---
+++ packages/liferay-npm-bundler-loader-babel-loader/README.md
@@ -0,0 +1,38 @@
+# liferay-npm-bundler-loader-babel-loader
+
+> A liferay-npm-bundler loader that runs Babel on source files.
+
+## Installation
+
+```sh
+npm install --save-dev liferay-npm-bundler-loader-babel-loader
+```
+
+## Usage
+
+In order to use this loader you must declare a rule in your module's `.npmbundlerrc` file:
+
+```json
+{
+	"packages": {
+		"/": {
+			"rules": [
+				{
+					"files": ["**/*.js"],
+					"use": [
+						{
+							"loader": "babel-loader",
+							"options": {
+								"presets": ["env", "react"]
+							}
+						}
+					]
+				}
+			]
+		}
+	}
+}
+```
+
+See the project's wiki for more information on
+[how to use build rules](https://github.com/liferay/liferay-js-toolkit/wiki/How-to-use-build-rules).
---
+++ packages/liferay-npm-bundler-loader-babel-loader/src/index.js
@@ -0,0 +1,104 @@
+/**
+ * © 2017 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+import * as babel from 'babel-core';
+import resolveModule from 'resolve';
+
+/**
+ * @param {object} context loader's context
+ * @param {object} config a .babelrc like configuration object
+ * @return {string} the processed file content
+ */
+export default function(context, config) {
+	const {content, filePath, sourceMap} = context;
+
+	const babelConfig = Object.assign(
+		{
+			filenameRelative: filePath,
+			inputSourceMap: sourceMap,
+		},
+		config
+	);
+
+	// Tune babel config
+	babelConfig.babelrc = false;
+	babelConfig.only = '**/*';
+	if (babelConfig.sourceMaps === undefined) {
+		babelConfig.sourceMaps = true;
+	}
+
+	// Intercept presets and plugins to load them from project
+	babelConfig.plugins = loadBabelPlugins(
+		babelConfig.presets || [],
+		babelConfig.plugins || []
+	);
+	delete babelConfig.presets;
+
+	const result = babel.transform(content, babelConfig);
+
+	context.sourceMap = result.map;
+	context.artifacts[`${filePath}.map`] = JSON.stringify(result.map);
+
+	return result.code;
+}
+
+/**
+ * Load Babel plugins from a given array of presets and plugins.
+ * @param {Array} presets an array of Babel preset names as defined by .babelrc
+ * @param {Array} plugins an array of Babel plugins names as defined by .babelrc
+ * @return {Array} the instantiated Babel plugins
+ */
+function loadBabelPlugins(presets, plugins) {
+	return []
+		.concat(
+			...presets.map(preset => {
+				let presetModule, presetName;
+
+				try {
+					presetName = `babel-preset-${preset}`;
+					presetModule = projectRequire(presetName);
+				} catch (err) {
+					presetName = preset;
+					presetModule = projectRequire(presetName);
+				}
+
+				if (presetModule === undefined) {
+					throw new Error(
+						`Babel preset '${presetName}' does not export anything`
+					);
+				}
+
+				if (presetModule)
+					if (presetModule.default !== undefined) {
+						presetModule = presetModule.default;
+					}
+
+				if (
+					presetModule.plugins === undefined &&
+					typeof presetModule !== 'function'
+				) {
+					throw new Error(
+						`Babel preset '${presetName}' does not export a ` +
+							'valid function or plugins object'
+					);
+				}
+
+				return presetModule.plugins || presetModule().plugins;
+			})
+		)
+		.concat(plugins);
+}
+
+/**
+ * Require a module using the configured plugins directory.
+ * @param {String} module a module name
+ * @return {Object} the required module object
+ */
+export function projectRequire(module) {
+	return require(resolveModule.sync(module, {
+		basedir: '.',
+	}));
+}
