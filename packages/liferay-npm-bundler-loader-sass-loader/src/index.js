44f27546d7e03fbc0df5390d543dde46c011d452
Iván Zaera Avellón <izaera@gmx.es>
2019-08-12 13:05:37 +0200

    Fixes #303: Tune SASS loader
    Make it simpler and only include `sass` dependency for failover.

--- packages/liferay-npm-bundler-loader-sass-loader/src/index.js
+++ packages/liferay-npm-bundler-loader-sass-loader/src/index.js
@@ -4,50 +4,59 @@
  * SPDX-License-Identifier: LGPL-3.0-or-later
  */
 
+import resolveModule from 'resolve';
+
 /**
  * @param {object} context loader's context
+ * @param {object} options loader's options
  * @return {string} the processed file content
  */
-export default function(context) {
-	const {content, filePath} = context;
-	const self = this;
-
-	function getDefaultRenderer() {
-		let sassImplPkg = 'node-sass';
-
-		try {
-			require.resolve('node-sass');
-		} catch (error) {
-			try {
-				require.resolve('sass');
-				sassImplPkg = 'sass';
-			} catch (ignoreError) {
-				sassImplPkg = 'node-sass';
-			}
-		}
-
-		return require(sassImplPkg);
-	}
-
-	function getRenderer() {
-		const options = self.options || {};
-		const impl = options.implementation;
-
-		return impl ? require(impl) : getDefaultRenderer();
-	}
-
-	const renderOptions = Object.assign(
-		{
-			data: content,
-		},
-		self.options
-	);
+export default function(context, options) {
+	const {content} = context;
 
 	const renderer = getRenderer();
 
-	const result = renderer.renderSync(renderOptions);
+	const result = renderer.renderSync(
+		Object.assign(
+			{
+				data: content,
+			},
+			options
+		)
+	);
 
-	const css = result.css.toString();
+	return result.css.toString();
+}
 
-	return css;
+/**
+ * Get configured SASS renderer.
+ * @return {*}
+ */
+function getRenderer() {
+	let rendererFilePath = tryResolve('node-sass', '.');
+
+	if (rendererFilePath === undefined) {
+		rendererFilePath = tryResolve('sass', '.');
+	}
+
+	if (rendererFilePath === undefined) {
+		rendererFilePath = tryResolve('sass');
+	}
+
+	return require(rendererFilePath);
+}
+
+/**
+ * Resolve a module name into its file path.
+ * @param {string} moduleName module name
+ * @param {string} basedir optional base directory
+ */
+function tryResolve(moduleName, basedir) {
+	try {
+		return resolveModule.sync(moduleName, {
+			basedir,
+		});
+	} catch (err) {
+		return undefined;
+	}
 }
