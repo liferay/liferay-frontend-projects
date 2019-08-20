dddbbe709e82306a0a3d2df0f028896fb1ffcba4
Iván Zaera Avellón <izaera@gmx.es>
2019-08-20 14:25:13 +0200

    Fixes #303: Add a loader to handle .json files
    We apply the same technique of style-loader to JSON files so that people
    can require JSON files in their projects.

---
+++ packages/liferay-npm-bundler-loader-json-loader/src/__tests__/index.test.js
@@ -0,0 +1,59 @@
+/**
+ * © 2017 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+import PluginLogger from 'liferay-npm-build-tools-common/lib/plugin-logger';
+import loader from '../index';
+
+const TEST_JSON_OBJECT = {
+	name: 'backslash-inserter',
+	version: '1.0.0',
+	description: 'A module that inserts \\ characters in files.',
+	main: 'index.js',
+	devDependencies: {
+		'babel-cli': '^6.24.1',
+		'babel-preset-es2015': '^6.24.1',
+	},
+	dependencies: {
+		'is-array': '1.0.0',
+	},
+};
+
+it('logs results correctly', () => {
+	const context = {
+		content: JSON.stringify(TEST_JSON_OBJECT, null, 2),
+		filePath: 'package.json',
+		log: new PluginLogger(),
+		extraArtifacts: {},
+	};
+
+	loader(context, {});
+
+	expect(context.log.messages).toEqual([
+		{
+			level: 'info',
+			source: 'json-loader',
+			things: ['Generated JavaScript JSON module'],
+		},
+	]);
+});
+
+it('correctly generates JS module', () => {
+	const context = {
+		content: JSON.stringify(TEST_JSON_OBJECT, null, 2),
+		filePath: 'package.json',
+		log: new PluginLogger(),
+		extraArtifacts: {},
+	};
+
+	const result = loader(context, {});
+
+	expect(result).toBeUndefined();
+
+	expect(Object.keys(context.extraArtifacts)).toEqual(['package.json.js']);
+	expect(eval(context.extraArtifacts['package.json.js'])).toEqual(
+		TEST_JSON_OBJECT
+	);
+});
