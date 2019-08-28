33aeb1b4bd853f5140e88f611b5bea6ba73466fd
Iván Zaera Avellón <izaera@gmx.es>
2019-08-28 12:21:09 +0200

    Fixes #303: Enhance link-js-toolkit
    Make it more failsafe by running `yarn install` without the JS toolkit
    dependencies in the package.json and then restoring and linking them to
    the local yarn registry.

--- resources/devtools/link-js-toolkit/link-js-toolkit.js
+++ resources/devtools/link-js-toolkit/link-js-toolkit.js
@@ -5,6 +5,7 @@
  * SPDX-License-Identifier: LGPL-3.0-or-later
  */
 
+const clone = require('clone');
 const fs = require('fs');
 const path = require('path');
 const readJsonSync = require('read-json-sync');
@@ -23,6 +24,10 @@ deps = [
 ].filter(dep => dep.startsWith('liferay-npm'));
 
 // Grab dependencies from node_modules
+try {
+	fs.mkdirSync('node_modules');
+} catch (err) {}
+
 const dirs = fs
 	.readdirSync('node_modules')
 	.filter(dir => dir.startsWith('liferay-npm'));
@@ -32,12 +37,31 @@ deps.push(...dirs);
 // Deduplicate dependencies
 deps = Array.from(new Set(deps));
 
+// Install all but JS toolkit dependencies
+const modPkgJson = clone(pkgJson);
+
+deps.forEach(dep => {
+	delete modPkgJson.dependencies[dep];
+	delete modPkgJson.devDependencies[dep];
+});
+
+fs.writeFileSync('package.json', JSON.stringify(modPkgJson, null, 2));
+
+console.log('\n--- Installing dependencies not from JS Toolkit\n');
+try {
+	yarn('install');
+} finally {
+	fs.writeFileSync('package.json', JSON.stringify(pkgJson, null, 2));
+}
+
 // Link dependencies with yarn
+console.log('\n--- Linking dependencies from JS Toolkit\n');
 deps.forEach(dep => {
 	yarn('link', dep);
 });
 
 // Link binaries by hand (yarn doesn't do it)
+console.log('\n--- Linking binaries from JS Toolkit\n');
 deps.forEach(dep => {
 	const depPkgJson = readJsonSync(
 		path.join('.', 'node_modules', dep, 'package.json')
