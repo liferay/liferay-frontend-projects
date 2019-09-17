e9114a42dccf178a0b300da19cbcc57828a28234
Greg Hurrell <greg@hurrell.net>
2019-09-17 14:41:25 +0200

    feat: activate JSP formatting as part of standard formatting runs
    Note that we can drop the "simple glob" check because it is no longer
    required after:
    
    https://github.com/liferay/liferay-npm-tools/pull/247

--- packages/liferay-npm-scripts/test/utils/getMergedConfig.js
+++ packages/liferay-npm-scripts/test/utils/getMergedConfig.js
@@ -6,41 +6,12 @@
 
 const getMergedConfig = require('../../src/utils/getMergedConfig');
 
-/**
- * Helper function that overrides `getUserConfig()` for the duration of the
- * callback.
- */
-function withUserConfig(mockConfig = {}, callback) {
-	// This line required due to: https://github.com/facebook/jest/issues/7863
-	jest.resetModules();
-
-	jest.isolateModules(() => {
-		jest.mock('../../src/utils/getUserConfig', () => {
-			return () => {
-				return mockConfig;
-			};
-		});
-
-		const getMergedConfig = require('../../src/utils/getMergedConfig');
-
-		callback(getMergedConfig);
-	});
-}
-
 describe('getMergedConfig()', () => {
 	it('rejects invalid types', () => {
 		expect(() => getMergedConfig('foo')).toThrow('not a valid config');
 	});
 
 	describe('"npmscripts" config', () => {
-		it('rejects non-simple globs', () => {
-			withUserConfig({fix: ['**/*.{js,scss}']}, getMergedConfig => {
-				expect(() => getMergedConfig('npmscripts')).toThrow(
-					'glob "**/*.{js,scss}" must end with a simple extension'
-				);
-			});
-		});
-
 		it('returns a specific property when requested', () => {
 			expect(getMergedConfig('npmscripts', 'check')).toEqual(
 				expect.any(Array)
