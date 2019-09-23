d8336edcb7e2ee711ceda42cbbbdc44ea68a2ba6
Greg Hurrell <greg@hurrell.net>
2019-09-23 23:17:01 +0200

    feat: make Jest descriptions better looking
    From:
    
        FAIL  plugins/eslint-plugin-liferay/tests/lib/rules/group-imports.js
          ● group-imports › invalid › \u000a\u0009\u0009\u0009\u0009const del = require('del');\u000a\u0009\u0009\u0009\u0009const fs = require('fs-extra');\u000a\u0009\u0009\u0009\u0009const _ = require('lodash');\u000a\u0009\u0009\u0009\u0009const path = require('path');\u000a\u0009\u0009\u0009\u0009const plugins = require('gulp-load-plugins')();\u000a\u0009\u0009\u0009\u0009const replace = require('gulp-replace-task');\u000a\u0009\u0009\u0009\u0009const through = require('through2');\u000a\u0009\u0009\u0009\u0009const PluginError = require('plugin-error');\u000a\u0009\u0009\u0009
    
    To:
    
        FAIL  plugins/eslint-plugin-liferay/tests/lib/rules/group-imports.js
          ● group-imports › invalid › \n\t\t\t\tconst del = require('del');\n\t\t\t\tconst fs = require('fs-extra');\n\t\t\t\tconst _ = require('lodash');\n\t\t\t\tconst path = require('path');\n\t\t\t\tconst plugins = require('gulp-load-plugins')();\n\t\t\t\tconst replace = require('gulp-replace-task');\n\t\t\t\tconst through = require('through2');\n\t\t\t\tconst PluginError = require('plugin-error');\n\t\t\t
    
    Still not the prettiest thing in the world, but it is an improvement.

--- scripts/setupJest.js
+++ scripts/setupJest.js
@@ -9,4 +9,18 @@
 const {RuleTester} = require('eslint');
 
 RuleTester.describe = describe;
-RuleTester.it = it;
+
+RuleTester.it = function(description, method) {
+	const prettifiedDescription = description.replace(
+		/\\u0009|\\u000a/g,
+		match => {
+			if (match === '\\u0009') {
+				return '\\t';
+			} else {
+				return '\\n';
+			}
+		}
+	);
+
+	it(prettifiedDescription, method);
+};
