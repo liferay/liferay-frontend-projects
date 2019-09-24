b39671c10376a9cbbffb5b4a7741d3cdc4df54c1
Greg Hurrell <greg.hurrell@liferay.com>
2019-09-24 12:15:56 +0200

    refactor(destructure-requires): reduce Pyramid of Doom™ a bit

--- plugins/eslint-plugin-liferay/lib/rules/destructure-requires.js
+++ plugins/eslint-plugin-liferay/lib/rules/destructure-requires.js
@@ -22,70 +22,41 @@ module.exports = {
 
 					const code = context.getSourceCode();
 
+					let replacement;
+
 					if (declarator.id.type === 'Identifier') {
 						const binding = declarator.id.name;
 
 						if (binding === property) {
 							// ie. `thing = require('foo').thing`
 							// --> `{thing} = require('foo')`
-							context.report({
-								fix: fixer => {
-									return [
-										fixer.replaceText(
-											node.parent,
-											code.getText(node)
-										),
-										fixer.replaceText(
-											declarator.id,
-											`{${property}}`
-										),
-									];
-								},
-								message: DESCRIPTION,
-								node: declarator,
-							});
+							replacement = `{${property}}`;
 						} else {
 							// ie. `alias = require('foo').thing`
 							// --> `{thing: alias} = require('foo')`
-							context.report({
-								fix: fixer => {
-									return [
-										fixer.replaceText(
-											node.parent,
-											code.getText(node)
-										),
-										fixer.replaceText(
-											declarator.id,
-											`{${property}: ${binding}}`
-										),
-									];
-								},
-								message: DESCRIPTION,
-								node: declarator,
-							});
+							replacement = `{${property}: ${binding}}`;
 						}
 					} else if (declarator.id.type === 'ObjectPattern') {
 						// ie. `{pattern} = require('foo').thing`
 						// --> `{thing: {pattern}} = require('foo')`
-						context.report({
-							fix: fixer => {
-								return [
-									fixer.replaceText(
-										node.parent,
-										code.getText(node)
-									),
-									fixer.replaceText(
-										declarator.id,
-										`{${property}: ${code.getText(
-											declarator.id
-										)}}`
-									),
-								];
-							},
-							message: DESCRIPTION,
-							node: declarator,
-						});
+						replacement = `{${property}: ${code.getText(
+							declarator.id
+						)}}`;
 					}
+
+					context.report({
+						fix: fixer => {
+							return [
+								fixer.replaceText(
+									node.parent,
+									code.getText(node)
+								),
+								fixer.replaceText(declarator.id, replacement),
+							];
+						},
+						message: DESCRIPTION,
+						node: declarator,
+					});
 				}
 			},
 		};
