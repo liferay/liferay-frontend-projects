e9fc4f343d8a0b2b198b89c87d841c2fde4d9874
Iván Zaera Avellón <izaera@gmx.es>
2019-09-30 17:09:16 +0200

    Fixes #429: Add QA step for angular-cli

---
+++ scripts/qa/resources.js
@@ -0,0 +1,97 @@
+/**
+ * © 2017 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
+const qaDir = path.join(__dirname, '..', '..', 'qa');
+const samplesDir = path.join(qaDir, 'samples');
+const packagesDir = path.join(samplesDir, 'packages');
+
+const currentSDKVersion = require(path.join(
+	'..',
+	'..',
+	'packages',
+	'liferay-npm-bundler',
+	'package.json'
+)).version;
+
+const lernaPath = path.join(
+	__dirname,
+	'..',
+	'..',
+	'node_modules',
+	'lerna',
+	'cli.js'
+);
+
+const liferayDir = findLiferayDir();
+
+const liferayJsAdaptGeneratorPath = path.join(
+	__dirname,
+	'..',
+	'..',
+	'packages',
+	'generator-liferay-js',
+	'generators',
+	'adapt',
+	'index.js'
+);
+
+const liferayJsGeneratorPath = path.join(
+	__dirname,
+	'..',
+	'..',
+	'packages',
+	'generator-liferay-js',
+	'generators',
+	'app',
+	'index.js'
+);
+
+const linkJsToolkitPath = path.join(
+	__dirname,
+	'..',
+	'..',
+	'resources',
+	'devtools',
+	'link-js-toolkit',
+	'link-js-toolkit.js'
+);
+
+const yoPath = path.join(__dirname, '..', '..', 'node_modules', '.bin', 'yo');
+
+module.exports = {
+	currentSDKVersion,
+	lernaPath,
+	liferayDir,
+	liferayJsAdaptGeneratorPath,
+	liferayJsGeneratorPath,
+	linkJsToolkitPath,
+	packagesDir,
+	qaDir,
+	samplesDir,
+	yoPath,
+};
+
+function findLiferayDir() {
+	// Find Liferay installation directory
+	let liferayDir = path.join(__dirname, '..', '..', 'liferay');
+
+	try {
+		const json = JSON.parse(
+			fs.readFileSync(
+				path.join(os.homedir(), '.generator-liferay-js.json')
+			)
+		);
+		liferayDir = json.answers['*'].liferayDir;
+	} catch (err) {
+		// swallow
+	}
+
+	return liferayDir;
+}
