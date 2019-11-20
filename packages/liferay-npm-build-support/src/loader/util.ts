eed9fe6ff4bb06805aa899d497ab68ca1456d9bf
Iván Zaera Avellón <izaera@gmx.es>
2019-11-20 10:19:41 +0100

    Fixes #432: Unify webpack hash logic
    We use it in two places, so make it common. Also, support more
    scenarios (needed for create-react-app adaptation).

--- packages/liferay-npm-build-support/src/loader/util.ts
+++ packages/liferay-npm-build-support/src/loader/util.ts
@@ -31,3 +31,29 @@ export function replaceTokens<T>(
 
 	return options;
 }
+
+/**
+ * Remove a webpack hash (a hex number surrounded by dots near the end of the
+ * file name) if present from a file path.
+ */
+export function removeWebpackHash(filePath: string): string {
+	const filePathParts = filePath.split('.');
+
+	let hashIndex;
+
+	for (hashIndex = filePathParts.length - 1; hashIndex >= 0; hashIndex--) {
+		const filePathPart = filePathParts[hashIndex];
+
+		if (filePathPart.match(/^[0-9a-fA-F]+$/)) {
+			break;
+		}
+	}
+
+	if (hashIndex == -1) {
+		return filePath;
+	}
+
+	filePathParts.splice(hashIndex, 1);
+
+	return filePathParts.join('.');
+}
