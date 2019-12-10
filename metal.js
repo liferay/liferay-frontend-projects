ba83d044684a101d9bef407fcce341000535413d
Greg Hurrell <greg.hurrell@liferay.com>
2019-12-10 16:04:33 +0100

    fix!: exclude react/no-unknown-property from metal preset
    Seems like this is a common source of false positive in Metal projects
    because Metal uses "class" while React expects "className":
    
        Unknown property 'class' found, use 'className' instead
        react/no-unknown-property

--- metal.js
+++ metal.js
@@ -18,7 +18,13 @@ const config = {
 		/**
 		 * @see https://github.com/yannickcr/eslint-plugin-react
 		 */
+
+		// In Metal, string refs are the only kind of refs.
 		'react/no-string-refs': 'off',
+
+		// In Metal, common property names like "class" will cause this rule to
+		// fire (because React expects "className").
+		'react/no-unknown-property': 'off',
 	},
 	settings: {
 		react: {
