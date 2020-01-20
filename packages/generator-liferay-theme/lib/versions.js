cd6f5ca624081ad775da18e246755197c44f2d79
Iván Zaera Avellón <izaera@gmx.es>
2020-01-20 16:52:47 +0100

    feat: add module to hold portal versions information (#419)
    This module contains version information that is exclusively used by the
    generators. Otherwise, it is stored in the devDependencies.js module of
    the liferay-theme-tasks package.

---
+++ packages/generator-liferay-theme/lib/versions.js
@@ -0,0 +1,20 @@
+/**
+ * © 2017 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+/* eslint-disable quote-props */
+module.exports = {
+	// Supported portal versions
+	supported: ['7.3', '7.2'],
+
+	// Supported theme versions indexed by portal version
+	theme: {
+		classic: {
+			'7.2': '^2.0.0',
+			'7.3': '^3.0.0',
+		},
+	},
+};
+/* eslint-enable quote-props */
