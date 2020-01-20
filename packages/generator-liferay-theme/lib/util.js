56f6b6ae9c0644c9aad3438c38cda0da77f3716e
Iván Zaera Avellón <izaera@gmx.es>
2020-01-20 16:46:24 +0100

    chore: refactor utility modules (#419)
    Put all helper methods into one module and out of generators folder,
    which is better dedicated to just the high level generator code.
    
    Also remove unused isString method.

---
+++ packages/generator-liferay-theme/lib/util.js
@@ -0,0 +1,111 @@
+/**
+ * © 2017 Liferay, Inc. <https://liferay.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+const gulp = require('gulp');
+const {
+	info,
+	print,
+	success,
+	title,
+} = require('liferay-npm-build-tools-common/lib/format');
+
+const pkgJson = require('../package.json');
+const versions = require('./versions');
+
+const FIRST_SUPPORTED_VERSION =
+	versions.supported[versions.supported.length - 1];
+const LAST_SUPPORTED_VERSION = versions.supported[0];
+
+/**
+ * Returns a normalized "kebab case" version of the supplied string, suitable
+ * for use as a theme (or themelet, or layout) name.
+ */
+function normalizeName(name) {
+	return splitWords(name.replace(/[^a-z0-9-]/gi, ''))
+		.join('-')
+		.toLowerCase();
+}
+
+/**
+ * Run `gulp init` after successful creation of a project.
+ */
+function runGulpInit() {
+	print(
+		'\n',
+		success`
+		The project has been created successfully.
+		`,
+		info`
+		Now we will invoke {gulp init} for you, to configure your deployment
+		strategy. 
+		
+		Remember, that you can change your answers whenever you want by 
+		running {gulp init} again.
+		`
+	);
+
+	// We cannot load this before the project is created because it crashes
+	const liferayThemeTasks = require('liferay-theme-tasks');
+
+	liferayThemeTasks.registerTasks({gulp});
+
+	gulp.start('init');
+}
+
+/**
+ * Show welcome message
+ * @param {Generator} generator
+ */
+function sayHello(generator) {
+	const generatorNamespace = generator.options.namespace;
+
+	print(
+		title`
+		
+
+		Welcome to the splendid Themes SDK generator!
+		
+		`,
+		info`
+		This version of the Themes SDK (${pkgJson.version}) supports Liferay DXP 
+		and Portal CE from ${FIRST_SUPPORTED_VERSION} to ${LAST_SUPPORTED_VERSION}.
+
+		For older versions, please use v8 of the toolkit:
+	
+			> npm install -g generator-liferay-theme@^8.0.0 ↩
+			> yo ${generatorNamespace} ↩
+
+		`
+	);
+}
+
+/**
+ * Returns a "snake case" version of `name` (eg. "foo_bar_baz").
+ */
+function snakeCase(name) {
+	return splitWords(name)
+		.join('_')
+		.toLowerCase();
+}
+
+/**
+ * Splits `input` at word boundaries.
+ *
+ * eg. "foo bar" -> ["foo", "bar"]
+ *     "foo-bar" -> ["foo", "bar"]
+ *     "foo_bar" -> ["foo", "bar"]
+ *     "FooBar" -> ["foo", "bar"]
+ */
+function splitWords(input) {
+	return input.trim().split(/[ _-]|(?<=[a-z])(?=[A-Z])/);
+}
+
+module.exports = {
+	normalizeName,
+	runGulpInit,
+	sayHello,
+	snakeCase,
+};
