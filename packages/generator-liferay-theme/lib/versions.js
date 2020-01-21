aba6e1f65ca7ee4551fcb54081fef9efc9a7705c
Iván Zaera Avellón <izaera@gmx.es>
2020-01-21 14:27:27 +0100

    chore: no need to disable eslint for this (#419)
    Given that JS implicitly converts the number keys to string, there's no
    need to quote them.

--- packages/generator-liferay-theme/lib/versions.js
+++ packages/generator-liferay-theme/lib/versions.js
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: MIT
  */
 
-/* eslint-disable quote-props */
 module.exports = {
 	// Supported portal versions
 	supported: ['7.3', '7.2'],
@@ -12,9 +11,8 @@ module.exports = {
 	// Supported theme versions indexed by portal version
 	theme: {
 		classic: {
-			'7.2': '^2.0.0',
-			'7.3': '^3.0.0',
+			7.2: '^2.0.0',
+			7.3: '^3.0.0',
 		},
 	},
 };
-/* eslint-enable quote-props */
