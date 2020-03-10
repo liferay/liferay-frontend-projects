6ec68991399d61a16580cdc61db70a0860737501
Iván Zaera Avellón <izaera@gmx.es>
2020-03-10 17:03:28 +0100

    chore: Don't clean directories outside of project
    This is to prevent people from shooting at their feet.

--- scripts/clean.js
+++ scripts/clean.js
@@ -3,17 +3,48 @@
  * SPDX-License-Identifier: LGPL-3.0-or-later
  */
 
+const path = require('path');
 const fs = require('fs-extra');
+
+const {abort} = require('./util/report');
+
+function hasValidOutDir(json) {
+	const {compilerOptions} = json;
+
+	if (!compilerOptions) {
+		return false;
+	}
+
+	const {outDir} = compilerOptions;
+
+	if (!outDir) {
+		return false;
+	}
+
+	const absOutDir = path.resolve(outDir);
+	const projectDir = path.resolve('.');
+
+	if (path.relative(projectDir, absOutDir).startsWith('../')) {
+		return false;
+	}
+
+	return true;
+}
 
 try {
 	const json = fs.readJSONSync('tsconfig.json');
 
-	if (json.compilerOptions && json.compilerOptions.outDir) {
-		fs.removeSync(json.compilerOptions.outDir);
+	if (!hasValidOutDir(json)) {
+		abort(
+			`Invalid compilerOptions.outDir found in ` +
+				`${path.basename(path.resolve('.'))}/tsconfig.json`
+		);
 	}
+
+	fs.removeSync(json.compilerOptions.outDir);
 } catch (err) {
 	if (err.code !== 'ENOENT') {
-		throw err;
+		abort(err.toString());
 	}
 }
 
