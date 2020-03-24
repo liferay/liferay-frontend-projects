f215256429622b6ca6b5d7d58224e30670af16a6
Iván Zaera Avellón <izaera@gmx.es>
2020-03-24 11:04:32 +0100

    chore: Add release:snapshot script
    The release-snapshot code is copied from JS Toolkit. Maybe we should make a
    common lib in the future but given that projects may have subtle differences and
    that it's not so critical to have the release scripts aligned, we may leave it
    like this for now (copy&paste) and decide what to do in the future.

---
+++ scripts/util/run.js
@@ -0,0 +1,56 @@
+/**
+ * SPDX-FileCopyrightText: © 2017 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+const execa = require('execa');
+
+async function run(cmd, ...args) {
+	let opts = {};
+
+	if (typeof args[args.length - 1] === 'object') {
+		opts = args.pop();
+	}
+
+	const {stderr, stdout} = await execa(cmd, args, opts);
+
+	stdout.stderr = stderr;
+
+	return stdout;
+}
+
+run.pipe = async (cmd, ...args) => {
+	let opts = {};
+
+	if (typeof args[args.length - 1] === 'object') {
+		opts = args.pop();
+	}
+
+	const promise = execa(cmd, args, opts);
+
+	promise.stdout.pipe(process.stdout);
+
+	const {stderr, stdout} = await promise;
+
+	stdout.stderr = stderr;
+
+	return stdout;
+};
+
+const git = async (...args) => await run('git', ...args);
+git.pipe = async (...args) => await run.pipe('git', ...args);
+
+const runNodeBin = async (bin, ...args) =>
+	await run(bin, ...args, {preferLocal: true});
+runNodeBin.pipe = async (bin, ...args) =>
+	await run.pipe(bin, ...args, {preferLocal: true});
+
+const yarn = async (...args) => await run('yarn', ...args);
+yarn.pipe = async (...args) => await run.pipe('yarn', ...args);
+
+module.exports = {
+	git,
+	run,
+	runNodeBin,
+	yarn,
+};
