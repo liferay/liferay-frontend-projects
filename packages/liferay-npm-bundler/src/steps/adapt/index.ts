3c5909afe344aef8650075ab8633c3bc8b7b24f3
Iván Zaera Avellón <ivan.zaera@liferay.com>
2020-04-30 17:38:01 +0200

    feat: #521 export webpack bundles as functions
    This is needed to have control over when webpack bundles are executed inside the
    portlet lifecycle.

--- packages/liferay-npm-bundler/src/steps/adapt/index.ts
+++ packages/liferay-npm-bundler/src/steps/adapt/index.ts
@@ -14,6 +14,7 @@ import Renderer from '../../util/renderer';
 import {SourceTransform, transformSourceFile} from '../../util/transform/js';
 import wrapModule from '../../util/transform/js/operation/wrapModule';
 import {removeWebpackHash} from '../../util/webpack';
+import exportModuleAsFunction from './transform/js/operation/exportModuleAsFunction';
 import namespaceWepbackJsonp from './transform/js/operation/namespaceWepbackJsonp';
 
 /**
@@ -61,9 +62,10 @@ export async function processWebpackBundles(
 			await transformSourceFile(
 				fromFile,
 				toFile,
-				wrapModule(`${name}@${version}/${moduleName}`),
+				...frameworkSpecificTransforms,
 				namespaceWepbackJsonp(),
-				...frameworkSpecificTransforms
+				exportModuleAsFunction(),
+				wrapModule(`${name}@${version}/${moduleName}`)
 			);
 		})
 	);
---
+++ packages/liferay-npm-bundler/src/steps/adapt/transform/js/operation/exportModuleAsFunction.ts
@@ -0,0 +1,77 @@
+/**
+ * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+import estree from 'estree';
+
+import {
+	SourceCode,
+	SourceTransform,
+	replace,
+} from '../../../../../util/transform/js';
+import {getProgramStatements} from '../../../../../util/transform/js/ast';
+import {parse} from '../../../../../util/transform/js/parse';
+
+export default function exportModuleAsFunction(): SourceTransform {
+	return (source => _exportModuleAsFunction(source)) as SourceTransform;
+}
+
+async function _exportModuleAsFunction(
+	source: SourceCode
+): Promise<SourceCode> {
+	return replace(source, {
+		enter(node) {
+			if (node.type !== 'Program') {
+				return;
+			}
+
+			const program = node;
+
+			const wrapAst = parse(`
+			module.exports = function(_LIFERAY_PARAMS_, _ADAPT_RT_) {
+			};
+			`);
+
+			const {body: wrapBody} = wrapAst;
+
+			const functionBody = getBlockStatement(wrapAst);
+
+			functionBody.body = getProgramStatements(program);
+
+			program.body = wrapBody;
+		},
+	});
+}
+
+function getBlockStatement(program: estree.Program): estree.BlockStatement {
+	const {body: programBody} = program;
+
+	if (programBody.length !== 1) {
+		throw new Error('Program body has more than one node');
+	}
+
+	if (programBody[0].type !== 'ExpressionStatement') {
+		throw new Error('Program is not an expression statement');
+	}
+
+	const {expression} = programBody[0];
+
+	if (expression.type !== 'AssignmentExpression') {
+		throw new Error('Program is not an assignment expression');
+	}
+
+	const {right} = expression;
+
+	if (right.type !== 'FunctionExpression') {
+		throw new Error('Right hand operator is not a function expression');
+	}
+
+	const {body: functionBody} = right;
+
+	if (functionBody.type !== 'BlockStatement') {
+		throw new Error('Right hand operator body is not a block statement');
+	}
+
+	return functionBody;
+}
