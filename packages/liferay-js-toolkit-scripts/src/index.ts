ddee63b86d41d1da6e33c99387fc375fc449b93b
Iván Zaera Avellón <ivan.zaera@liferay.com>
2020-07-02 12:16:48 +0200

    refactor: catalog list of scripts
    Given that the list of scripts is fixed and this review comment:
    
    https://github.com/liferay/liferay-js-toolkit/pull/608#pullrequestreview-440946581
    
    We won't use dynamic requires for commands.

--- packages/liferay-js-toolkit-scripts/src/index.ts
+++ packages/liferay-js-toolkit-scripts/src/index.ts
@@ -5,32 +5,84 @@
 
 import {format} from 'liferay-js-toolkit-core';
 
-const {error, print} = format;
+import build from './scripts/build';
+import copyAssets from './scripts/copy-assets';
+import copySources from './scripts/copy-sources';
+import createJar from './scripts/create-jar';
+import deploy from './scripts/deploy';
+import start from './scripts/start';
+import translate from './scripts/translate';
+
+const {error, info, print} = format;
+
+const COMMANDS = {
+	build: {
+		description: 'generate bundled files in build folder',
+		scriptFunction: build,
+	},
+	'copy-assets': {
+		description: 'copy files from assets to build folder',
+		scriptFunction: copyAssets,
+	},
+	'copy-sources': {
+		description: 'copy static files from source to build folder',
+		scriptFunction: copySources,
+	},
+	'create-jar': {
+		description: 'create a JAR file from bundled files',
+		scriptFunction: createJar,
+	},
+	deploy: {
+		description: 'deploy JAR file to local Liferay server',
+		scriptFunction: deploy,
+	},
+	start: {
+		description: 'start webpack devserver (deprecated)',
+		scriptFunction: start,
+	},
+	translate: {
+		description: 'pass project labels through Microsoft Translator',
+		scriptFunction: translate,
+	},
+};
 
 export default async function main(): Promise<void> {
 	const scripts = process.argv.slice(2);
 
+	if (scripts.length === 0) {
+		print(error`No scripts to run were specified`);
+		showUsage();
+		process.exit(1);
+	}
+
+	const unknownScripts = scripts.filter((script) => !COMMANDS[script]);
+
+	if (unknownScripts.length > 0) {
+		print(error`Unknown scripts found: ${unknownScripts.join(', ')}'`);
+		showUsage();
+		process.exit(1);
+	}
+
 	for (const script of scripts) {
 		try {
-			// eslint-disable-next-line liferay/no-dynamic-require, @typescript-eslint/no-var-requires
-			const {default: scriptFunction} = require(`./scripts/${script}`);
-
-			if (typeof scriptFunction !== 'function') {
-				throw `Script 'js-toolkit ${script}' is invalid`;
-			}
+			const {scriptFunction} = COMMANDS[script];
 
 			await scriptFunction();
 		} catch (err) {
-			if (err.code === 'MODULE_NOT_FOUND') {
-				abort(`Script 'js-toolkit ${script}' does not exist`);
-			} else {
-				abort(`Script 'js-toolkit ${script}' failed:\n${err.stack}`);
-			}
+			print(error`Script 'js-toolkit ${script}' failed:\n${err.stack}`);
+			process.exit(1);
 		}
 	}
 }
 
-function abort(msg: string): void {
-	print(error`${msg}`);
-	process.exit(1);
+function showUsage(): void {
+	print(
+		'',
+		info`
+			Supported scripts are:
+			${Object.entries(COMMANDS)
+				.map(([id, {description}]) => `    {${id}}: ${description}`)
+				.join('\n')}
+		`
+	);
 }
