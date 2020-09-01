liferay/liferay-frontend-projects

commit 0c14c6970503d66d73cb5b311d15ce5748d9eca5

fix: #589 wrap when define calls exist
The code in this commit apparently goes against #68 and #74, but since #111 was
fixes, they are no longer needed as all modules think they are inside Node.js,
so there's no need to namespace anything but the first define() call.

packages/babel-plugin-namespace-amd-define/src/index.js
@@ -32,33 +32,21 @@ export default function() {
 					return;
 				}
 
-				let scope;
-
-				// Find if 'define' is defined in any scope
-				for (scope = path.scope; scope != null; scope = scope.parent) {
-					if (scope.bindings.define || scope.globals.define) {
-						break;
-					}
+				if (path.parent.type !== 'CallExpression') {
+					return;
 				}
 
-				if (
-					scope == null ||
-					(scope.parent == null && !scope.bindings.define)
-				) {
-					// If 'define' is not defined in any scope namespace or
-					// defined in the root scope as global...
-					if (!firstDefineNamespaced) {
-						// ...and it's its first appearance, namespace it
+				if (!firstDefineNamespaced) {
+					if (path.scope.parent === null) {
 						const namespace =
 							this.opts.namespace || 'Liferay.Loader';
 
 						path.node.name = `${namespace}.define`;
 
 						firstDefineNamespaced = true;
-					} else {
-						// ...and appeared before, record a new extra appearance
-						extraNamespaceCount++;
 					}
+				} else {
+					extraNamespaceCount++;
 				}
 			}
 		},

packages/babel-plugin-namespace-amd-define/src/__tests__/__snapshots__/index.test.js.snap
@@ -39,7 +39,7 @@ var a = {
 };"
 `;
 
-exports[`logs results correctly 1`] = `
+exports[`logs warnings when more than one global define appears 1`] = `
 Array [
   Object {
     "level": "info",
@@ -53,7 +53,7 @@ Array [
     "source": "namespace-amd-define",
     "things": Array [
       "Found",
-      2,
+      1,
       "define() calls inside the module definition",
       "which have been ignored as they should never",
       "be executed during runtime",

packages/babel-plugin-namespace-amd-define/src/__tests__/index.test.js
@@ -17,12 +17,27 @@ beforeEach(() => {
 	});
 });
 
-it('logs results correctly', () => {
+it('logs nothing when only one define appears', () => {
 	const source = `
 	define([], function(){})
 	if (typeof define === "function" && define.amd) {
 		console.log('UMD!');
 	}
+	`;
+
+	babel.transform(source, {
+		filename: __filename,
+		plugins: [plugin],
+	});
+
+	expect(logger.messages).toEqual([]);
+});
+
+it('logs warnings when more than one global define appears', () => {
+	const source = `
+	define([], function(){
+		define("something");
+	})
 	`;
 
 	babel.transform(source, {

commit 3a17c2be40e9e821975559e3521cedb36d47fcd4

fix: make namespace-amd-define work with css-loader
As noted here:

    https://github.com/liferay-frontend/liferay-portal/pull/345#issuecomment-684867451

Code like this which is produced by the css-loader:

```js
function defineModule() {
  define("frontend-js-web@4.0.27/liferay/modal/Modal.scss", ['module', 'exports', 'require'], function (module, exports, require) {
    var define = undefined;
    var global = window;
    {
      module.exports = link;
    }
  });
}
```

winds up with a naked `define` in it, instead of the expected
`Liferay.Loader.define`.

This behavior regressed in 0c14c6970503d66d73cb5b311d15ce5748d9eca5.

Now, I don't have enough context to know whether this is the right fix,
because the issues listed in that commit are kind of opaque to me.
Hopefully @izaera can confirm or refute that the proposed change here is
safe.

Of course, it's not very clever; if you have code like this:

```js
function a() {
  define([], function () {});
}

define([], function () {});
```

It is going to namespace the first `define` it sees and warn about the
second one, but it's totally arbitrary; there is nothing "special" about
the first one that appeared higher up in the file. If move the function
to the bottom, then the other `define` "wins" and gets transformed.

packages/babel-plugin-namespace-amd-define/src/index.js
@@ -37,7 +37,11 @@ export default function() {
 				}
 
 				if (!firstDefineNamespaced) {
-					if (path.scope.parent === null) {
+					if (
+						path.scope.parent === null ||
+						(!path.scope.hasOwnBinding('define') &&
+							!path.scope.hasBinding('define'))
+					) {
 						const namespace =
 							this.opts.namespace || 'Liferay.Loader';
 

packages/babel-plugin-namespace-amd-define/src/__tests__/__snapshots__/index.test.js.snap
@@ -66,3 +66,10 @@ exports[`namespaces unqualified define calls 1`] = `
 "
 Liferay.Loader.define([], function () {});"
 `;
+
+exports[`namespaces unqualified define calls that are wrapped 1`] = `
+"
+function defineModule() {
+	Liferay.Loader.define([], function () {});
+}"
+`;

packages/babel-plugin-namespace-amd-define/src/__tests__/index.test.js
@@ -61,6 +61,22 @@ it('namespaces unqualified define calls', () => {
 	expect(code).toMatchSnapshot();
 });
 
+it('namespaces unqualified define calls that are wrapped', () => {
+	// eg. the css-loader produces wrappers like this.
+	const source = `
+		function defineModule() {
+			define([], function () {});
+		}
+	`;
+
+	const {code} = babel.transform(source, {
+		filename: __filename,
+		plugins: [plugin],
+	});
+
+	expect(code).toMatchSnapshot();
+});
+
 it('does not namespace already qualified define calls', () => {
 	const source = `
 	Other.Namespace.define([], function(){})