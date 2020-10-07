0ef87e0b836d526f1bf453ee4a344994e61f59e8
Greg Hurrell <greg.hurrell@liferay.com>
2020-10-07 15:18:42 +0200

    test(js-toolkit): make v2.x tests pass in CI
    Example failure here[0].
    
    As explained in the comment, the behavior of this code is sensitive to
    the environment that you run in, so I'm making the test looser to work
    both locally and in CI.
    
    In the monorepo, the `getRenderer()`[1] method is going to try to get
    "node-sass" first and it is going to find it because it is upstairs in
    the top-level "node_modules" folder.
    
    In CI, when we run the v2.x tests, we only do a yarn install inside
    "maintenance/projects/js-toolkit", because it doesn't participate in the
    top-level Yarn workspaces. So in CI, `getRenderer()` is going to try to
    get "node-sass", fail, and fallback to "sass", which is bundled more
    locally.
    
    As such, we need to loosen our assertion about what the renderer
    description looks like, as well as make the assertion about the expected
    output whitespace-agnostic, seeing as "node-sass" and "sass" make
    slightly different choices about whitespace. I figure this looseness is
    more future-proof and flexible than the alternatives, because it is more
    likely to keep the suite green even if we refactor and restructure the
    relationship between packages in the monorepo; and, at the end of the
    day, the `getRenderer()` is fundamentally dynamic in nature.
    
    [0]: https://github.com/liferay/liferay-frontend-projects/pull/133/checks?check_run_id=1220537713
    [1]: https://github.com/liferay/liferay-frontend-projects/blob/47c938cc1c1a7a2837453eb96535fcec9a6492a3/maintenance/projects/js-toolkit/packages/liferay-npm-bundler-loader-sass-loader/src/index.js#L31-L58

--- maintenance/projects/js-toolkit/packages/liferay-npm-bundler-loader-sass-loader/src/__tests__/index.test.js
+++ maintenance/projects/js-toolkit/packages/liferay-npm-bundler-loader-sass-loader/src/__tests__/index.test.js
@@ -20,12 +20,23 @@ it('logs results correctly', () => {
 	// This test returns '(from project)' because of yarn hoisting, which makes
 	// the loader think that it is retrieving sass from a project (as opposed to
 	// itself).
+	//
+	// We use a loose match here because the actual version obtained may
+	// vary depend on environment (eg. in local environment, it will
+	// usually pick up node-sass from the monorepo root, but in CI it
+	// will pick up sass from the local node_modules folder, because in
+	// CI when running the js-toolkit v2 tests we only set up the local
+	// folder.
 
 	expect(context.log.messages).toEqual([
 		{
 			level: 'info',
 			source: 'sass-loader',
-			things: ['Processed file with node-sass v4.14.1 (from project)'],
+			things: [
+				expect.stringMatching(
+					/Processed file with \S+ v\S+ \(from project\)/
+				),
+			],
 		},
 	]);
 });
@@ -40,9 +51,12 @@ it('correctly generates CSS', () => {
 
 	const result = loader(context, {});
 
-	expect(result).toEqual(`.Button {
-  border: 1px solid red; }
-`);
+	// Here again we do a loose match (whitespace-agnostic) to account for
+	// differences between node-sass and sass.
+
+	expect(result.trim().replace(/\s+/g, ' ')).toEqual(
+		`.Button { border: 1px solid red; }`
+	);
 
 	expect(Object.keys(context.extraArtifacts)).toEqual([]);
 });
