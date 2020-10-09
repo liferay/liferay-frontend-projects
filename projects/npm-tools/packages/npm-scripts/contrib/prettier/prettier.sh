47fb7fa126335edb65c97782fe22dce38e1c9c81
Greg Hurrell <greg.hurrell@liferay.com>
2020-10-09 13:31:02 +0200

    feat(npm-scripts): teach Bash Prettier wrapper about this repo
    Same as the parent commit did for the sibling prettier.js wrapper, this
    commit teaches the Bash wrapper about this repo too.

--- projects/npm-tools/packages/npm-scripts/contrib/prettier/prettier.sh
+++ projects/npm-tools/packages/npm-scripts/contrib/prettier/prettier.sh
@@ -5,11 +5,27 @@
 # "liferay-npm-scripts prettier" subcommand to plugins.
 #
 
-# Auto-detect liferay-portal checkout.
+LIFERAY_NPM_SCRIPTS=""
+LIFERAY_PRETTIER=""
+
+# Auto-detect a liferay-portal or liferay-frontend-projects checkout.
 while [[ $PWD != '/' ]]; do
-	if [ -d portal-web ]; then
+	if [ -e .liferay-frontend-projects ]; then
+		LIFERAY_NPM_SCRIPTS=projects/npm-tools/packages/npm-scripts/bin/liferay-npm-scripts.js
+		if [ ! -e "$LIFERAY_NPM_SCRIPTS" ]; then
+			LIFERAY_NPM_SCRIPTS=""
+		fi
+
+		break
+	elif [ -d portal-web ]; then
 		if [ -e portal-web/docroot/WEB-INF/liferay-web.xml ]; then
 			PORTAL="$PWD"
+
+			LIFERAY_NPM_SCRIPTS="$PORTAL/modules/node_modules/@liferay/npm-scripts/bin/liferay-npm-scripts.js"
+			if [ ! -e "$LIFERAY_NPM_SCRIPTS" ]; then
+				LIFERAY_NPM_SCRIPTS="$PORTAL/modules/node_modules/liferay-npm-scripts/bin/liferay-npm-scripts.js"
+			fi
+			LIFERAY_PRETTIER="$PORTAL/modules/node_modules/prettier/bin-prettier.js"
 		fi
 
 		break
@@ -18,26 +34,15 @@ while [[ $PWD != '/' ]]; do
 	fi
 done
 
-if [ -n "$PORTAL" ]; then
-  LIFERAY_NPM_SCRIPTS="$PORTAL/modules/node_modules/liferay-npm-scripts/bin/liferay-npm-scripts.js"
-  if [ ! -e "$LIFERAY_NPM_SCRIPTS" ]; then
-    LIFERAY_NPM_SCRIPTS="$PORTAL/modules/node_modules/@liferay/npm-scripts/bin/liferay-npm-scripts.js"
-  fi
-  LIFERAY_PRETTIER="$PORTAL/modules/node_modules/prettier/bin-prettier.js"
-else
-  LIFERAY_NPM_SCRIPTS=""
-  LIFERAY_PRETTIER=""
-fi
-
 if [ -x "$LIFERAY_NPM_SCRIPTS" ]; then
-  "$LIFERAY_NPM_SCRIPTS" prettier "$@"
+	"$LIFERAY_NPM_SCRIPTS" prettier "$@"
 elif [ -x "$LIFERAY_PRETTIER" ]; then
-  "$LIFERAY_PRETTIER" "$@"
+	"$LIFERAY_PRETTIER" "$@"
 elif command -v prettier > /dev/null; then
-  prettier "$@"
+	prettier "$@"
 elif command -v npx > /dev/null; then
-  npx --quiet prettier "$@"
+	npx --quiet prettier "$@"
 else
-  echo "error: No prettier or npx executable found"
-  exit 1
+	echo "error: No prettier or npx executable found"
+	exit 1
 fi
