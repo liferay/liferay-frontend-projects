9228a7cefa7ecde4b58a20b14a707e05cd8b832f
Greg Hurrell <greg.hurrell@liferay.com>
2020-10-19 17:48:13 +0200

    fix(amd-loader): remove pointless jsdoc call
    As far as I can tell, we removed all traces of JSDoc in Feb 2018, in
    35d50d9dd12b60a173f7bdec7fab19cf69a5e21c.
    
    Noticed while trouble-shooting Windows CI failures; on adding logging, I
    see that calls to `run()` are not error-checked, which is why we never
    noticed the missing `jsdoc` executable.

--- projects/amd-loader/bin/build-loader.js
+++ projects/amd-loader/bin/build-loader.js
@@ -3,22 +3,8 @@
  * SPDX-License-Identifier: LGPL-3.0-or-later
  */
 
-const globby = require('globby');
-
 const {run} = require('./util');
 
 run('webpack', '--env.flavor=debug');
 run('webpack', '--env.flavor=prod');
 run('webpack', '--env.flavor=min');
-
-run(
-	...['jsdoc', '-d', 'build/jsdoc']
-		.concat(['README.md'])
-		.concat(
-			globby.sync([
-				'src/loader/**/*.js',
-				'!src/loader/bootstrap.js',
-				'!test/**/*',
-			])
-		)
-);
