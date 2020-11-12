101110aea23a28a049504659d8b18d9454edce7b
Greg Hurrell <greg.hurrell@liferay.com>
2020-11-12 11:45:25 +0100

    refactor(npm-scripts): avoid use of "temp" abbreviation

--- projects/npm-tools/packages/npm-scripts/src/jest/setup.js
+++ projects/npm-tools/packages/npm-scripts/src/jest/setup.js
@@ -10,15 +10,15 @@ global.Liferay = require('./mocks/Liferay');
 if (!global.createRange) {
 	global.createRange = () => ({
 		createContextualFragment(htmlString) {
-			const tempDiv = document.createElement('div'); // eslint-disable-line no-undef
+			const div = document.createElement('div'); // eslint-disable-line no-undef
 
-			tempDiv.innerHTML = `<br>${htmlString}`;
-			tempDiv.removeChild(tempDiv.firstChild);
+			div.innerHTML = `<br>${htmlString}`;
+			div.removeChild(div.firstChild);
 
 			const fragment = document.createDocumentFragment(); // eslint-disable-line no-undef
 
-			while (tempDiv.firstChild) {
-				fragment.appendChild(tempDiv.firstChild);
+			while (div.firstChild) {
+				fragment.appendChild(div.firstChild);
 			}
 
 			return fragment;
