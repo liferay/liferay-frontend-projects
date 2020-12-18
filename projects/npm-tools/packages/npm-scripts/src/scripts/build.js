43fbfed1a0ca064fb0374cf94b99217574e3be5f
Iván Zaera Avellón <ivan.zaera@liferay.com>
2020-12-18 13:00:02 +0100

    refactor(npm-scripts): always invoke createBridges
    Because we need to create the bridge for the `main` module, we need to call
    createBridges even if no bridges are configured in npmscript.config.js.

--- projects/npm-tools/packages/npm-scripts/src/scripts/build.js
+++ projects/npm-tools/packages/npm-scripts/src/scripts/build.js
@@ -6,6 +6,7 @@
 const fs = require('fs');
 const path = require('path');
 
+let createBridges = require('../utils/createBridges');
 const getMergedConfig = require('../utils/getMergedConfig');
 const instrument = require('../utils/instrument');
 let minify = require('../utils/minify');
@@ -15,7 +16,6 @@ let runBundler = require('../utils/runBundler');
 const setEnv = require('../utils/setEnv');
 let {buildSoy, cleanSoy, soyExists, translateSoy} = require('../utils/soy');
 const validateConfig = require('../utils/validateConfig');
-let createBridges = require('./createBridges');
 let webpack = require('./webpack');
 
 const {build: BUILD_CONFIG, federation: FEDERATION_CONFIG} = getMergedConfig(
@@ -101,10 +101,8 @@ module.exports = async function (...args) {
 		fs.writeFileSync(path.join(output, 'manifest.json'), '{}');
 	}
 
-	const {bridges} = FEDERATION_CONFIG;
-
-	if (bridges) {
-		createBridges(bridges, BUILD_CONFIG.output);
+	if (FEDERATION_CONFIG) {
+		createBridges(FEDERATION_CONFIG.bridges, BUILD_CONFIG.output);
 	}
 
 	translateSoy(BUILD_CONFIG.output);
