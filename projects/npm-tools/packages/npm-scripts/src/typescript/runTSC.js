fd502b40cccd865c2ef8020b0ae61ce89ab567d1
Greg Hurrell <greg.hurrell@liferay.com>
2021-03-30 16:12:01 +0200

    feat(npm-scripts): teach build to emit TypeScript type info
    Extracted the TS build into a separate function, `runTSC()` and tweaked
    it in the following two ways:
    
    1. Instead of `--noEmit` (ie. just check types, emit nothing), we now do
       `--emitDeclarationOnly` (ie. check types and emit `.d.ts` +
       `tsconfig.tsbuildinfo` files). Note that for now the path is
       hard-coded as "types", but a subsequent commit is going to manage the
       `tsconfig.json` files for us, including the set-up of the output
       directory.
    
    2. Now that we're emitting type information that we expect to be
       committed, print a big yellow warning if it is not up-to-date at the
       end of the run. We'll be complementing this check with a separate
       top-level check that we can run from CI to confirm that type
       definitions are up to date.
    
    Test plan:
    
    Copy files over to `liferay-portal`, specifically a local clone with
    this PR checked out:
    
        https://github.com/liferay-frontend/liferay-portal/pull/942
    
    Run the build (`yarn run build`) with a change to the "tsconfig.json"
    file and see the following in bold/yellow:
    
        ***************
        *** WARNING ***
        ***************
    
        Out-of-date TypeScript build artifacts at:
    
            apps/frontend-js/frontend-js-react-web/types
    
        Please commit them.
    
        M modules/apps/frontend-js/frontend-js-react-web/types/tsconfig.tsbuildinfo
    
    Run the build with a clean "tsconfig.json" file and see no warning.

---
+++ projects/npm-tools/packages/npm-scripts/src/typescript/runTSC.js
@@ -0,0 +1,51 @@
+/**
+ * SPDX-FileCopyrightText: © 2019 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+const path = require('path');
+
+const color = require('../utils/color');
+const findRoot = require('../utils/findRoot');
+const git = require('../utils/git');
+const log = require('../utils/log');
+const spawnSync = require('../utils/spawnSync');
+
+/**
+ * Runs the TypeScript compiler, `tsc`, in the current working directory.
+ *
+ * Currently in liferay-portal we use either webpack or Babel to do
+ * code-generation; `tsc` is only doing type-checking and emitting type
+ * definitions.
+ */
+function runTSC() {
+	spawnSync('tsc', ['--emitDeclarationOnly']);
+
+	// Check for stale (uncommitted) type artifacts.
+
+	const output = git('status', '--porcelain', '--', 'types');
+
+	if (output.length) {
+		const root = findRoot() || '';
+
+		const location = path.relative(root, path.join(process.cwd(), 'types'));
+
+		log(
+			`${color.YELLOW}${color.BOLD}`,
+			'***************',
+			'*** WARNING ***',
+			'***************',
+			'',
+			'Out-of-date TypeScript build artifacts at:',
+			'',
+			`    ${location}`,
+			'',
+			'Please commit them.',
+			'',
+			output,
+			color.RESET
+		);
+	}
+}
+
+module.exports = runTSC;
