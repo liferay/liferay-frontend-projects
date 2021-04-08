liferay/liferay-frontend-projects

commit 77db6f875cdbb05b05ff5bd5a7d456a04d19b707

feat(npm-scripts): allow format/lint to run without package.json
As noted in the doc comment, `yarn run liferay-npm-scripts` will `cd` to
the closest project root (ie. directory that has a `package.json` file)
before invoking our tool. By that time, it is "too late", to know where
we were invoked from. Fortunately, it sets an `INIT_CWD` environment
variable with this information.

Whenever we run the `lint()` or `format()` functions (whether in `check`
or `fix` modes), we check to see if `INIT_CWD` is defined and different
from our apparent "current" working directory. If there is a
discrepancy, that means that the user is probably working down inside a
`src/` directory (or similar), or they are running in a project that
doesn't have a `package.json` (eg. `click-to-chat-web`). Nevertheless,
projects like `click-to-chat-web` do have JSP in them even though they
don't have a `package.json`, so it is reasonable to want to format them
with our frontend formatting tools.

Now, prior to this change, running `yarn run format` in a place like
`click-to-chat-web` would result in `yarn` starting from the "top" level
(`modules/`) directory and running formatting for the entire repo, which
would work but is slow. Running from a subdirectory inside a project
with a `package.json` (like `frontend-js-react-web`) wasn't quite as
bad, because `yarn` would `cd` up to the project root where the
`package.json` lives and run from there; our default globs would take
effect (which have the form "/src/**/*.jsp" etc).

After this change, if we detect that `INIT_CWD` is set and different,
that means we are "down" somewhere where there is no `package.json`. We
walk up the filesystem looking for a `build.gradle` or a `package.json`,
whichever we see first, and we run from there. This means that one of
three scenarios may apply:

- Inside `click-to-chat-web` you get exactly what you would want and the
  JSP files get checked/formatted.

projects/npm-tools/packages/npm-scripts/src/scripts/lint.js
@@ -12,6 +12,7 @@ const lintJSP = require('../jsp/lintJSP');
 const color = require('../utils/color');
 const getMergedConfig = require('../utils/getMergedConfig');
 const getPaths = require('../utils/getPaths');
+const inCurrentWorkingDirectory = require('../utils/inCurrentWorkingDirectory');
 const log = require('../utils/log');
 const {SpawnError} = require('../utils/spawnSync');
 const isSCSS = require('./lint/stylelint/isSCSS');
@@ -41,127 +42,129 @@ const IGNORE_FILE = '.eslintignore';
  * ESLint (etc) wrapper.
  */
 async function lint(options = {}) {
-	const {fix, quiet} = {
-		...DEFAULT_OPTIONS,
-		...options,
-	};
+	await inCurrentWorkingDirectory(async () => {
+		const {fix, quiet} = {
+			...DEFAULT_OPTIONS,
+			...options,
+		};
 
-	const globs = fix
-		? getMergedConfig('npmscripts', 'fix')
-		: getMergedConfig('npmscripts', 'check');
+		const globs = fix
+			? getMergedConfig('npmscripts', 'fix')
+			: getMergedConfig('npmscripts', 'check');
 
-	const extensions = [].concat(...Object.values(EXTENSIONS));
+		const extensions = [].concat(...Object.values(EXTENSIONS));
 
-	const paths = getPaths(globs, extensions, IGNORE_FILE);
+		const paths = getPaths(globs, extensions, IGNORE_FILE);
 
-	if (!paths.length) {
-		return;
-	}
-
-	const config = getMergedConfig('eslint');
-
-	const CLIEngine = eslint.CLIEngine;
-
-	const cli = new CLIEngine({
-		baseConfig: config,
-
-		// Note that "fix: true" here does not actually write to the filesystem
-		// (that happens below).
-		//
-		// - "fix: true": means fixes are not reported in the list of problems;
-		//   results with possible fixes have an "output" property.
-		// - "fix: false": means that they are reported; results with possible
-		//   fixes do not have an "output" property, but
-		//   `report.fixableErrorCount` and `report.fixableWarningCount` will be
-		//   set.
-
-		fix,
-
-		// Avoid spurious warnings of the form, "File ignored by
-		// default. Use a negated ignore pattern ... to override"
-
-		ignorePattern: '!*',
-	});
-
-	const {default: jsPaths, jspPaths, scssPaths} = partitionArray(paths, {
-		jspPaths: isJSP,
-		scssPaths: isSCSS,
-	});
-
-	let report;
-
-	if (jsPaths.length) {
-		report = cli.executeOnFiles(jsPaths);
-
-		if (fix && jsPaths.length) {
-
-			// This is what actually writes to the file-system.
-
-			CLIEngine.outputFixes(report);
+		if (!paths.length) {
+			return;
 		}
-	}
-	else {
-		report = {results: []};
-	}
 
-	for (const [paths, linter] of [
-		[jspPaths, lintJSP],
-		[scssPaths, lintSCSS],
-	]) {
-		for (const filePath of paths) {
+		const config = getMergedConfig('eslint');
 
-			// TODO: non-sync version to make use of I/O concurrency
+		const CLIEngine = eslint.CLIEngine;
 
-			const contents = fs.readFileSync(filePath, 'utf8');
+		const cli = new CLIEngine({
+			baseConfig: config,
 
-			try {
-				const updated = await linter(
-					contents,
-					(result) => {
-						report.results.push({
-							...result,
-							filePath: path.resolve(filePath),
-						});
-					},
-					{filePath, fix, quiet}
-				);
+			// Note that "fix: true" here does not actually write to the filesystem
+			// (that happens below).
+			//
+			// - "fix: true": means fixes are not reported in the list of problems;
+			//   results with possible fixes have an "output" property.
+			// - "fix: false": means that they are reported; results with possible
+			//   fixes do not have an "output" property, but
+			//   `report.fixableErrorCount` and `report.fixableWarningCount` will be
+			//   set.
 
-				if (fix && updated !== contents) {
-					fs.writeFileSync(filePath, updated);
+			fix,
+
+			// Avoid spurious warnings of the form, "File ignored by
+			// default. Use a negated ignore pattern ... to override"
+
+			ignorePattern: '!*',
+		});
+
+		const {default: jsPaths, jspPaths, scssPaths} = partitionArray(paths, {
+			jspPaths: isJSP,
+			scssPaths: isSCSS,
+		});
+
+		let report;
+
+		if (jsPaths.length) {
+			report = cli.executeOnFiles(jsPaths);
+
+			if (fix && jsPaths.length) {
+
+				// This is what actually writes to the file-system.
+
+				CLIEngine.outputFixes(report);
+			}
+		}
+		else {
+			report = {results: []};
+		}
+
+		for (const [paths, linter] of [
+			[jspPaths, lintJSP],
+			[scssPaths, lintSCSS],
+		]) {
+			for (const filePath of paths) {
+
+				// TODO: non-sync version to make use of I/O concurrency
+
+				const contents = fs.readFileSync(filePath, 'utf8');
+
+				try {
+					const updated = await linter(
+						contents,
+						(result) => {
+							report.results.push({
+								...result,
+								filePath: path.resolve(filePath),
+							});
+						},
+						{filePath, fix, quiet}
+					);
+
+					if (fix && updated !== contents) {
+						fs.writeFileSync(filePath, updated);
+					}
+				}
+				catch (error) {
+					log(error);
 				}
 			}
-			catch (error) {
-				log(error);
-			}
 		}
-	}
 
-	// In `quiet` mode, keep only errors.
-	// Otherwise keep both errors and warnings.
-	// Filter out everything else.
+		// In `quiet` mode, keep only errors.
+		// Otherwise keep both errors and warnings.
+		// Filter out everything else.
 
-	const results = (report.results || [])
-		.map((result) => {
-			const messages = result.messages.filter((message) => {
-				return quiet ? message.severity === 2 : true;
-			});
+		const results = (report.results || [])
+			.map((result) => {
+				const messages = result.messages.filter((message) => {
+					return quiet ? message.severity === 2 : true;
+				});
 
-			if (result.messages.length === 0) {
-				return null;
-			}
+				if (result.messages.length === 0) {
+					return null;
+				}
 
-			return {
-				...result,
-				messages,
-			};
-		})
-		.filter(Boolean);
+				return {
+					...result,
+					messages,
+				};
+			})
+			.filter(Boolean);
 
-	log(formatter(results));
+		log(formatter(results));
 
-	if (results.length) {
-		throw new SpawnError();
-	}
+		if (results.length) {
+			throw new SpawnError();
+		}
+	});
 }
 
 /**

projects/npm-tools/packages/npm-scripts/src/scripts/format.js
@@ -10,6 +10,7 @@ const isJSP = require('../jsp/isJSP');
 const prettier = require('../prettier');
 const getMergedConfig = require('../utils/getMergedConfig');
 const getPaths = require('../utils/getPaths');
+const inCurrentWorkingDirectory = require('../utils/inCurrentWorkingDirectory');
 const log = require('../utils/log');
 const {SpawnError} = require('../utils/spawnSync');
 
@@ -40,94 +41,97 @@ const IGNORE_FILE = '.prettierignore';
  * Prettier wrapper.
  */
 async function format(options = {}) {
-	const {check} = {
-		...DEFAULT_OPTIONS,
-		...options,
-	};
+	await inCurrentWorkingDirectory(async () => {
+		const {check} = {
+			...DEFAULT_OPTIONS,
+			...options,
+		};
 
-	const globs = check
-		? getMergedConfig('npmscripts', 'check')
-		: getMergedConfig('npmscripts', 'fix');
+		const globs = check
+			? getMergedConfig('npmscripts', 'check')
+			: getMergedConfig('npmscripts', 'fix');
 
-	const paths = getPaths(globs, EXTENSIONS, IGNORE_FILE);
+		const paths = getPaths(globs, EXTENSIONS, IGNORE_FILE);
 
-	if (!paths.length) {
-		return;
-	}
+		if (!paths.length) {
+			return;
+		}
 
-	const config = getMergedConfig('prettier');
+		const config = getMergedConfig('prettier');
 
-	let checked = 0;
-	let bad = 0;
-	let fixed = 0;
+		let checked = 0;
+		let bad = 0;
+		let fixed = 0;
 
-	for (const filepath of paths) {
-		checked++;
+		for (const filepath of paths) {
+			checked++;
 
-		try {
+			try {
 
-			// TODO: don't re-read file, run eslint on it too
+				// TODO: don't re-read file, run eslint on it too
 
-			const source = fs.readFileSync(filepath, 'utf8');
+				const source = fs.readFileSync(filepath, 'utf8');
 
-			const prettierOptions = {
-				...config,
-				filepath,
-			};
-
-			let checkFormat;
-			let format;
-
-			if (isJSP(filepath)) {
-				checkFormat = async (source, prettierOptions) => {
-					return (
-						source === (await formatJSP(source, prettierOptions))
-					);
+				const prettierOptions = {
+					...config,
+					filepath,
 				};
-				format = formatJSP;
-			}
-			else {
-				checkFormat = prettier.check;
-				format = prettier.format;
-			}
 
-			if (!(await checkFormat(source, prettierOptions))) {
-				if (check) {
-					log(`${filepath}: BAD`);
-					bad++;
+				let checkFormat;
+				let format;
+
+				if (isJSP(filepath)) {
+					checkFormat = async (source, prettierOptions) => {
+						return (
+							source ===
+							(await formatJSP(source, prettierOptions))
+						);
+					};
+					format = formatJSP;
 				}
 				else {
-					const formatted = await format(source, prettierOptions);
-					fs.writeFileSync(filepath, formatted);
-					fixed++;
+					checkFormat = prettier.check;
+					format = prettier.format;
+				}
+
+				if (!(await checkFormat(source, prettierOptions))) {
+					if (check) {
+						log(`${filepath}: BAD`);
+						bad++;
+					}
+					else {
+						const formatted = await format(source, prettierOptions);
+						fs.writeFileSync(filepath, formatted);
+						fixed++;
+					}
 				}
 			}
+			catch (error) {
+
+				// Generally this means a syntax error.
+
+				log(`${filepath}: ${error}`);
+				bad++;
+			}
 		}
-		catch (error) {
 
-			// Generally this means a syntax error.
+		const files = (count) => (count === 1 ? 'file' : 'files');
+		const have = (count) => (count === 1 ? 'has' : 'have');
 
-			log(`${filepath}: ${error}`);
-			bad++;
+		const summary = [`Prettier checked ${checked} ${files(checked)}`];
+
+		if (fixed) {
+			summary.push(`fixed ${fixed} ${files(fixed)}`);
 		}
-	}
 
-	const files = (count) => (count === 1 ? 'file' : 'files');
-	const have = (count) => (count === 1 ? 'has' : 'have');
-
-	const summary = [`Prettier checked ${checked} ${files(checked)}`];
-
-	if (fixed) {
-		summary.push(`fixed ${fixed} ${files(fixed)}`);
-	}
-
-	if (bad) {
-		summary.push(`${bad} ${files(bad)} ${have(bad)} problems`);
-		throw new SpawnError(summary.join(', '));
-	}
-	else {
-		log(summary.join(', '));
-	}
+		if (bad) {
+			summary.push(`${bad} ${files(bad)} ${have(bad)} problems`);
+			throw new SpawnError(summary.join(', '));
+		}
+		else {
+			log(summary.join(', '));
+		}
+	});
 }
 
 module.exports = format;

projects/npm-tools/packages/npm-scripts/src/utils/inCurrentWorkingDirectory.js
@@ -1,0 +1,67 @@
+/**
+ * SPDX-FileCopyrightText: © 2019 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * Runs the `callback()` in the context of the "current" working directory,
+ * which may not be the working directory at the time `liferay-npm-scripts` was
+ * invoked.
+ *
+ * This seems to be an undocumented feature, but `yarn run liferay-npm-scripts`
+ * will walk up the directory tree looking for a `package.json` file and change
+ * to that directory before invoking us, but it will helpfully set the
+ * `INIT_CWD` environment variable so that we can tell where we came from:
+ *
+ *      https://twitter.com/kentcdodds/status/922875214173757440
+ *
+ * This means that you can run a command like `yarn run format` in an OSGi
+ * project that doesn't have a `package.json` (but may still have JS inside JSP
+ * files) and have it execute scoped to that project. This is much quicker than
+ * running `format` from the top, which would examine every eligible file in the
+ * entire repo.
+ */
+async function inCurrentWorkingDirectory(callback) {
+	const cwd = process.cwd();
+
+	const INIT_CWD = process.env.INIT_CWD || cwd;
+
+	let directory = INIT_CWD;
+
+	if (INIT_CWD !== cwd) {
+
+		// Walk up until we find the closest `package.json` or `build.gradle`.
+
+		while (directory) {
+			if (
+				fs.existsSync(path.join(directory, 'package.json')) ||
+				fs.existsSync(path.join(directory, 'build.gradle'))
+			) {
+				break;
+			}
+
+			if (path.dirname(directory) === directory) {
+
+				// Can't go any higher.
+
+				break;
+			}
+
+			directory = path.dirname(directory);
+		}
+	}
+
+	try {
+		process.chdir(directory);
+
+		return await callback();
+	}
+	finally {
+		process.chdir(cwd);
+	}
+}
+
+module.exports = inCurrentWorkingDirectory;

projects/npm-tools/packages/npm-scripts/test/scripts/format.js
@@ -16,6 +16,7 @@ jest.mock('../../src/jsp/formatJSP');
 jest.mock('../../src/utils/log');
 
 describe('scripts/format.js', () => {
+	let INIT_CWD;
 	let cwd;
 	let temp;
 
@@ -25,6 +26,8 @@ describe('scripts/format.js', () => {
 	};
 
 	beforeEach(() => {
+		INIT_CWD = process.env.INIT_CWD;
+
 		cwd = process.cwd();
 
 		jest.resetAllMocks();
@@ -33,6 +36,8 @@ describe('scripts/format.js', () => {
 		jest.spyOn(prettier, 'format');
 
 		temp = fs.mkdtempSync(path.join(os.tmpdir(), 'format-'));
+
+		process.env.INIT_CWD = temp;
 
 		process.chdir(temp);
 
@@ -44,6 +49,8 @@ describe('scripts/format.js', () => {
 
 	afterEach(() => {
 		process.chdir(cwd);
+
+		process.env.INIT_CWD = INIT_CWD;
 	});
 
 	it('invokes check() on our prettier.check() wrapper', async () => {

commit 1c15def7eb211e1765db88a66683bb333ea183a8

test(npm-scripts): avoid unhandled promise rejection in tests
Today we have a new problem caused by 77db6f875cdb, and this time the
fix we applied in 7cabd4f8efa9 doesn't cover it.

Our prior fix dealt with cases where a directory like "/var" might
actually be a symlink to "/private/var", meaning that you can't detect
the "identity" of a directory using `===` without first resolving
symlinks. Probably never happens in actual use, but could (and did)
happen in the test suite. So, we added code to resolve symlinks before
comparing.

Well, today we have another case of something that only happens in the
test suite (which is why I am labeling this with the "test" scope
instead of the "fix" scope, even though it is a bug).

In the lint tests, we `cd` down into our `modules` directory under
`__fixtures__` folder and try to run lints. Now, `INIT_CWD` will be the
`npm-scripts` root folder (or the monorepo root folder, depending on
where you run `yarn test` from), and `cwd` will be something under that,
with no symlinks in between. But, they are different paths, so,
`inCurrentWorkingDirectory()` will do its thing and walk up looking for
a `package.json` file. Turns out that it finds it in the `npm-scripts`
root folder because there isn't one in our `modules` fixtures.

You might think we could just add a `package.json` file there to make
the problem go away, but that won't work because,
`inCurrentWorkingDirectory()`, in its infinite wisdom, starts its walk
at `INIT_CWD`, not `cwd`, which means it will _still_ wind up running
from the `npm-scripts` root.

Now, I thought for about 10 seconds of making
`inCurrentWorkingDirectory()` _even_ "smarter", and detecting this case
where `cwd` is actually a subdirectory inside the `INIT_CWD` hierarchy,
instead of the other way around (which is the usual case), and thought
better of it. Let's not make it even more complicated. Instead, just
blast right over the `INIT_CWD` that is causing us this headache in the

projects/npm-tools/packages/npm-scripts/test/scripts/lint.js
@@ -74,8 +74,18 @@ describe('scripts/lint.js', () => {
 	});
 
 	describe('when running from the top-level "modules/"  directory', () => {
+		let INIT_CWD;
+
 		beforeEach(() => {
+			INIT_CWD = process.env.INIT_CWD;
+
+			process.env.INIT_CWD = MODULES;
+
 			process.chdir(MODULES);
+		});
+
+		afterEach(() => {
+			process.env.INIT_CWD = INIT_CWD;
 		});
 
 		it("calls ESLint's `executeOnFiles()` function", async () => {