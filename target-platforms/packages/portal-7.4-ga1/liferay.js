c804fcee6da9b1197e32178a75ba8b09404224e8
Iván Zaera Avellón <ivan.zaera@liferay.com>
2021-05-24 14:37:12 +0200

    feat(portal-7.4-ga1): invoke babel 7 from target platform script
    Fixes #543

--- target-platforms/packages/portal-7.4-ga1/liferay.js
+++ target-platforms/packages/portal-7.4-ga1/liferay.js
@@ -5,34 +5,30 @@
  * SPDX-License-Identifier: MIT
  */
 
-/* eslint-disable @liferay/liferay/no-dynamic-require */
-/* eslint-disable no-console */
+const {
+	error,
+	print,
+	title,
+} = require('liferay-npm-build-tools-common/lib/format');
 
-const childProcess = require('child_process');
-const path = require('path');
+const build = require('./build');
+const pkgJson = require('./package.json');
 
 if (process.argv.length < 3) {
-	console.log('ERROR: no command provided');
+	print(error`No command provided`);
 	process.exit(1);
 }
 
 const cmd = process.argv[2];
 
-const bundlerPkgJsonPath = require.resolve('liferay-npm-bundler/package.json');
-const bundlerDir = path.dirname(bundlerPkgJsonPath);
-const bundlerPkgJson = require(bundlerPkgJsonPath);
-const bundlerPath = path.resolve(
-	bundlerDir,
-	bundlerPkgJson.bin['liferay-npm-bundler']
-);
-
 switch (cmd) {
 	case 'build':
-		childProcess.fork(bundlerPath);
+		print(title`Building project for target platform: {${pkgJson.name}}`);
+		build();
 		break;
 
 	default:
-		console.log('ERROR: unknown command', cmd, 'provided');
+		print(error`Unknown command: {${cmd}}`);
 		process.exit(1);
 		break;
 }
