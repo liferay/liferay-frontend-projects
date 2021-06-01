5f1910d75838d67fac3cdc3143f7bb06d7728654
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2021-06-01 13:51:21 +0200

    feat(eslint-config): Create rule to avoid abbreviations in identifiers

---
+++ projects/eslint-config/plugins/liferay/tests/lib/rules/no-abbreviations.js
@@ -0,0 +1,63 @@
+/**
+ * SPDX-FileCopyrightText: © 2017 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+const MultiTester = require('../../../../../scripts/MultiTester');
+const rule = require('../../../lib/rules/no-abbreviations');
+
+const parserOptions = {
+	parserOptions: {
+		ecmaVersion: 6,
+		sourceType: 'module',
+	},
+};
+
+const ruleTester = new MultiTester(parserOptions);
+
+ruleTester.run('no-abbreviations', rule, {
+	invalid: [
+		{
+			code: `
+				e.preventDefault();
+			`,
+			errors: [
+				{
+					message:
+						'Avoid e abbreviation, preferred alternatives are [event,error]',
+					type: 'Identifier',
+				},
+			],
+		},
+
+		{
+			code: `
+				const btn = document.getElementById('button');
+			`,
+			errors: [
+				{
+					message:
+						'Avoid btn abbreviation, preferred alternative is button',
+					type: 'Identifier',
+				},
+			],
+		},
+
+		{
+			code: `
+				trigger.addEventListener('click', (e) => {
+					console.log('lint error');
+				});
+			`,
+			errors: [
+				{
+					message:
+						'Avoid e abbreviation, preferred alternatives are [event,error]',
+					type: 'Identifier',
+				},
+			],
+		},
+	],
+
+	valid: [],
+});
