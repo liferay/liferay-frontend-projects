a3134d9e3ad53e6da740da11d5df9d49dd008127
Iván Zaera Avellón <ivan.zaera@liferay.com>
2021-06-02 13:08:12 +0200

    feat(liferay-cli): add sample code generation to liferay target
    The generated code draws a portlet widget using portal's React. It assumes
    the project has L10N support since it is targetting Liferay's platform.

--- projects/js-toolkit/packages/liferay-cli/src/new/target-liferay/index.ts
+++ projects/js-toolkit/packages/liferay-cli/src/new/target-liferay/index.ts
@@ -4,11 +4,16 @@
  */
 
 import {
+	FilePath,
+	TemplateRenderer,
 	addPkgJsonDependencies,
 	addPkgJsonScripts,
 	transformJsonFile,
 } from '@liferay/js-toolkit-core';
 
+import * as facetConfiguration from '../facet-configuration';
+import * as facetLocalization from '../facet-localization';
+import * as facetPortlet from '../facet-portlet';
 import * as facetProject from '../facet-project';
 import prompt from '../util/prompt';
 
@@ -19,7 +24,7 @@ export const name = 'Liferay Platform Project';
 export async function processOptions(options: Options): Promise<Options> {
 	options = await facetProject.processOptions(options);
 
-	return prompt(options, [
+	options = await prompt(options, [
 		{
 			choices: ['portal-7.4-ga1'],
 			default: 'portal-7.4-ga1',
@@ -28,10 +33,27 @@ export async function processOptions(options: Options): Promise<Options> {
 			type: 'list',
 		},
 	]);
+
+	options = await facetLocalization.processOptions(options);
+	options = await facetPortlet.processOptions(options);
+	options = await facetConfiguration.processOptions(options);
+
+	return options;
 }
 
 export async function render(options: Options): Promise<void> {
 	await facetProject.render(options);
+	await facetLocalization.render(options);
+	await facetPortlet.render(options);
+	await facetConfiguration.render(options);
+
+	const renderer = new TemplateRenderer(
+		new FilePath(__dirname).join('templates'),
+		options.outputPath
+	);
+
+	await renderer.render('src/AppComponent.js', options);
+	await renderer.render('src/index.js', options);
 
 	const pkgJsonFile = options.outputPath.join('package.json');
 
