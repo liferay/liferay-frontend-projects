b5922c2847e1095b438a48af58790b4257477147
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2021-06-08 13:46:34 -0700

    feat(@liferay/eslint-config): add rule to prevent developers from using createPortal directly from react-dom.

--- projects/eslint-config/plugins/portal/index.js
+++ projects/eslint-config/plugins/portal/index.js
@@ -10,6 +10,7 @@ module.exports = {
 		'no-global-fetch': require('./lib/rules/no-global-fetch'),
 		'no-loader-import-specifier': require('./lib/rules/no-loader-import-specifier'),
 		'no-metal-plugins': require('./lib/rules/no-metal-plugins'),
+		'no-react-dom-create-portal': require('./lib/rules/no-react-dom-create-portal'),
 		'no-react-dom-render': require('./lib/rules/no-react-dom-render'),
 		'no-side-navigation': require('./lib/rules/no-side-navigation'),
 	},
