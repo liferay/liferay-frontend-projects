e1ca4eb36761f157e2efb9828bcd3c8b85f045d3
Iván Zaera Avellón <ivan.zaera@liferay.com>
2021-06-08 14:21:39 +0200

    test(js-toolkit-core): add tests for addPkgJsonDependencies

---
+++ projects/js-toolkit/packages/js-toolkit-core/src/transform/json/operation/__tests__/addPkgJsonDependencies.test.ts
@@ -0,0 +1,72 @@
+/**
+ * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+import PkgJson from '../../../../schema/PkgJson';
+import addPkgJsonDependencies from '../addPkgJsonDependencies';
+
+describe('addPkgJsonDependencies', () => {
+	let pkgJson: PkgJson;
+
+	beforeEach(() => {
+		pkgJson = {
+			name: 'a-project',
+			version: '1.0.0',
+		};
+	});
+
+	it('adds dependencies', async () => {
+		const pkgJson2 = await addPkgJsonDependencies({
+			react: '^16.0.0',
+			'react-dom': '^16.0.0',
+		})(pkgJson);
+
+		expect(pkgJson2).toStrictEqual({
+			dependencies: {
+				react: '^16.0.0',
+				'react-dom': '^16.0.0',
+			},
+			name: 'a-project',
+			version: '1.0.0',
+		});
+	});
+
+	it('adds devDependencies', async () => {
+		const pkgJson2 = await addPkgJsonDependencies(
+			{
+				react: '^16.0.0',
+				'react-dom': '^16.0.0',
+			},
+			'dev'
+		)(pkgJson);
+
+		expect(pkgJson2).toStrictEqual({
+			devDependencies: {
+				react: '^16.0.0',
+				'react-dom': '^16.0.0',
+			},
+			name: 'a-project',
+			version: '1.0.0',
+		});
+	});
+
+	it('adds peerDependencies', async () => {
+		const pkgJson2 = await addPkgJsonDependencies(
+			{
+				react: '^16.0.0',
+				'react-dom': '^16.0.0',
+			},
+			'peer'
+		)(pkgJson);
+
+		expect(pkgJson2).toStrictEqual({
+			name: 'a-project',
+			peerDependencies: {
+				react: '^16.0.0',
+				'react-dom': '^16.0.0',
+			},
+			version: '1.0.0',
+		});
+	});
+});
