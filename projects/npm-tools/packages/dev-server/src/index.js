53b5add56a161599cb86a5e1d5a4f7daea91f8c0
Chema Balsas <jose.balsas@liferay.com>
2021-08-31 09:22:49 +0100

    feat(dev-server): Adds file watcher to trigger module builds on changed files
    Naively attempts to run `yarn build` from the root of the modules that
    have changed.

--- projects/npm-tools/packages/dev-server/src/index.js
+++ projects/npm-tools/packages/dev-server/src/index.js
@@ -4,6 +4,8 @@
  */
 
 const chalk = require('chalk');
+const {spawn} = require('child_process');
+const chokidar = require('chokidar');
 const {Command} = require('commander');
 const {readFile, readFileSync} = require('fs');
 const glob = require('glob');
@@ -11,17 +13,19 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 const open = require('open');
 const {dirname, resolve} = require('path');
+const pkgUp = require('pkg-up');
 
 const log = require('./util/log');
 
-const IGNORED_GLOBS = [
-	'**/node_modules/**',
-	'**/classes/**',
-	'**/tmp/**',
-	'**/build/**',
-	'**/sdk/**',
+const IGNORED_PATHS = [
+	'build',
+	'classes',
+	'node_modules',
+	'sdk',
+	'tmp',
+	'types',
 ];
-
+const WATCH_GLOB = '**/*.{js,jsx,ts,tsx}';
 const MODULE_REGEXP = /\/o\/(?<module>[^/]*)\/(?<file>[^?]*)/;
 const RESOLVED_MODULE_REGEXP = /\/o\/js\/resolved-module\/(?<module>@?[^@]*)@(?<version>\d\.\d\.\d)\/(?<file>[^?]*)/;
 
@@ -52,7 +56,7 @@ module.exports = async function () {
 
 	const modules = glob.sync('**/package.json', {
 		cwd: WD,
-		ignore: IGNORED_GLOBS,
+		ignore: IGNORED_PATHS.map((path) => `**/${path}/**`),
 	});
 
 	modules.forEach((module) => {
@@ -124,5 +128,43 @@ module.exports = async function () {
 	}).listen(port);
 
 	log(chalk.green(`\nLiferay Dev Server is ready at http:127.0.0.1:${port}`));
+
 	open(`http://127.0.0.1:${port}`);
+
+	const watcher = chokidar.watch(`${WD}/modules/${WATCH_GLOB}`, {
+		ignoreInitial: true,
+		ignored: (path) =>
+			IGNORED_PATHS.some((ignoredPath) => path.includes(ignoredPath)),
+	});
+
+	watcher.on('change', async (path) => {
+		log(chalk.blue(`\nDetected file change: ${path}`));
+
+		const moduleWD = dirname(await pkgUp({cwd: dirname(path)}));
+
+		log(chalk.blue(`Triggering yarn build from ${moduleWD}`));
+
+		const logger = (data) => log(chalk.grey(`\t ${data}`));
+
+		const yarnBuild = spawn('yarn', ['build'], {cwd: moduleWD});
+
+		yarnBuild.stdout.on('data', logger);
+		yarnBuild.stderr.on('data', logger);
+
+		yarnBuild.on('close', (code) => {
+			if (!code) {
+				log(
+					chalk.green(
+						`Build completed successfully, you can reload now to see your changes!`
+					)
+				);
+			}
+		});
+
+		yarnBuild.on('error', (error) => {
+			log(chalk.red(error));
+		});
+	});
+
+	log(chalk.green(`\nWatching changes at ${WD}/modules/${WATCH_GLOB}`));
 };
