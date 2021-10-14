8f862e4736151299f5b148d75b8749fb580aad22
Krešimir <kresimir.coko@liferay.com>
2021-10-14 09:35:17 +0200

    feat(eslint-plugin): add one-var rule to our rulelist

--- projects/eslint-plugin/configs/general.js
+++ projects/eslint-plugin/configs/general.js
@@ -111,6 +111,7 @@ const config = {
 		'no-unused-expressions': 'error',
 		'no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
 		'object-shorthand': 'error',
+		'one-var': ['error', 'never'],
 		'padding-line-between-statements': [
 			'error',
 			{blankLine: 'always', next: 'return', prev: '*'},
