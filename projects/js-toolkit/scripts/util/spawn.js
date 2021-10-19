dd30f4dd18c0d33a24f435d357e0654412cbca2f
Iván Zaera Avellón <ivan.zaera@liferay.com>
2021-10-19 14:27:16 +0200

    chore(js-toolkit): fix Windows build

--- projects/js-toolkit/scripts/util/spawn.js
+++ projects/js-toolkit/scripts/util/spawn.js
@@ -7,9 +7,14 @@ const childProcess = require('child_process');
 
 module.exports = function spawn(cmd, args = [], options = {}) {
 	const result = childProcess.spawnSync(cmd, args, {
+		shell: true,
 		stdio: 'inherit',
 		...options,
 	});
+
+	if (result.error) {
+		throw result.error;
+	}
 
 	if (result.status !== 0) {
 		throw new Error(`Execution failure: ${cmd} ${args.join(' ')}`);
