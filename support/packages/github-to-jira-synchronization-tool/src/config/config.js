aeae6a397fcf02287544a035ec52f782f10195a1
Victor Galan <victor.galan@liferay.com>
2021-11-22 23:25:00 +0100

    chore(github-to-jira-synchronization-tool): add configuration file and methods to read that.
    This configuration will include the mapping from github users to jira issues and the mapping from github labels to jira issue types

---
+++ support/packages/github-to-jira-synchronization-tool/src/config/config.js
@@ -0,0 +1,43 @@
+/**
+ * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+let config = null;
+
+function loadConfig() {
+	if (config) {
+		return config;
+	}
+
+	try {
+		config = require('../../mapping-config.json');
+
+		return config;
+	}
+	catch (error) {
+		console.error(
+			'mapped-config.json cannot be read, please make sure to create it in the root of the project'
+		);
+
+		process.exit(1);
+	}
+}
+
+function getUserMapping(username) {
+	const mappingConfig = loadConfig();
+
+	const userMappings = mappingConfig.userMappings;
+
+	return userMappings[username] || userMappings.default;
+}
+
+function getLabelMapping(label) {
+	const mappingConfig = loadConfig();
+
+	const labelMappings = mappingConfig.labelMappings;
+
+	return labelMappings[label] || labelMappings.default;
+}
+
+module.exports = {getLabelMapping, getUserMapping, loadConfig};
