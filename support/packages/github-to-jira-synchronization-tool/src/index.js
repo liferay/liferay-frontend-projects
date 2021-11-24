eda440c868116f368558622018a6491112b62b59
Victor Galan <victor.galan@liferay.com>
2021-11-24 09:03:53 +0000

    chore(github-to-jira-synchronization-tool): proccess all the events

--- support/packages/github-to-jira-synchronization-tool/src/index.js
+++ support/packages/github-to-jira-synchronization-tool/src/index.js
@@ -4,17 +4,38 @@
  */
 
 const Server = require('./Server');
-const {SECRET, PORT} = require('./constants');
+const {loadConfig} = require('./config/config');
+const {PORT, SECRET} = require('./constants');
 
-const eventHandlers = [require('./event-handlers/issueCreatedHandler')];
+const eventHandlers = [
+	require('./event-handlers/issueAssignedHandler'),
+	require('./event-handlers/issueClosedHandler'),
+	require('./event-handlers/issueCreatedHandler'),
+	require('./event-handlers/issueEditedHandler'),
+	require('./event-handlers/issueLabeledHandler'),
+	require('./event-handlers/issueMilestonedHandler'),
+	require('./event-handlers/issueReopenedHandler'),
+	require('./event-handlers/issueCommentCreatedHandler'),
+	require('./event-handlers/issueCommentEditedHandler'),
+	require('./event-handlers/issueCommentDeletedHandler'),
+];
 
-function main() {
+async function main() {
+	loadConfig();
+
 	const server = new Server(SECRET, PORT);
 
-	server.start(['issues', 'issue_comment'], ({name, payload}) => {
+	server.start(['issues', 'issue_comment'], async ({name, payload}) => {
 		for (const eventHandler of eventHandlers) {
 			if (eventHandler.canHandleEvent(name, payload)) {
-				eventHandler.handleEvent(payload);
+				try {
+					await eventHandler.handleEvent(payload);
+				}
+				catch (error) {
+					console.error(
+						`Error while processing ${name} ${payload.action} webhook\n\n${error.message}`
+					);
+				}
 			}
 		}
 	});
