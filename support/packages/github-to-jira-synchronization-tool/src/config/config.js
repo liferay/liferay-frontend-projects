b5d8b7f77ce2627b40a6613488f2345730f754a4
Victor Galan <victor.galan@liferay.com>
2021-12-13 11:06:45 +0100

    chore(github-to-jira-synchronization-tool): allow changing the mapping configuration while the application is running

--- support/packages/github-to-jira-synchronization-tool/src/config/config.js
+++ support/packages/github-to-jira-synchronization-tool/src/config/config.js
@@ -3,21 +3,46 @@
  * SPDX-License-Identifier: MIT
  */
 
+const fs = require('fs');
+const path = require('path');
+
+const log = require('../utils/log');
+
 let config = null;
+let lastModifiedTime = 0;
+
+const CONFIGURATION_PATH = path.resolve(__dirname, '../../mapping-config.json');
+
+function fileHasChanged() {
+	const stat = fs.statSync(CONFIGURATION_PATH);
+
+	if (stat.mtime > lastModifiedTime) {
+		lastModifiedTime = stat.mtime;
+
+		return true;
+	}
+
+	return false;
+}
 
 function loadConfig() {
-	if (config) {
+	if (!fileHasChanged() && config) {
 		return config;
 	}
 
 	try {
-		config = require('../../mapping-config.json');
+		config = JSON.parse(fs.readFileSync(CONFIGURATION_PATH, 'utf-8'));
 
 		return config;
-	}
-	catch (error) {
+	} catch (error) {
+		if (config) {
+			log('new config file is wrong, reusing past config');
+
+			return;
+		}
+
 		console.error(
-			'mapped-config.json cannot be read, please make sure to create it in the root of the project'
+			'mapped-config.json cannot be read, please make sure it is a valid json and to create it in the root of the project'
 		);
 
 		process.exit(1);
