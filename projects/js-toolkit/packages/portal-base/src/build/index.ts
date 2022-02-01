4daca89d166796138daf801df7305ad4c0533ac0
Iván Zaera Avellón <ivan.zaera@liferay.com>
2022-02-01 14:34:08 +0100

    refactor(portal-base): move bundler2 build to its own file

---
+++ projects/js-toolkit/packages/portal-base/src/build/index.ts
@@ -0,0 +1,33 @@
+/**
+ * SPDX-FileCopyrightText: © 2021 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+import {Project, format} from '@liferay/js-toolkit-core';
+
+import abort from '../util/abort';
+import bundler2 from './bundler2';
+import customElement from './customElement';
+
+const {print, success} = format;
+
+export default async function build(): Promise<void> {
+	const project = new Project('.');
+
+	try {
+		switch (project.build.type) {
+			case 'customElement':
+				await customElement(project);
+				break;
+
+			default:
+				await bundler2(project);
+				break;
+		}
+
+		print(success`{Project successfully built}`);
+	}
+	catch (error) {
+		abort(`Build failed!\n${error.stack}`);
+	}
+}
