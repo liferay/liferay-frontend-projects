74c9b7da9236c7be10fdbed51325d0729fea8948
Krešimir Čoko <kresimir.coko@liferay.com>
2022-03-01 13:59:58 +0100

    feat(github-to-jira-sync-tool): Add the /status page that displays a message if the tool is running

--- support/packages/github-to-jira-synchronization-tool/src/Server.js
+++ support/packages/github-to-jira-synchronization-tool/src/Server.js
@@ -10,12 +10,30 @@ class Server {
 	constructor(secret, port) {
 		this.port = port;
 		this.webhooks = new Webhooks({secret});
+		this.responseHead = {
+			'Content-Type': 'application/json',
+		};
+
+		this.handleStatusRequest = (req, res) => {
+			if (req.url === '/status' && req.method === 'GET') {
+				res.writeHead(200, this.responseHead);
+				res.write('The server is running!');
+				res.end();
+			}
+			else {
+				res.writeHead(404, this.responseHead);
+				res.end(JSON.stringify({message: 'Route not found'}));
+			}
+		};
+
+		this.middleware = createNodeMiddleware(this.webhooks, {
+			onUnhandledRequest: (req, res) =>
+				this.handleStatusRequest(req, res),
+		});
 	}
 
 	start(events, onEvent) {
-		http.createServer(createNodeMiddleware(this.webhooks)).listen(
-			this.port
-		);
+		http.createServer(this.middleware).listen(this.port);
 
 		events.forEach((event) => {
 			this.webhooks.on(event, onEvent);
