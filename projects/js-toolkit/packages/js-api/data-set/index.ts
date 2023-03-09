f7f88c8fedfae81e651ed4a36988671014b30687
Iván Zaera Avellón <ivan.zaera@liferay.com>
2023-03-09 16:36:35 +0100

    style: fix linting

--- projects/js-toolkit/packages/js-api/data-set/index.ts
+++ projects/js-toolkit/packages/js-api/data-set/index.ts
@@ -1,15 +1,6 @@
 /**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: LGPL-3.0-or-later
  */
 
 export interface FDSCellRendererArgs {
---
+++ projects/js-toolkit/packages/js-api/index.ts
@@ -0,0 +1,5 @@
+/**
+ * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
