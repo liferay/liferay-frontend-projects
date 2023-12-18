50938f3fb7ea6f0a799d45a508d618299dd2f5df
Marko Čikoš <marko.cikos@liferay.com>
2023-12-18 15:59:49 +0100

    feat(js-api): add types for Editor Config Contributor client extension

---
+++ projects/js-toolkit/packages/js-api/editor/index.ts
@@ -0,0 +1,19 @@
+/**
+ * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+/**
+ * Editor Transformer
+ *
+ * For CKEditor 4, see config API:
+ * https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
+ */
+
+export interface EditorConfigTransformer<T> {
+	(args: T): T;
+}
+
+export interface EditorTransformer<T> {
+	editorConfigTransformer: EditorConfigTransformer<T>;
+}
