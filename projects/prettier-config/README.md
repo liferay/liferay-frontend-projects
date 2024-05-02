c0f08f42eb25f054cb841c71f4ebd6fff3804779
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2024-05-02 12:39:07 +0400

    feat(prettier-config): create new prettier config npm package

---
+++ projects/prettier-config/README.md
@@ -0,0 +1,1 @@
+# @liferay/prettier-config
---
+++ projects/prettier-config/index.js
@@ -0,0 +1,17 @@
+/**
+ * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+export default {
+	braceStyle: 'stroustrup',
+	bracketSpacing: false,
+	endOfLine: 'lf',
+	jsxSingleQuote: false,
+	plugins: ['prettier-plugin-brace-style'],
+	quoteProps: 'consistent',
+	singleQuote: true,
+	tabWidth: 4,
+	trailingComma: 'es5',
+	useTabs: true,
+};
