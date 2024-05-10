cc4b8e6f7e965df89764a286184aa0c6dc9328b8
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2024-05-10 13:57:38 +0400

    feat(prettier-plugin): create new @liferay/prettier-plugin package

---
+++ projects/prettier-plugin/index.js
@@ -0,0 +1,7 @@
+/**
+ * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+export {parsers} from './parsers.js';
+export {printers} from './printers.js';
