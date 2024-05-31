0b96f2ff08431f3bdbbb35aec7c6789a1a77bafb
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2024-05-31 13:20:24 +0400

    fix(prettier-plugin): fix default value for plugin options

--- projects/prettier-plugin/options.mjs
+++ projects/prettier-plugin/options.mjs
@@ -7,6 +7,7 @@ export const options = {
 	commentIgnoreAfterPatterns: {
 		array: true,
 		category: 'Format',
+		default: [{value: []}], // idk why prettier expects this format. ¯\_(ツ)_/¯
 		description:
 			'Ignore line after comment node that contains a given pattern',
 		since: '1.0.0',
@@ -15,6 +16,7 @@ export const options = {
 	commentIgnoreBeforePatterns: {
 		array: true,
 		category: 'Format',
+		default: [{value: []}], // idk why prettier expects this format. ¯\_(ツ)_/¯
 		description:
 			'Ignore new line before comment node that contains a given pattern',
 		since: '1.0.0',
@@ -23,6 +25,7 @@ export const options = {
 	commentIgnorePatterns: {
 		array: true,
 		category: 'Format',
+		default: [{value: []}], // idk why prettier expects this format. ¯\_(ツ)_/¯
 		description:
 			'Ignore new lines line before and after comment nodes that contain a given pattern',
 		since: '1.0.0',
