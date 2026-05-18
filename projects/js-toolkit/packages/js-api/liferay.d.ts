8845603b594caa0490e9fe2ef599ff3ecab9d949
antonio-ortega <antonio.ortega@liferay.com>
2026-05-18 20:47:24 +0200

    LPD-78127 Avoid using __unsafe__ namespace wherever it is possible

--- projects/js-toolkit/packages/js-api/liferay.d.ts
+++ projects/js-toolkit/packages/js-api/liferay.d.ts
@@ -3,16 +3,30 @@
  * SPDX-License-Identifier: LGPL-3.0-or-later
  */
 
-declare const Liferay: {
-	State: {
-		__unsafe__: {
-			getAtomOrSelectorKey(key: string): unknown;
-			readKey(key: string): unknown;
-			subscribeKey(
+declare namespace Liferay {
+	namespace State {
+		interface Atom<T> {
+			readonly __type?: T;
+			readonly key: string;
+		}
+
+		function read<T>(atom: Atom<T>): T;
+		function subscribe<T>(
+			atom: Atom<T>,
+			callback: (value: T) => void
+		): {dispose: () => void};
+		function write<T>(atom: Atom<T>, value: T): void;
+
+		namespace __unsafe__ {
+			function getAtomOrSelectorKey(
+				key: string
+			): Atom<unknown> | null;
+			function readKey(key: string): unknown;
+			function subscribeKey(
 				key: string,
 				callback: (value: unknown) => void
 			): {dispose: () => void};
-			writeKey(key: string, value: unknown): void;
-		};
-	};
-};
+			function writeKey(key: string, value: unknown): void;
+		}
+	}
+}
