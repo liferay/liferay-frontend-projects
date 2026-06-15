b3fbd6a2918fb82aeb54879895bc6db5ab90d865
chestofwonders <miguelarroyoes@hotmail.com>
2026-06-15 18:14:03 +0200

    fix(js-api): add eslint config with LGPL notice for js-api package

---
+++ projects/js-api/.eslintrc.js
@@ -0,0 +1,17 @@
+/**
+ * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+const path = require('path');
+
+module.exports = {
+	rules: {
+		'notice/notice': [
+			'error',
+			{
+				templateFile: path.join(__dirname, 'copyright.js'),
+			},
+		],
+	},
+};
---
+++ projects/js-api/copyright.js
@@ -0,0 +1,4 @@
+/**
+ * SPDX-FileCopyrightText: © <%= YEAR %> Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
