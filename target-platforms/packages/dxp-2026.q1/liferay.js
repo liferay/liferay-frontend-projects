8ae8a3dfe4d6a07fc9b127914c70431781ed336a
Iván Zaera Avellón <ivan.zaera@liferay.com>
2026-07-29 12:01:53 +0200

    LPD-100111 Create dxp-2026.q1 target platform
    Since we moved Clay from frontend-taglib-clay to frontend-js-clay-web, all customer projects based
    on target platforms will fail from version 2026.q1 without any possibility to make them work.
    
    The AMD loader is already under a deprecation FF and will be removed but for now we should give
    customers a way to keep deploying their projects until the final removal is done.
    
    We will create a dxp-2026.q1 target platform than can be used instead of dxp-7.4 by customers. We
    will not create target platforms for further versions (eg: 2026.q2) unless it is absolutely
    necessary and we will maintain dxp-2026.q1 by hand (there was a create-platform script that did the
    trick before node-scripts was introduced but now it cannot be used any more and it doesn’t make
    sense to rewrite it).
    
    Since dxp-2026.q1 is more like a compatibility patch I won’t add all packages and versions that are
    available in a DXP 2026.q1 instance as we did before because:
    
      - It will have a big cost
      - It will be useless (as we have moved a lot of stuff)
      - Most of the packages were considered internals and could break already before node-scripts (ie:
        when target platforms were maintained).
    
    So I think that exposing clay and react should be enough. Maybe we need to expose frontend-js-web or
    something alike in the future if any customer used it but in that case we must reflect about it
    before doing it because even if we expose it chances are that symbols have changed or renamed so
    maybe that keeps breaking the customer’s project and it’s better for them to migrate them to CXs.

---
+++ target-platforms/packages/dxp-2026.q1/liferay.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+/**
+ * SPDX-FileCopyrightText: © 2021 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+const {default: portalBase} = require('@liferay/portal-base');
+
+portalBase(__dirname);
